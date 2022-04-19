510b0c79418408c80b3a511d2444a2d0eca3af42
Igor Shurubura <igorr@igorr.name>
2022-04-19 12:53:40 +0200

    fix: error message color
    Closes FAAS-2682

--- libs/ui/error-message/src/error-message.styles.ts
+++ libs/ui/error-message/src/error-message.styles.ts
@@ -2,9 +2,16 @@ import { css } from 'lit';
 
 export const errorStyles = css`
   :host {
+    --oryx-icon-size: 15px;
+
     display: flex;
     gap: 5px;
     align-items: center;
+    color: var(--oryx-color-error);
+    font-size: 12px;
+  }
+
+  oryx-icon {
     color: var(--oryx-color-error);
   }
 `;
