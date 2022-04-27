641f300c309c85dfc0b36656c35058ca914d6873
tarashaliuk <43376934+tarashaliuk@users.noreply.github.com>
2022-04-27 18:55:53 +0300

    feat: toogle component interactive stories (#626)
    * feat: add interactive stories
    
    * feat: update dep tree
    
    * feat: added timeouts

---
+++ libs/ui/actions/toggle/src/stories/interactive/checkbox-based.stories.ts
@@ -0,0 +1,63 @@
+import { expect } from '@storybook/jest';
+import { userEvent } from '@storybook/testing-library';
+import { Meta, Story } from '@storybook/web-components';
+import { html, TemplateResult } from 'lit';
+import { storybookPrefix } from '../../../../../.storybook/constant';
+import '../../index';
+
+export default {
+  title: `${storybookPrefix}/Actions/Toggle/Interactive`,
+} as Meta;
+
+const Template: Story<unknown> = (): TemplateResult => {
+  return html`
+    <oryx-toggle>
+      <input
+        name="test-group"
+        value="first"
+        type="checkbox"
+        checked
+        aria-label="make a11y happy"
+      />
+      first toggle
+    </oryx-toggle>
+    <oryx-toggle>
+      <input
+        name="test-group"
+        value="second"
+        type="checkbox"
+        aria-label="make a11y happy"
+      />
+      second toggle
+    </oryx-toggle>
+
+    <style>
+      oryx-toggle {
+        margin-bottom: 10px;
+      }
+    </style>
+  `;
+};
+
+export const Checkbox = Template.bind({});
+
+Checkbox.play = async (obj: {
+  args: unknown;
+  canvasElement: HTMLElement;
+}): Promise<void> => {
+  const firstCheckbox = obj.canvasElement.querySelector(
+    'input[value=first]'
+  ) as HTMLInputElement;
+  const secondCheckbox = obj.canvasElement.querySelector(
+    'input[value=second]'
+  ) as HTMLInputElement;
+
+  await new Promise((r) => setTimeout(r, 1000));
+  userEvent.click(firstCheckbox);
+
+  await new Promise((r) => setTimeout(r, 1000));
+  userEvent.click(secondCheckbox);
+
+  expect(firstCheckbox.checked).toBeFalsy();
+  expect(secondCheckbox.checked).toBeTruthy();
+};
---
+++ libs/ui/actions/toggle/src/stories/interactive/radio-based.stories.ts
@@ -0,0 +1,78 @@
+import { expect } from '@storybook/jest';
+import { userEvent } from '@storybook/testing-library';
+import { Meta, Story } from '@storybook/web-components';
+import { html, TemplateResult } from 'lit';
+import { storybookPrefix } from '../../../../../.storybook/constant';
+import '../../index';
+
+export default {
+  title: `${storybookPrefix}/Actions/Toggle/Interactive`,
+} as Meta;
+
+const Template: Story<unknown> = (): TemplateResult => {
+  return html`
+    <oryx-toggle>
+      <input
+        name="test-group"
+        value="first"
+        type="radio"
+        checked
+        aria-label="make a11y happy"
+      />
+      first toggle
+    </oryx-toggle>
+    <oryx-toggle>
+      <input
+        name="test-group"
+        value="second"
+        type="radio"
+        aria-label="make a11y happy"
+      />
+      second toggle
+    </oryx-toggle>
+    <oryx-toggle>
+      <input
+        name="test-group"
+        value="third"
+        type="radio"
+        aria-label="make a11y happy"
+      />
+      third toggle
+    </oryx-toggle>
+
+    <style>
+      oryx-toggle {
+        margin-bottom: 10px;
+      }
+    </style>
+  `;
+};
+
+export const Radio = Template.bind({});
+
+Radio.play = async (obj: {
+  args: unknown;
+  canvasElement: HTMLElement;
+}): Promise<void> => {
+  const firstRadio = obj.canvasElement.querySelector(
+    'input[value=first]'
+  ) as HTMLInputElement;
+  const secondRadio = obj.canvasElement.querySelector(
+    'input[value=second]'
+  ) as HTMLInputElement;
+  const thirdRadio = obj.canvasElement.querySelector(
+    'input[value=third]'
+  ) as HTMLInputElement;
+
+  await new Promise((r) => setTimeout(r, 1000));
+  userEvent.click(secondRadio);
+  expect(firstRadio.checked).toBeFalsy();
+  expect(secondRadio.checked).toBeTruthy();
+  expect(thirdRadio.checked).toBeFalsy();
+
+  await new Promise((r) => setTimeout(r, 1000));
+  userEvent.click(thirdRadio);
+  expect(firstRadio.checked).toBeFalsy();
+  expect(secondRadio.checked).toBeFalsy();
+  expect(thirdRadio.checked).toBeTruthy();
+};
