71870813238e3fe323ed22c803319b4cc2e04616
tarashaliuk <43376934+tarashaliuk@users.noreply.github.com>
2022-04-28 11:03:05 +0300

    feat: modal interaction stories (#625)

--- libs/ui/overlays/modal/src/stories/interaction/common.ts
+++ libs/ui/overlays/modal/src/stories/interaction/common.ts
@@ -1,4 +1,5 @@
 import { html, TemplateResult } from 'lit';
+import { when } from 'lit/directives/when.js';
 
 export const getModal = (isOpen: boolean): TemplateResult => {
   return html`
@@ -7,6 +8,29 @@ export const getModal = (isOpen: boolean): TemplateResult => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua
       </p>
+    </oryx-modal>
+  `;
+};
+
+export const getModalWithNestedModal = (
+  isNestedModalOpen: boolean
+): TemplateResult => {
+  return html`
+    <oryx-modal open header="First modal">
+      <div>
+        <p>
+          First modal content First modal content First modal content First
+          modal content First modal content First modal content
+        </p>
+        ${when(
+          !isNestedModalOpen,
+          () => html`<button id="openNestedModalBtn">Open nested modal</button>`
+        )}
+
+        <oryx-modal ?open=${isNestedModalOpen} header="Nested modal">
+          <p>Nested modal content</p>
+        </oryx-modal>
+      </div>
     </oryx-modal>
   `;
 };
---
+++ libs/ui/overlays/modal/src/stories/interaction/open-nested-modal.stories.ts
@@ -0,0 +1,49 @@
+import { expect } from '@storybook/jest';
+import { userEvent } from '@storybook/testing-library';
+import { Meta, Story } from '@storybook/web-components';
+import { html, TemplateResult } from 'lit';
+import { storybookPrefix } from '../../../../../.storybook/constant';
+import '../../index';
+import { ModalComponent } from '../../modal.component';
+import { getModalWithNestedModal } from './common';
+
+export default {
+  title: `${storybookPrefix}/Overlays/Modal/Interaction`,
+} as Meta;
+
+const Template: Story = (): TemplateResult => {
+  return html`
+    <script>
+      (() => {
+        const openNestedModalBtn = document.querySelector(
+          '#openNestedModalBtn'
+        );
+
+        const nestedModal = document.querySelectorAll('oryx-modal')[1];
+
+        openNestedModalBtn?.addEventListener('click', () => {
+          nestedModal?.open();
+        });
+      })();
+    </script>
+
+    ${getModalWithNestedModal(false)}
+  `;
+};
+
+export const OpenNestedModal = Template.bind({});
+
+OpenNestedModal.play = async (obj: {
+  canvasElement: HTMLElement;
+}): Promise<void> => {
+  const nestedModal = obj.canvasElement.querySelector(
+    'oryx-modal[header="Nested modal"]'
+  ) as ModalComponent;
+  const openNestedModalBtn = obj.canvasElement.querySelector(
+    '#openNestedModalBtn'
+  ) as HTMLButtonElement;
+
+  await new Promise((r) => setTimeout(r, 1000));
+  userEvent.click(openNestedModalBtn);
+  expect(nestedModal.hasAttribute('open')).toBeTruthy();
+};
