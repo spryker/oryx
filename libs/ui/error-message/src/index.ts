f696967bd13cd042ec9a30488599d1022955443b
Talos Ioan <tjhn.tech@gmail.com>
2022-05-02 10:39:07 +0300

    feat: organizations select (#616)
    * feat: WIP replace organization old spy-select with oryx-select
    
    * feat: adjust styles and some properties of oryx-select
    
    * fix import/export
    
    * drop generation of empty option (temp solution)
    
    * general type and organisation select
    
    * use placeholder value on organization
    
    - default value must match the placeholder
    - we must reset to the placeholder value
    
    * feat: revert general component changes; add error message for required field in organization, repository and branch selects
    
    * feat: convert organizationId to string from number, to avoid matching issues with the html select value, which is always a string; add coverage for organization component; update tests for github and gitlab loaders, loadOrganizations methods
    
    * feat: fix repository and branch selects tests - for accessibility, we are fetching data on focus in instead of click; avoid UTs warnings about oryx-chips and input trim directive
    
    * feat: remove unnecessary dependencies
    
    * feat: adjust e2e tests; add back removed by mistake dependencies in code repository module
    
    * feat: remove unnecessary changes from select controller
    
    * feat: use organization instead of organisation everywhere in the ***
    
    * feat: remove the redundant color of the oryx-select select
    
    * feat: let the organisation text instead of organization, where we compare to what we receive from be
    
    * feat: code review follow-up: remove unnecessary scss file; remove unnecessary checks;
    
    * feat: code review follow-up: add back needed check
    
    * feat: code review follow-up: create LoaderStateValue.Empty constant and use it
    
    * feat: prettier
    
    * feat: code review follow-up: use the handleInputError pipe for organization, repository and branch selects
    
    * feat: add HandleInputErrorModule dependency to organization, repository and branch test files
    
    * feat: update error message property for organization, repository and branch after handleInputError pipe was updated; add missing oryx-radio import in code-repository component tests file
    
    Co-authored-by: tobi-or-not-tobi <tobiasouwejan@gmail.com>

--- libs/ui/error-message/src/index.ts
+++ libs/ui/error-message/src/index.ts
@@ -1,4 +1,7 @@
 import { ErrorMessageComponent } from './error-message.component';
 
+export * from './error-message.component';
+export * from './error-message.styles';
+
 customElements.get('oryx-error-message') ||
   customElements.define('oryx-error-message', ErrorMessageComponent);
