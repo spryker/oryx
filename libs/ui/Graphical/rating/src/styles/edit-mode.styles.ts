b19e5ec497410c518941e8722f5cbc727736a279
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2022-05-13 17:33:16 +0200

    fixed broken style in rating component (#702)
    A broken style was causing the emoji based styling not to function during editing.

--- libs/ui/Graphical/rating/src/styles/edit-mode.styles.ts
+++ libs/ui/Graphical/rating/src/styles/edit-mode.styles.ts
@@ -27,7 +27,7 @@ export const ratingEditModeStyles = css`
 
   fieldset input:valid:hover + slot ~ slot[has-char],
   fieldset input:hover + slot ~ slot[has-char],
-  fieldset :not(:hover) input:checked + slot ~ slot[has-char] {
+  fieldset:not(:hover) input:checked + slot ~ slot[has-char] {
     opacity: 30%;
   }
 `;
