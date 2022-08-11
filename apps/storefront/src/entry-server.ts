6916ace8e55965a30ddefd34be92aedc3cb0aa6a
aleespryker <94365900+aleespryker@users.noreply.github.com>
2022-08-11 17:06:06 +0200

    fix: navigate RouterService when route changes on server side (#1142)
    Fixes issue with components missing on page refresh in dev environments in SSR.

--- apps/storefront/src/entry-server.ts
+++ apps/storefront/src/entry-server.ts
@@ -1,6 +1,7 @@
 // organize-imports-ignore
 import './app.server';
 import { getInjector } from '@spryker-oryx/injector';
+import { RouterService } from '@spryker-oryx/experience';
 import { SSRAwaiterService, ContextService } from '@spryker-oryx/core';
 import { render as litRender } from '@lit-labs/ssr/lib/render-lit-html.js';
 import { html } from 'lit';
@@ -18,9 +19,12 @@ export const renderComponent = async (
 
 export const render = async (element): Promise<string> => {
   window.location = element.route;
+  const routerService = getInjector().inject(RouterService);
   const awaiter = getInjector().inject(SSRAwaiterService);
   const context = getInjector().inject(ContextService) as ServerContextService;
   const ssrResult = await renderComponent(element);
+
+  routerService.go(window.location.pathname);
 
   let stream = '';
   let data = ssrResult.next();
