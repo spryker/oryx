522b98995ae1d8f8d47c5c2b04c4be460dc00bba
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2022-09-19 10:36:56 +0200

    chore: move colors from icon component to SVGs (#1287)
    Cleaned up our icon component and moved colors to the SVGs. 
    
    closes [HRZ-1092](https://spryker.atlassian.net/browse/HRZ-1092)

--- libs/ui/error-message/src/error-message.styles.ts
+++ libs/ui/error-message/src/error-message.styles.ts
@@ -3,15 +3,12 @@ import { css } from 'lit';
 export const errorStyles = css`
   :host {
     --oryx-icon-size: 15px;
+    --oryx-icon-color: currentColor;
 
     display: flex;
     gap: 5px;
     align-items: center;
     color: var(--oryx-color-error);
     font-size: 12px;
-  }
-
-  oryx-icon {
-    color: var(--oryx-color-error);
   }
 `;
--- libs/ui/graphical/icon/src/icon.styles.ts
+++ libs/ui/graphical/icon/src/icon.styles.ts
@@ -14,7 +14,7 @@ export const styles = css`
 
   svg,
   ::slotted(svg) {
-    fill: currentColor;
+    fill: var(--oryx-icon-color, currentColor);
     width: calc(var(--oryx-icon-size, 24px) - (var(--_margin) * 2));
     aspect-ratio: 1 / 1;
     margin: var(--_margin);
@@ -27,32 +27,5 @@ export const styles = css`
 
   :host([size='small']) {
     --_margin: 6px;
-  }
-
-  /* standard colors */
-  :host([type='warning']) {
-    color: var(--oryx-color-warning);
-  }
-  :host([type='error']) {
-    color: var(--oryx-color-error);
-  }
-  :host([type='success']) {
-    color: var(--oryx-color-success);
-  }
-  :host([type='info']),
-  :host([type='filter']) {
-    color: var(--oryx-color-info);
-  }
-
-  :host([type='popular']) {
-    color: #ff6800;
-  }
-
-  :host([type='goldPartner']) {
-    color: #fdaf1c;
-  }
-
-  :host([type='silverPartner']) {
-    color: #9ea1a6;
   }
 `;
