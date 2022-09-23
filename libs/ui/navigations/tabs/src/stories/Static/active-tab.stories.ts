f87035fd0f4b92e57a9a5a283c2a68523b441d01
nsilva-spryker <105730125+nsilva-spryker@users.noreply.github.com>
2022-09-23 10:26:39 +0100

    fix: active index story for secondary tabs
    Closes https://spryker.atlassian.net/browse/HRZ-1147

--- libs/ui/navigations/tabs/src/stories/Static/active-tab.stories.ts
+++ libs/ui/navigations/tabs/src/stories/Static/active-tab.stories.ts
@@ -32,6 +32,31 @@ const Template: Story<Props> = (): TemplateResult => {
       <div slot="panels">Сontent for primary tab 4</div>
       <div slot="panels">Сontent for primary tab 5</div>
     </oryx-tabs>
+
+    <h3 class="margin-top">Secondary tabs</h3>
+
+    <oryx-tabs activeTabIndex="2" appearance="secondary">
+      <oryx-tab>Tab 1</oryx-tab>
+      <oryx-tab>Tab 2</oryx-tab>
+      <oryx-tab>Tab 3</oryx-tab>
+      <oryx-tab>
+        <oryx-icon type="star"></oryx-icon>
+        Tab 4</oryx-tab
+      >
+      <oryx-tab>Tab 5</oryx-tab>
+
+      <div slot="panels">Сontent for primary tab 1</div>
+      <div slot="panels">Сontent for primary tab 2</div>
+      <div slot="panels">Сontent for primary tab 3</div>
+      <div slot="panels">Сontent for primary tab 4</div>
+      <div slot="panels">Сontent for primary tab 5</div>
+    </oryx-tabs>
+
+    <style>
+      .margin-top {
+        margin-top: 50px;
+      }
+    </style>
   `;
 };
 
