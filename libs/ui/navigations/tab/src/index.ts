e68bd39ea1e9ed40d55d31746194ac8484cf8c22
nsilva-spryker <105730125+nsilva-spryker@users.noreply.github.com>
2022-09-26 10:54:32 +0100

    feat: integrate tab component in PA
    Closes https://spryker.atlassian.net/browse/HRZ-766 and https://spryker.atlassian.net/browse/HRZ-1126

--- libs/ui/navigations/tab/src/index.ts
+++ libs/ui/navigations/tab/src/index.ts
@@ -1,3 +1,4 @@
 export * from './tab.component';
+export * from './tab.def';
 export * from './tab.model';
 export * from './tab.styles';
--- libs/ui/navigations/tabs/src/index.ts
+++ libs/ui/navigations/tabs/src/index.ts
@@ -1,3 +1,4 @@
 export * from './tabs.component';
+export * from './tabs.def';
 export * from './tabs.model';
 export * from './tabs.styles';
