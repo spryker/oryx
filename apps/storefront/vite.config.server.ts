f6dab9106b0c8be7630551c855c2fd048afb6908
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2022-10-03 10:45:50 +0300

    fix(sf): inconsistency in test / preview deploy envs between SF and EB (#1350)
    Follow up on investigation - https://spryker.atlassian.net/browse/HRZ-1221

--- apps/storefront/vite.config.server.ts
+++ apps/storefront/vite.config.server.ts
@@ -1,4 +1,4 @@
-import { defineConfig, SSROptions } from 'vite';
+import { defineConfig, loadEnv, SSROptions } from 'vite';
 import checker from 'vite-plugin-checker';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -9,37 +9,53 @@ declare module 'vite' {
   }
 }
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    target: 'esnext',
-    lib: {
-      entry: 'src/entry-server.ts',
-      formats: ['iife'],
-      name: 'storefront',
-    },
-    emptyOutDir: true,
-    outDir: '../../dist/apps/storefront/server',
-    ssr: 'src/entry-server.ts',
-  },
-  ssr: {
-    noExternal: true,
-  },
-  envPrefix: ['FES', 'SCOS'],
-  plugins: [
-    checker({
-      typescript: {
-        tsconfigPath: 'tsconfig.app.json',
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd(), '');
+
+  const mockServerUrl = env.FES_CONTENT_BACKEND_URL;
+  const adjustedMockServerUrl = env.REVIEW_ID
+    ? mockServerUrl.slice(0, 8) +
+      `deploy-preview-${env.REVIEW_ID}--` +
+      mockServerUrl.slice(8)
+    : mockServerUrl;
+
+  console.log('adjustedMockServerUrl: ', adjustedMockServerUrl);
+
+  process.env = Object.assign(process.env, env, {
+    FES_CONTENT_BACKEND_URL: adjustedMockServerUrl,
+  });
+
+  return {
+    build: {
+      target: 'esnext',
+      lib: {
+        entry: 'src/entry-server.ts',
+        formats: ['iife'],
+        name: 'storefront',
       },
-    }),
-    tsconfigPaths({ root: '../../' }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../libs/storefront/public/assets/icons.svg',
-          dest: './assets',
+      emptyOutDir: true,
+      outDir: '../../dist/apps/storefront/server',
+      ssr: 'src/entry-server.ts',
+    },
+    ssr: {
+      noExternal: true,
+    },
+    envPrefix: ['FES', 'SCOS'],
+    plugins: [
+      checker({
+        typescript: {
+          tsconfigPath: 'tsconfig.app.json',
         },
-      ],
-    }),
-  ],
+      }),
+      tsconfigPaths({ root: '../../' }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: '../../libs/storefront/public/assets/icons.svg',
+            dest: './assets',
+          },
+        ],
+      }),
+    ],
+  };
 });
--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig, splitVendorChunkPlugin, SSROptions } from 'vite';
+import {
+  defineConfig,
+  loadEnv,
+  splitVendorChunkPlugin,
+  SSROptions,
+} from 'vite';
 import checker from 'vite-plugin-checker';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -19,32 +24,48 @@ const esbuild =
       }
     : {};
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  esbuild,
-  build: {
-    outDir: '../../dist/apps/storefront/client',
-    emptyOutDir: true,
-  },
-  ssr: {
-    external: ['@lit-labs', 'rxjs'],
-  },
-  envPrefix: ['FES', 'SCOS'],
-  plugins: [
-    splitVendorChunkPlugin(),
-    checker({
-      typescript: {
-        tsconfigPath: 'tsconfig.app.json',
-      },
-    }),
-    tsconfigPaths({ root: '../../' }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../libs/storefront/public/assets/icons.svg',
-          dest: './assets',
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd(), '');
+
+  const mockServerUrl = env.FES_CONTENT_BACKEND_URL;
+  const adjustedMockServerUrl = env.REVIEW_ID
+    ? mockServerUrl.slice(0, 8) +
+      `deploy-preview-${env.REVIEW_ID}--` +
+      mockServerUrl.slice(8)
+    : mockServerUrl;
+
+  console.log('adjustedMockServerUrl: ', adjustedMockServerUrl);
+
+  process.env = Object.assign(process.env, env, {
+    FES_CONTENT_BACKEND_URL: adjustedMockServerUrl,
+  });
+
+  return {
+    esbuild,
+    build: {
+      outDir: '../../dist/apps/storefront/client',
+      emptyOutDir: true,
+    },
+    ssr: {
+      external: ['@lit-labs', 'rxjs'],
+    },
+    envPrefix: ['FES', 'SCOS'],
+    plugins: [
+      splitVendorChunkPlugin(),
+      checker({
+        typescript: {
+          tsconfigPath: 'tsconfig.app.json',
         },
-      ],
-    }),
-  ],
+      }),
+      tsconfigPaths({ root: '../../' }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: '../../libs/storefront/public/assets/icons.svg',
+            dest: './assets',
+          },
+        ],
+      }),
+    ],
+  };
 });
