985bcf0a7af50ef8b5caa9db3806024a2f746a51
Sinucid <Sinucid@users.noreply.github.com>
2023-01-06 18:00:31 +0300

    fix: showing of place order button  (#1720)
    Demo story was dropped due to it was of no use for visual regression
    
    closes to HRZ-1985

--- libs/domain/checkout/place-order/src/place-order.component.ts
+++ libs/domain/checkout/place-order/src/place-order.component.ts
@@ -1,19 +1,41 @@
-import { CheckoutService } from '@spryker-oryx/checkout';
+import { AuthService } from '@spryker-oryx/auth';
+import { CheckoutDataService, CheckoutService } from '@spryker-oryx/checkout';
 import { resolve } from '@spryker-oryx/di';
 import { ComponentMixin } from '@spryker-oryx/experience';
-import { i18n } from '@spryker-oryx/utilities';
+import { asyncValue, i18n } from '@spryker-oryx/utilities';
 import { html, TemplateResult } from 'lit';
+import { combineLatest, map } from 'rxjs';
 
 export class CheckoutPlaceOrderComponent extends ComponentMixin() {
   protected checkout = resolve(CheckoutService);
+
+  protected isGuestCheckout$ = resolve(CheckoutDataService).isGuestCheckout();
+  protected isAuthenticated$ = resolve(AuthService).isAuthenticated();
+  protected showPlaceOrderButton$ = combineLatest([
+    this.isGuestCheckout$,
+    this.isAuthenticated$,
+  ]).pipe(
+    map(
+      ([isGuestCheckout, isAuthenticated]) => isGuestCheckout || isAuthenticated
+    )
+  );
 
   submit(): void {
     this.checkout.placeOrder();
   }
 
   protected override render(): TemplateResult {
-    return html`<oryx-button @click="${this.submit}"
-      ><button>${i18n('checkout.place-order')}</button></oryx-button
-    >`;
+    return html`${asyncValue(
+      this.showPlaceOrderButton$,
+      (showPlaceOrderButton) => {
+        if (!showPlaceOrderButton) {
+          return html``;
+        }
+
+        return html`<oryx-button @click="${this.submit}">
+          <button>${i18n('checkout.place-order')}</button>
+        </oryx-button>`;
+      }
+    )}`;
   }
 }
