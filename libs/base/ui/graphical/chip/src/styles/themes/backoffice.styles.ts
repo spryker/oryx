96095b4a674fffa37e01430b907c7ebcb111e35a
nsilva-spryker <105730125+nsilva-spryker@users.noreply.github.com>
2023-01-25 14:56:16 +0000

    fix: back office chip colors
    After align with Nuwan, there was a need to update our back office style color for chips component like it's currently on figma palette(400)
    
    Closes HRZ-2022

--- libs/base/ui/graphical/chip/src/styles/themes/backoffice.styles.ts
+++ libs/base/ui/graphical/chip/src/styles/themes/backoffice.styles.ts
@@ -1,7 +1,6 @@
 import { ThemeData } from '@spryker-oryx/core';
 import { css, unsafeCSS } from 'lit';
 import { ChipAppearance } from '../../chip.model';
-
 export const chipBackofficeUI: ThemeData = {
   styles: [
     css`
@@ -11,44 +10,44 @@ export const chipBackofficeUI: ThemeData = {
 
       :host([appearance='${unsafeCSS(ChipAppearance.Inactive)}']) {
         --bg: var(--oryx-color-canvas-200);
-        --c: var(--oryx-color-neutral-300);
+        --c: var(--oryx-color-neutral-400);
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Success)}']),
       :host([appearance='${unsafeCSS(ChipAppearance.Online)}']),
       :host([appearance='${unsafeCSS(ChipAppearance.Active)}']) {
         --bg: var(--oryx-color-success-100);
-        --c: var(--oryx-color-success-300);
+        --c: var(--oryx-color-success-400);
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Offline)}']) {
-        --c: var(--oryx-color-neutral-300);
-        --bg: var(--oryx-color-canvas-300);
+        --c: var(--oryx-color-neutral-400);
+        --bg: var(--oryx-color-canvas-400);
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Low)}']) {
         --bg: var(--oryx-chip-low-background-color, #fffbe6);
-        --c: var(--oryx-chip-low-color, #cd8814);
+        --c: var(--oryx-chip-low-color, var(--oryx-color-secondary-400));
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Warning)}']) {
         --bg: var(--oryx-color-warning-100);
-        --c: var(--oryx-color-warning-300);
+        --c: var(--oryx-color-warning-400);
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Highlight)}']) {
         --bg: var(--oryx-color-highlight-100);
-        --c: var(--oryx-color-highlight-300);
+        --c: var(--oryx-color-highlight-400);
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Info)}']) {
         --bg: var(--oryx-color-info-100);
-        --c: var(--oryx-color-info-300);
+        --c: var(--oryx-color-info-400);
       }
 
       :host([appearance='${unsafeCSS(ChipAppearance.Error)}']) {
         --bg: var(--oryx-color-error-100);
-        --c: var(--oryx-color-error-300);
+        --c: var(--oryx-color-error-400);
       }
     `,
   ],
