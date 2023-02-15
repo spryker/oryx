356fedccc93fe41ce14c2881b8ae8a1a1e65ee01
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-02-15 17:09:15 +0100

    stylelint

--- libs/base/ui/graphical/video/src/video.styles.ts
+++ libs/base/ui/graphical/video/src/video.styles.ts
@@ -8,9 +8,8 @@ export const videoStyles = css`
   video,
   object {
     width: 100%;
-    aspect-ratio: var(--aspect-ratio, 16/9);
-    /* height: var(--height); */
     height: 100%;
+    aspect-ratio: var(--aspect-ratio, 16/9);
     object-fit: cover;
   }
 `;
