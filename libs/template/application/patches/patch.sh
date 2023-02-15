63330f2632d3210092c761364fc66d8670103501
aleespryker <alvina.lee@spryker.com>
2023-02-15 13:19:02 +0100

    fix: upgrade lit to ^2.6.0

--- libs/template/application/patches/patch.sh
+++ libs/template/application/patches/patch.sh
@@ -8,11 +8,6 @@ if ! patch -R -s -f --dry-run node_modules/@lit-labs/ssr/lib/util/parse5-utils.j
   patch node_modules/@lit-labs/ssr/lib/util/parse5-utils.js < $LIB/template/application/patches/parse5-utils.patch
 fi
 
-if ! patch -R -s -f --dry-run node_modules/lit-html/development/experimental-hydrate.js < $LIB/template/application/patches/experimental-hydrate.patch; then
-  patch node_modules/lit-html/development/experimental-hydrate.js < $LIB/template/application/patches/experimental-hydrate.patch
-fi
-
-
 cp $LIB/template/application/patches/experimental-hydrate.js node_modules/lit-html/experimental-hydrate.js
 
 exit 0
