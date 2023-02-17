4dd83fd92001e701ef761ffe572e2a2b72ae1a74
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-02-17 10:38:22 +0100

    chore: video UI element (#25)
    Move video element down to the UI library, so that it is directly reusable in other components.

--- libs/domain/content/video/src/video.component.ts
+++ libs/domain/content/video/src/video.component.ts
@@ -1,8 +1,6 @@
 import { ContentMixin, defaultOptions } from '@spryker-oryx/experience';
-import { ssrShim, subscribe } from '@spryker-oryx/utilities';
+import { ssrShim } from '@spryker-oryx/utilities';
 import { html, LitElement, TemplateResult } from 'lit';
-import { ifDefined } from 'lit/directives/if-defined.js';
-import { tap } from 'rxjs';
 import { ContentVideoOptions } from './video.model';
 import { videoStyles } from './video.styles';
 
@@ -19,76 +17,20 @@ export class ContentVideoComponent extends ContentMixin<ContentVideoOptions>(
 ) {
   static styles = [videoStyles];
 
-  @subscribe()
-  protected options$ = this.contentController.getOptions().pipe(
-    tap((options) => {
-      if (options.aspectRatio) {
-        this.style.setProperty('--aspect-ratio', options.aspectRatio);
-      }
-    })
-  );
-
   protected override render(): TemplateResult | void {
     const options = this.componentOptions;
 
     if (!options?.src) return;
 
-    if (this.isYoutube(options.src)) {
-      return html`<object
-        data=${ifDefined(this.resolveYoutubeUrl(options))}
-      ></object>`;
-    } else {
-      return html` <video
-        ?autoplay=${options.autoplay}
-        ?controls=${options.controls}
-        ?loop=${options.loop}
-        ?muted=${options.muted}
-        preload=${ifDefined(options.preload)}
-        ?playsInline=${options.playsInline}
-      >
-        <source src=${ifDefined(options.src)} />
-      </video>`;
-    }
-  }
-
-  protected isYoutube(url: string): boolean {
-    return /youtu.be|youtube/g.test(url);
-  }
-
-  protected resolveYoutubeUrl(
-    options: ContentVideoOptions
-  ): string | undefined {
-    if (!options.src) {
-      return;
-    }
-
-    const resolveId = (url: string): string | null | undefined => {
-      if (url.includes('youtu.be')) {
-        return url.split('/').pop();
-      } else if (url.includes('youtube.com')) {
-        if (url.includes('/embed/')) {
-          return url.split('/embed/').pop()?.split('?')[0];
-        }
-        return new URL(url).searchParams.get('v');
-      }
-      return;
-    };
-
-    const allowedUrlQueryParams: (keyof ContentVideoOptions)[] = [
-      'loop',
-      'autoplay',
-      'controls',
-      'muted',
-    ];
-
-    const params = allowedUrlQueryParams
-      .filter((key) => !!options[key])
-      .map((key) => `&${key}=${options[key]}`);
-
-    const separator = params.length ? '?' : '';
-
-    return `https://www.youtube.com/embed/${resolveId(
-      options.src
-    )}${separator}${params.join('')}`;
+    return html`<oryx-video
+      .url=${options.src}
+      ?autoplay=${options.autoplay}
+      ?controls=${options.controls}
+      ?loop=${options.loop}
+      ?muted=${options.muted}
+      ?playsInline=${options.playsInline}
+      .preload=${options.preload}
+      .aspectRatio=${options.aspectRatio}
+    ></oryx-video>`;
   }
 }
