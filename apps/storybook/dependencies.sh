d2d36a6e9299f75e3efb4edeeaf9616afdaf8f84
Eugene Galitsky <eugen.galitsky@spryker.com>
2023-02-17 11:00:01 +0200

    fix: deps

--- apps/storybook/dependencies.sh
+++ apps/storybook/dependencies.sh
@@ -1,1 +1,1 @@
-DEPENDENCIES="apps/storybook/* libs/base/ui/* libs/domain/content/*"
+DEPENDENCIES="apps/storybook/* libs/base/di/* libs/base/form/* libs/base/ui/* libs/base/utilities/* libs/domain/auth/* libs/domain/cart/* libs/domain/checkout/* libs/domain/content/* libs/domain/order/* libs/domain/product/* libs/domain/search/* libs/domain/site/* libs/domain/user/* libs/platform/core/* libs/platform/experience/* libs/platform/i18n/* libs/platform/router/* libs/template/application/* libs/template/presets/* libs/template/themes/*"
