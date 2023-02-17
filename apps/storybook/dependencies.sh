098520066684e10b8c11ee8005ddb1300cd3b16d
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-02-17 11:31:05 +0100

    deps

--- apps/storybook/dependencies.sh
+++ apps/storybook/dependencies.sh
@@ -1,1 +1,1 @@
-DEPENDENCIES="apps/storybook/* libs/base/ui/* libs/base/utilities/* libs/domain/content/* libs/platform/experience/*"
+DEPENDENCIES="apps/storybook/* libs/base/di/* libs/base/form/* libs/base/ui/* libs/base/utilities/* libs/domain/auth/* libs/domain/cart/* libs/domain/checkout/* libs/domain/content/* libs/domain/order/* libs/domain/product/* libs/domain/search/* libs/domain/site/* libs/domain/user/* libs/platform/core/* libs/platform/experience/* libs/platform/i18n/* libs/platform/router/* libs/template/application/* libs/template/presets/* libs/template/themes/*"
