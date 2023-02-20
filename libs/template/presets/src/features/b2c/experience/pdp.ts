8e737abcd32c5aad70ce1ab193098d0aa18bd725
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-02-20 15:41:33 +0100

    add pdp

---
+++ libs/template/presets/src/features/b2c/experience/pdp.ts
@@ -0,0 +1,97 @@
+import { Component } from '@spryker-oryx/experience';
+
+export const ProductPage: Component<any> = {
+  type: 'Page',
+  meta: {
+    title: 'Product Page',
+    route: '/product/:sku',
+  },
+  components: [
+    {
+      type: 'experience-composition',
+      options: {
+        data: {
+          rules: [
+            {
+              container: true,
+              layout: 'two-column',
+            },
+          ],
+        },
+      },
+      components: [
+        {
+          type: 'experience-composition',
+          options: {
+            data: {
+              rules: [
+                {
+                  padding: '30px 0',
+                },
+              ],
+            },
+          },
+          components: [
+            { type: 'oryx-product-labels' },
+            { type: 'product-images' },
+            { type: 'oryx-product-description' },
+            { type: 'oryx-product-attributes' },
+          ],
+        },
+        {
+          type: 'experience-composition',
+          options: {
+            data: {
+              rules: [
+                {
+                  sticky: true,
+                  top: '110px',
+                  maxWidth: true,
+                  layout: 'list',
+                  gap: '10px',
+                  padding: '30px 0',
+                },
+              ],
+            },
+          },
+          components: [
+            {
+              type: 'oryx-product-title',
+              options: { data: { tag: 'h1' } },
+            },
+            { type: 'oryx-product-average-rating' },
+            { type: 'oryx-product-id' },
+            {
+              type: 'oryx-product-labels',
+              options: {
+                data: {
+                  included: 'sale %',
+                  invert: true,
+                },
+              },
+            },
+            { type: 'oryx-product-price' },
+            { type: 'oryx-cart-add' },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'oryx-product-list',
+      options: {
+        data: {
+          rules: [
+            {
+              layout: 'carousel',
+              container: true,
+              gap: '20',
+              padding: '20 0 0',
+            },
+          ],
+          category: '10',
+          sort: 'rating',
+        },
+      },
+    },
+  ],
+};
--- libs/template/presets/src/features/b2c/experience/static-experience.ts
+++ libs/template/presets/src/features/b2c/experience/static-experience.ts
@@ -3,9 +3,15 @@ import { provideExperienceData } from '@spryker-oryx/experience';
 import { FooterTemplate } from './footer';
 import { HeaderTemplate } from './header';
 import { HomePage } from './home-page';
+import { ProductPage } from './pdp';
 
 export const StaticExperienceFeature: AppFeature = {
   providers: [
-    provideExperienceData([HeaderTemplate, FooterTemplate, HomePage]),
+    provideExperienceData([
+      HeaderTemplate,
+      FooterTemplate,
+      HomePage,
+      ProductPage,
+    ]),
   ],
 };
