e9b73f389e1230b8509ecaaee6007d262a5bfe47
aleespryker <alvina.lee@spryker.com>
2023-02-20 11:53:28 +0100

    fix: use isServer detection tinstead of globalThis

--- libs/base/ui/overlays/drawer/src/component.ts
+++ libs/base/ui/overlays/drawer/src/component.ts
@@ -1,10 +1,11 @@
 import { componentDef } from '@spryker-oryx/core';
+import { isServer } from 'lit';
 import { TAG_NAME } from './tag';
 
 export const drawerComponent = componentDef({
   name: TAG_NAME,
   impl: () =>
-    globalThis.HTMLDialogElement
+    !isServer && window.HTMLDialogElement
       ? import('./drawer.component').then((m) => m.DrawerComponent)
       : import('./no-dialog-support/drawer.component').then(
           (m) => m.NDSDrawerComponent
--- libs/base/ui/overlays/modal/src/component.ts
+++ libs/base/ui/overlays/modal/src/component.ts
@@ -1,9 +1,10 @@
 import { componentDef } from '@spryker-oryx/core';
+import { isServer } from 'lit';
 
 export const modalComponent = componentDef({
   name: 'oryx-modal',
   impl: () =>
-    globalThis.HTMLDialogElement
+    !isServer && window.HTMLDialogElement
       ? import('./modal.component').then((m) => m.ModalComponent)
       : import('./no-dialog-support/modal.component').then(
           (m) => m.NDSModalComponent
