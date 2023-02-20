300c1513d1e216e70721bcd580d84f2779c67c76
aleespryker <alvina.lee@spryker.com>
2023-02-20 17:03:33 +0100

    fix: revert globalThis changes for now

--- libs/base/ui/overlays/drawer/src/component.ts
+++ libs/base/ui/overlays/drawer/src/component.ts
@@ -1,11 +1,10 @@
 import { componentDef } from '@spryker-oryx/core';
-import { isServer } from 'lit';
 import { TAG_NAME } from './tag';
 
 export const drawerComponent = componentDef({
   name: TAG_NAME,
   impl: () =>
-    !isServer && window.HTMLDialogElement
+    globalThis.HTMLDialogElement
       ? import('./drawer.component').then((m) => m.DrawerComponent)
       : import('./no-dialog-support/drawer.component').then(
           (m) => m.NDSDrawerComponent
--- libs/base/ui/overlays/modal/src/component.ts
+++ libs/base/ui/overlays/modal/src/component.ts
@@ -1,10 +1,9 @@
 import { componentDef } from '@spryker-oryx/core';
-import { isServer } from 'lit';
 
 export const modalComponent = componentDef({
   name: 'oryx-modal',
   impl: () =>
-    !isServer && window.HTMLDialogElement
+    globalThis.HTMLDialogElement
       ? import('./modal.component').then((m) => m.ModalComponent)
       : import('./no-dialog-support/modal.component').then(
           (m) => m.NDSModalComponent
