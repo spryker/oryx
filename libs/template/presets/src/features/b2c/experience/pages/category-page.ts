7e76036cc7fdd310e122fe1b3567f4ce70482dae
Stan <dunqan@w9.pl>
2023-02-21 08:23:13 +0100

    feat: support for static EB data  (#44)
    Initial support for static structure data.

---
+++ libs/template/presets/src/features/b2c/experience/pages/category-page.ts
@@ -0,0 +1,83 @@
+import { StaticComponent } from '@spryker-oryx/experience';
+
+export const CategoryPage: StaticComponent = {
+  type: 'Page',
+  meta: { title: 'Category Page', route: '/category/:id' },
+  components: [
+    {
+      type: 'experience-composition',
+      components: [
+        {
+          type: 'search-facet-navigation',
+          options: {
+            data: {
+              rules: [
+                {
+                  height: 'calc(100vh - 78px)',
+                  sticky: true,
+                  top: '78px',
+                  span: '3',
+                  margin: '30px 0',
+                },
+              ],
+            },
+          },
+        },
+        {
+          type: 'experience-composition',
+          components: [
+            {
+              type: 'experience-composition',
+              components: [
+                {
+                  type: 'search-product-sort',
+                  options: { data: { rules: [{ margin: '0 0 0 auto' }] } },
+                },
+              ],
+              options: {
+                data: { rules: [{ layout: 'flex', maxWidth: true }] },
+              },
+            },
+            {
+              id: 'category-list',
+              type: 'oryx-product-list',
+              options: {
+                data: {
+                  rules: [
+                    {
+                      layout: 'grid',
+                      columnCount: '3',
+                      span: '3',
+                      gap: '30',
+                      maxWidth: false,
+                    },
+                  ],
+                },
+              },
+            },
+            {
+              type: 'experience-composition',
+              components: [{ type: 'oryx-search-pagination' }],
+              options: { data: { rules: [{ margin: '0 auto' }] } },
+            },
+          ],
+          options: {
+            data: {
+              rules: [
+                {
+                  maxWidth: true,
+                  layout: 'flex',
+                  padding: '30px 0',
+                  gap: '20',
+                },
+              ],
+            },
+          },
+        },
+      ],
+      options: {
+        data: { rules: [{ container: true, layout: 'column', gap: '30' }] },
+      },
+    },
+  ],
+};
---
+++ libs/template/presets/src/features/b2c/experience/pages/contact-page.ts
@@ -0,0 +1,7 @@
+import { StaticComponent } from '@spryker-oryx/experience';
+
+export const ContactPage: StaticComponent = {
+  type: 'Page',
+  meta: { title: 'Contact Page', route: '/contact' },
+  components: [],
+};
