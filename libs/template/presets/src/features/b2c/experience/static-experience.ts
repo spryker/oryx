51fc90de5bc58ed7bba3fd60f3521deb6d5a7769
Eugene Galitsky <eugen.galitsky@spryker.com>
2023-02-21 14:08:38 +0200

    fix: pages import for b2c

--- libs/template/presets/src/features/b2c/experience/static-experience.ts
+++ libs/template/presets/src/features/b2c/experience/static-experience.ts
@@ -1,9 +1,8 @@
 import { AppFeature } from '@spryker-oryx/core';
 import { provideExperienceData } from '@spryker-oryx/experience';
-import { pages } from '.';
 import { FooterTemplate } from './footer';
 import { HeaderTemplate } from './header';
-export * as pages from './pages';
+import * as pages from './pages';
 
 export const StaticExperienceFeature: AppFeature = {
   providers: [
