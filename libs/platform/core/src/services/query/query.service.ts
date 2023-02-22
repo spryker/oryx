cdcdcd696de4bf7524fedeaf6ec0cbff5b7f1f4b
dunqan <dunqan@w9.pl>
2023-02-22 09:34:56 +0100

    query service definition

---
+++ libs/platform/core/src/services/query/query.service.ts
@@ -0,0 +1,43 @@
+import { Observable } from 'rxjs';
+import { Command, CommandOptions } from './command';
+import { Query, QueryOptions } from './query';
+
+export const QueryService = 'oryx.QueryService';
+
+export const QUERY_TOKEN = 'oryx.QueryToken*';
+export const EFFECT_TOKEN = 'oryx.EffectToken*';
+export const COMMAND_TOKEN = 'oryx.CommandToken*';
+
+export interface QueryService {
+  createQuery<ValueType, Qualifier extends object | undefined = undefined>(
+    options: QueryOptions<ValueType, Qualifier>
+  ): Query<ValueType, Qualifier>;
+
+  createCommand<ResultType, Qualifier extends object | undefined = undefined>(
+    options: CommandOptions<ResultType, Qualifier>
+  ): Command<ResultType, Qualifier>;
+
+  createEffect(effect: any): void;
+
+  getQuery<
+    ValueType = unknown,
+    Qualifier extends object | undefined = undefined
+  >(
+    id: string
+  ): Query<ValueType, Qualifier> | undefined;
+  getCommand<
+    ResultType = unknown,
+    Qualifier extends object | undefined = undefined
+  >(
+    id: string
+  ): Command<ResultType, Qualifier> | undefined;
+
+  emit(stateEvent: any): void;
+  getEvents(eventType?: string): Observable<any>;
+}
+
+declare global {
+  interface InjectionTokensContractMap {
+    [QueryService]: QueryService;
+  }
+}
