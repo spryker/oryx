622964dd0388dd32e799c0b64f4e0d22686a5987
dunqan <dunqan@w9.pl>
2023-02-22 09:36:15 +0100

    query service implementation

---
+++ libs/platform/core/src/services/query/default-query.service.ts
@@ -0,0 +1,61 @@
+import { inject, INJECTOR } from '@spryker-oryx/di';
+import { Command } from './command';
+import { CoreQueryService } from './core';
+import { Query } from './query';
+import {
+  COMMAND_TOKEN,
+  EFFECT_TOKEN,
+  QueryService,
+  QUERY_TOKEN,
+} from './query.service';
+
+export class DefaultQueryService
+  extends CoreQueryService
+  implements QueryService
+{
+  constructor(
+    effects = inject(EFFECT_TOKEN, []),
+    protected injector = inject(INJECTOR)
+  ) {
+    super();
+    this.initEffects(effects);
+  }
+
+  protected initEffects(effects: any[]): void {
+    effects.forEach((effect) => this.createEffect(effect));
+  }
+
+  override getQuery<
+    ValueType = unknown,
+    Qualifier extends object | undefined = undefined
+  >(id: string): Query<ValueType, Qualifier> | undefined {
+    const query = super.getQuery<ValueType, Qualifier>(id);
+    if (query) {
+      return query;
+    }
+
+    const providedQuery = this.injector.inject(`${QUERY_TOKEN}${id}`, null);
+    if (providedQuery) {
+      return this.createQuery<ValueType, Qualifier>(providedQuery);
+    }
+
+    return undefined;
+  }
+
+  override getCommand<
+    ResultType = unknown,
+    Qualifier extends object | undefined = undefined
+  >(id: string): Command<ResultType, Qualifier> | undefined {
+    const command = super.getCommand<ResultType, Qualifier>(id);
+    if (command) {
+      return command;
+    }
+
+    const providedCommand = this.injector.inject(`${COMMAND_TOKEN}${id}`, null);
+    if (providedCommand) {
+      return this.createCommand<ResultType, Qualifier>(providedCommand);
+    }
+
+    return undefined;
+  }
+}
---
+++ libs/platform/core/src/services/query/index.ts
@@ -0,0 +1,6 @@
+export * from './query.service';
+export * from './default-query.service';
+export * from './query';
+export * from './command';
+export * from './utils';
+
