d1689485fb16c5e49ee7032139309528181445fb
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-02-22 08:35:50 +0100

    fix test

--- libs/base/form/src/renderers/default-form.renderer.spec.ts
+++ libs/base/form/src/renderers/default-form.renderer.spec.ts
@@ -1,7 +1,7 @@
 import { fixture, html } from '@open-wc/testing-helpers';
 import { createInjector, destroyInjector } from '@spryker-oryx/di';
 import { LitElement, TemplateResult } from 'lit';
-import { customElement } from 'lit/decorators';
+import { customElement } from 'lit/decorators.js';
 import { FormFieldDefinition, FormFieldType } from '../models';
 import { DefaultFormRenderer } from './default-form.renderer';
 import { FormRenderer } from './form.renderer';
