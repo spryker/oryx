88d9c8c0ee6fb4131a0fe3853074fbbf39617c9d
dunqan <dunqan@w9.pl>
2023-02-22 14:44:08 +0100

    product service

--- libs/domain/product/src/services/default-product.service.ts
+++ libs/domain/product/src/services/default-product.service.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { HttpErrorResponse } from '@spryker-oryx/core';
+import { createQuery, HttpErrorResponse, QueryState } from '@spryker-oryx/core';
 import { inject } from '@spryker-oryx/di';
 import { NullableGeneric } from '@spryker-oryx/utilities';
-import { distinctUntilChanged, Observable, ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, map, Observable, ReplaySubject } from 'rxjs';
 import { Product, ProductQualifier } from '../models';
 import { ProductAdapter } from './adapter/product.adapter';
 import { ProductService } from './product.service';
@@ -15,48 +15,24 @@ export interface ProductData {
 export class DefaultProductService implements ProductService {
   protected products = new Map<string, ProductData>();
 
+  protected productQuery = createQuery({
+    loader: (q: ProductQualifier) => this.adapter.get(q),
+  });
+
   constructor(protected adapter = inject(ProductAdapter)) {}
 
-  get(qualifier: ProductQualifier): Observable<NullableGeneric<Product>> {
-    return this.getProductData(qualifier).value;
+  get(qualifier: ProductQualifier): Observable<Product | undefined> {
+    return this.productQuery.get(qualifier);
   }
 
-  getError(
-    qualifier: ProductQualifier
-  ): Observable<NullableGeneric<HttpErrorResponse>> {
-    return this.getProductData(qualifier)!.error.pipe(distinctUntilChanged());
+  getState(qualifier: ProductQualifier): Observable<QueryState<Product>> {
+    return this.productQuery.getState(qualifier);
   }
 
-  protected getProductData(qualifier: ProductQualifier): ProductData {
-    const key = this.adapter.getKey(qualifier);
-
-    if (!this.products.has(key)) {
-      this.products.set(key, {
-        value: new ReplaySubject<NullableGeneric<Product>>(1),
-        error: new ReplaySubject<NullableGeneric<HttpErrorResponse>>(1),
-      });
-      this.reload(qualifier, key);
-    }
-
-    return this.products.get(key)!;
-  }
-
-  protected reload(qualifier: ProductQualifier, key?: string): void {
-    if (key === undefined) {
-      key = this.adapter.getKey(qualifier);
-    }
-
-    const productData = this.products.get(key!)!;
-
-    this.adapter.get(qualifier).subscribe({
-      next: (product) => {
-        productData.value.next(product);
-        productData.error.next(null);
-      },
-      error: (error: HttpErrorResponse) => {
-        productData.error.next(error);
-        productData.value.next(null);
-      },
-    });
+  getError(qualifier: ProductQualifier): Observable<Error | false> {
+    return this.productQuery.getState(qualifier).pipe(
+      map((state) => state.error),
+      distinctUntilChanged()
+    );
   }
 }
