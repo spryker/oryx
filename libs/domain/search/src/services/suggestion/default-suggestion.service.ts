f6c0e2a271764e4cf6d59e317fac88c161d9ae8f
dunqan <dunqan@w9.pl>
2023-02-24 07:09:54 +0100

    update suggestions service with query

--- libs/domain/search/src/services/suggestion/default-suggestion.service.ts
+++ libs/domain/search/src/services/suggestion/default-suggestion.service.ts
@@ -1,59 +1,34 @@
-import { HttpErrorResponse } from '@spryker-oryx/core';
+import { createQuery, QueryService, QueryState } from '@spryker-oryx/core';
 import { inject } from '@spryker-oryx/di';
-import { NullableGeneric } from '@spryker-oryx/utilities';
-import { Observable, ReplaySubject } from 'rxjs';
+import { ProductQuery } from '@spryker-oryx/product';
+import { Observable } from 'rxjs';
 import { Suggestion, SuggestionQualifier } from '../../models';
 import { SuggestionAdapter } from '../adapter';
 import { SuggestionService } from './suggestion.service';
 
-export interface SuggestionData {
-  value: ReplaySubject<NullableGeneric<Suggestion>>;
-  error: ReplaySubject<NullableGeneric<HttpErrorResponse>>;
-}
-
 export class DefaultSuggestionService implements SuggestionService {
-  protected suggestions = new Map<string, SuggestionData>();
+  protected suggestionsQuery = createQuery<Suggestion, SuggestionQualifier>({
+    loader: (qualifier) => this.adapter.get(qualifier),
+    onLoad: [
+      ({ data }) =>
+        data?.products.forEach((product) =>
+          this.query
+            .getQuery(ProductQuery)
+            ?.set({ data: product, qualifier: { sku: product.sku } })
+        ),
+    ],
+  });
 
-  constructor(protected adapter = inject(SuggestionAdapter)) {}
+  constructor(
+    protected adapter = inject(SuggestionAdapter),
+    protected query = inject(QueryService)
+  ) {}
 
-  get(qualifier: SuggestionQualifier): Observable<NullableGeneric<Suggestion>> {
-    return this.getData(qualifier).value;
+  get(qualifier: SuggestionQualifier): Observable<Suggestion | undefined> {
+    return this.suggestionsQuery.get(qualifier);
   }
 
-  getError(
-    qualifier: SuggestionQualifier
-  ): Observable<NullableGeneric<HttpErrorResponse>> {
-    return this.getData(qualifier).error;
-  }
-
-  protected getData(qualifier: SuggestionQualifier): SuggestionData {
-    const key = this.adapter.getKey(qualifier);
-
-    if (!this.suggestions.has(key)) {
-      this.suggestions.set(key, {
-        value: new ReplaySubject<NullableGeneric<Suggestion>>(1),
-        error: new ReplaySubject<NullableGeneric<HttpErrorResponse>>(1),
-      });
-      this.reload(qualifier, key);
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return this.suggestions.get(key)!;
-  }
-
-  protected reload(qualifier: SuggestionQualifier, key?: string): void {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const data = this.suggestions.get(key!)!;
-
-    this.adapter.get(qualifier).subscribe({
-      next: (product) => {
-        data.value.next(product);
-        data.error.next(null);
-      },
-      error: (error: HttpErrorResponse) => {
-        data.error.next(error);
-        data.value.next(null);
-      },
-    });
+  getState(qualifier: SuggestionQualifier): Observable<QueryState<Suggestion>> {
+    return this.suggestionsQuery.getState(qualifier);
   }
 }
