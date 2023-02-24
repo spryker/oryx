becb48014f05020fa1fcec4b45a0f1cde1bf606f
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-02-24 17:15:31 +0100

    avoid rxjs

--- libs/domain/user/address-list/src/address-list.component.ts
+++ libs/domain/user/address-list/src/address-list.component.ts
@@ -1,5 +1,5 @@
 import { resolve } from '@spryker-oryx/di';
-import { ContentMixin } from '@spryker-oryx/experience';
+import { ContentMixin, defaultOptions } from '@spryker-oryx/experience';
 import { Address, AddressService } from '@spryker-oryx/user';
 import {
   AddressDefaults,
@@ -9,48 +9,33 @@ import {
   asyncState,
   hydratable,
   i18n,
-  subscribe,
   valueType,
 } from '@spryker-oryx/utilities';
-import { html, LitElement, TemplateResult } from 'lit';
+import { html, LitElement, PropertyValues, TemplateResult } from 'lit';
 import { when } from 'lit-html/directives/when.js';
 import { state } from 'lit/decorators.js';
 import { keyed } from 'lit/directives/keyed.js';
 import { repeat } from 'lit/directives/repeat.js';
-import { tap } from 'rxjs';
 import { SELECT_EVENT } from './address-list.model';
 import { styles } from './address-list.styles';
 
+@defaultOptions({ addressDefaults: AddressDefaults.All })
 @hydratable('window:load')
 export class AddressListComponent extends ContentMixin<AddressListItemOptions>(
   LitElement
 ) {
   static styles = styles;
 
-  protected addressService = resolve(AddressService);
-
   @asyncState()
-  protected addresses = valueType(this.addressService.getAddresses());
+  protected addresses = valueType(resolve(AddressService).getAddresses());
 
   @state()
   protected selectedAddressId?: string;
 
-  @subscribe()
-  protected addressesSubscription = this.addressService.getAddresses().pipe(
-    tap((addresses) => {
-      if (!this.componentOptions?.selectable) return;
-
-      if (
-        !this.selectedAddressId ||
-        !addresses?.find((address) => address.id == this.selectedAddressId)
-      ) {
-        const defaultAddress = addresses?.find((address) =>
-          this.isDefault(address)
-        );
-        this.selectedAddressId = defaultAddress?.id ?? addresses?.[0]?.id;
-      }
-    })
-  );
+  protected update(changedProperties: PropertyValues): void {
+    this.select();
+    super.update(changedProperties);
+  }
 
   protected override render(): TemplateResult {
     if (!this.addresses?.length) {
@@ -67,9 +52,9 @@ export class AddressListComponent extends ContentMixin<AddressListItemOptions>(
           this.addresses,
           (address) => address.id,
           (address) => {
-            const isSelected = this.selectedAddressId === address.id;
-
-            return html`<oryx-tile ?selected=${isSelected}>
+            return html`<oryx-tile
+              ?selected=${this.selectedAddressId === address.id}
+            >
               <oryx-address-list-item
                 .addressId=${address.id}
                 .options=${this.componentOptions}
@@ -80,7 +65,7 @@ export class AddressListComponent extends ContentMixin<AddressListItemOptions>(
                     name="address"
                     type="radio"
                     value="${address.id as string}"
-                    ?checked=${isSelected}
+                    ?checked=${this.selectedAddressId === address.id}
                     @change=${this.onChange}
                   />`
                 )}
@@ -92,13 +77,23 @@ export class AddressListComponent extends ContentMixin<AddressListItemOptions>(
     `;
   }
 
+  protected select(): void {
+    const hasSelectedAddress = this.addresses?.find(
+      (address) => address.id === this.selectedAddressId
+    );
+    if (!hasSelectedAddress) {
+      this.selectedAddressId =
+        this.addresses?.find((a) => this.isDefault(a))?.id ??
+        this.addresses?.[0].id;
+    }
+  }
+
   protected onChange(ev: Event): void {
     const el = ev.target as HTMLInputElement;
 
     const address = this.addresses?.find((address) => address.id === el.value);
     if (address) {
       this.selectedAddressId = address.id;
-      this.requestUpdate('selectedAddressId');
       this.dispatchEvent(
         new CustomEvent(SELECT_EVENT, {
           bubbles: true,
@@ -109,10 +104,17 @@ export class AddressListComponent extends ContentMixin<AddressListItemOptions>(
     }
   }
 
-  protected createAddressesKey(): string | void {
-    return this.addresses?.map(({ id }) => id).join('-');
-  }
-
+  /**
+   * Indicates whether the given address is the default address. This is influenced by the
+   * address data in combination with the address default mode:
+   * - All
+   * - Billing
+   * - Shipping (aka delivery)
+   *
+   * If the address is a defaultBilling address and teh default mode is `All` or `Billing`,
+   * this method will return true. Similarly, when the address is marked as `isDefaultShipping`
+   * and the mode is `All` or `Shipping`.
+   */
   protected isDefault(address: Address): boolean {
     const isDefault =
       this.componentOptions?.addressDefaults === AddressDefaults.All;
@@ -128,7 +130,7 @@ export class AddressListComponent extends ContentMixin<AddressListItemOptions>(
     );
   }
 
-  protected isFirstInList(addressId?: string): boolean {
-    return this.addresses?.[0]?.id === addressId;
+  protected createAddressesKey(): string | void {
+    return this.addresses?.map(({ id }) => id).join('-');
   }
 }
