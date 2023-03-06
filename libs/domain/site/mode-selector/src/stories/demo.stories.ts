1464e6e3db3fc866bab091a1dfb4cac1d4ed0bd3
Aleksey Belan <alexey.belan@spryker.com>
2023-03-06 22:28:35 +0200

    story

---
+++ libs/domain/site/mode-selector/src/stories/demo.stories.ts
@@ -0,0 +1,17 @@
+import { Meta, Story } from '@storybook/web-components';
+import { html, TemplateResult } from 'lit';
+import { storybookPrefix } from '../../../.constants';
+
+export default {
+  title: `${storybookPrefix}/Mode Selector`,
+} as Meta;
+
+const Template: Story = (): TemplateResult => {
+  return html`
+    <oryx-site-mode-selector
+      @oryx.toggle-mode=${(e: Event) => console.log(`Toggle Mode Event ${e}`)}
+    ></oryx-site-mode-selector>
+  `;
+};
+
+export const Demo = Template.bind({});
