1cf018be4757a4a8d12028da650da6f87afb51e7
dunqan <dunqan@w9.pl>
2023-03-07 18:49:34 +0100

    list component

--- libs/domain/product/list/src/list.component.ts
+++ libs/domain/product/list/src/list.component.ts
@@ -1,20 +1,32 @@
 import { resolve } from '@spryker-oryx/di';
-import { ContentController, StyleRuleSet } from '@spryker-oryx/experience';
+import {
+  CompositionProperties,
+  ContentController,
+  ContentMixin,
+  StyleRuleSet,
+} from '@spryker-oryx/experience';
 import {
   Product,
-  ProductComponentMixin,
   ProductListPageService,
   ProductListQualifier,
   ProductListService,
+  ProductMixin,
 } from '@spryker-oryx/product';
-import { asyncValue, ssrShim, subscribe } from '@spryker-oryx/utilities';
-import { html, TemplateResult } from 'lit';
-import { property } from 'lit/decorators.js';
+import {
+  asyncState,
+  ssrShim,
+  subscribe,
+  valueType,
+} from '@spryker-oryx/utilities';
+import { html, LitElement, TemplateResult } from 'lit';
+import { property, state } from 'lit/decorators.js';
 import { map, switchMap, tap } from 'rxjs';
 import { baseStyles } from './list.styles';
 
 @ssrShim('style')
-export class ProductListComponent extends ProductComponentMixin<ProductListQualifier>() {
+export class ProductListComponent extends ProductMixin(
+  ContentMixin<ProductListQualifier>(LitElement)
+) {
   static styles = [baseStyles];
 
   protected productListService = resolve(ProductListService);
@@ -23,22 +35,29 @@ export class ProductListComponent extends ProductComponentMixin<ProductListQuali
   @property({ reflect: true, type: Boolean, attribute: 'has-layout' })
   hasLayout?: boolean;
 
+  @state()
+  layoutRules?: StyleRuleSet[];
+
   @subscribe()
   protected options$ = new ContentController(this).getOptions().pipe(
     tap((options) => {
       this.hasLayout = !!(options as any as { rules: StyleRuleSet[] })
         ?.rules?.[0]?.layout;
+      this.layoutRules = (options as CompositionProperties)?.rules;
     })
   );
 
-  protected products$ = this.options$.pipe(
-    switchMap((options) => {
-      const searchParams = this.productListService.getSearchParams(options);
-      return this.hasOptions(searchParams)
-        ? this.productListService.get(searchParams)
-        : this.productListPageService.get();
-    }),
-    map((list) => list?.products ?? [])
+  @asyncState()
+  protected products = valueType(
+    this.options$.pipe(
+      switchMap((options) => {
+        const searchParams = this.productListService.getSearchParams(options);
+        return this.hasOptions(searchParams)
+          ? this.productListService.get(searchParams)
+          : this.productListPageService.get();
+      }),
+      map((list) => list?.products ?? [])
+    )
   );
 
   protected hasOptions(options: ProductListQualifier): boolean {
@@ -50,18 +69,14 @@ export class ProductListComponent extends ProductComponentMixin<ProductListQuali
   protected override render(): TemplateResult {
     return this.hasLayout
       ? html`
-          <oryx-layout .uid=${this.uid}>
-            ${asyncValue(this.products$, (products) =>
-              this.renderProducts(products)
-            )}
+          <oryx-layout .rules=${this.layoutRules}>
+            ${this.renderProducts(this.products)}
           </oryx-layout>
         `
-      : html` ${asyncValue(this.products$, (products) =>
-          this.renderProducts(products)
-        )}`;
+      : html` ${this.renderProducts(this.products)}`;
   }
 
-  protected renderProducts(products: Product[]): TemplateResult {
+  protected renderProducts(products: Product[] = []): TemplateResult {
     return html`${products.map(
       (p) => html`<oryx-product-card .sku=${p.sku}></oryx-product-card>`
     )}`;
