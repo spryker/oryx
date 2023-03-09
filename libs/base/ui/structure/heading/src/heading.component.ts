7657e26561828013ebd3cdbb2a93d32fd822467f
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-03-09 16:33:55 +0100

    fix chromatics

--- libs/base/ui/structure/heading/src/heading.component.ts
+++ libs/base/ui/structure/heading/src/heading.component.ts
@@ -2,7 +2,7 @@ import { ssrShim } from '@spryker-oryx/utilities';
 import { html, LitElement, TemplateResult } from 'lit';
 import { property } from 'lit/decorators.js';
 import { HeadingAttributes, HeadingTag } from './heading.model';
-import { headlineStyles } from './styles/base.styles';
+import { headlineStyles } from './heading.styles';
 
 @ssrShim('style')
 export class HeadingComponent extends LitElement implements HeadingAttributes {
