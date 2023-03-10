c9a0a989c3c71865e3991f7b325b04d29b33f936
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-03-10 14:13:17 +0100

    stylelint

--- libs/base/ui/structure/tile/src/tile.styles.ts
+++ libs/base/ui/structure/tile/src/tile.styles.ts
@@ -2,6 +2,7 @@ import { css } from 'lit';
 
 export const tileStyles = css`
   ::slotted(*) {
+    display: block;
     padding: var(--oryx-tile-padding, 20px);
     outline: none;
     border: var(--oryx-border-thin) solid var(--oryx-color-canvas-500);
@@ -13,10 +14,6 @@ export const tileStyles = css`
     );
     background-color: var(--oryx-color-canvas-100);
     transition: var(--oryx-transition-time);
-  }
-
-  ::slotted(*) {
-    display: block;
   }
 
   ::slotted(*:active) {
