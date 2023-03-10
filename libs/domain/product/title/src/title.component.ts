6c44edd62c974a90ba36bd8a3853c23db898a152
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-03-10 12:27:10 +0100

    use heading options, introduce schema

--- libs/domain/product/title/src/title.component.ts
+++ libs/domain/product/title/src/title.component.ts
@@ -3,7 +3,6 @@ import { ProductMixin } from '@spryker-oryx/product';
 import { SemanticLinkType } from '@spryker-oryx/site';
 import { hydratable } from '@spryker-oryx/utilities';
 import { LitElement, TemplateResult } from 'lit';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import { html } from 'lit/static-html.js';
 import { ProductTitleOptions } from './title.model';
 import { styles } from './title.styles';
@@ -14,23 +13,24 @@ export class ProductTitleComponent extends ProductMixin(
 ) {
   static styles = styles;
 
-  protected override render(): TemplateResult {
-    const options = this.componentOptions;
+  protected override render(): TemplateResult | void {
+    const options = this.componentOptions ?? {};
+    const { tag, as, asLg, asMd, asSm, maxLines } = this.componentOptions ?? {};
 
     return html`<oryx-heading
-      tag=${ifDefined(options?.tag)}
-      maxLines=${ifDefined(options?.maxLines)}
+      .tag=${tag}
+      .maxLines=${maxLines}
+      .as=${as}
+      .asLg=${asLg}
+      .asMd=${asMd}
+      .asSm=${asSm}
     >
       ${options?.link ? this.renderLink() : html`${this.product?.name}`}
     </oryx-heading>`;
   }
 
   protected renderLink(): TemplateResult {
-    const options = {
-      type: SemanticLinkType.Product,
-      id: this.product?.sku,
-      multiLine: true,
-    };
+    const options = { type: SemanticLinkType.Product, id: this.product?.sku };
 
     return html`<oryx-content-link .options=${options}>
       ${this.product?.name}
