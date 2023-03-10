b43d78f4ca215d2f45e3c102e30f5c1a4ab4778c
dunqan <dunqan@w9.pl>
2023-03-10 10:22:43 +0100

    ability to set active store

--- libs/domain/site/src/services/store/default-store.service.ts
+++ libs/domain/site/src/services/store/default-store.service.ts
@@ -1,16 +1,24 @@
 import { inject } from '@spryker-oryx/di';
-import { map, Observable, shareReplay } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  map,
+  Observable,
+  shareReplay,
+} from 'rxjs';
 import { Store } from '../../models';
 import { StoreAdapter } from '../adapter';
 import { StoreService } from './store.service';
 
 export class DefaultStoreService implements StoreService {
+  protected activeStore$ = new BehaviorSubject<string | undefined>(this.store);
   protected stores$ = this.adapter.get().pipe(shareReplay(1));
-  protected store$ = this.stores$.pipe(
+  protected store$ = combineLatest([this.stores$, this.activeStore$]).pipe(
     map(
-      (stores) =>
-        (this.store && stores.find((store) => store.id === this.store)) ??
-        stores?.[0]
+      ([stores, activeStore]) =>
+        (activeStore
+          ? stores?.find((store) => store.id === activeStore)
+          : undefined) ?? stores?.[0]
     ),
     shareReplay(1)
   );
@@ -26,5 +34,9 @@ export class DefaultStoreService implements StoreService {
 
   get(): Observable<Store | undefined> {
     return this.store$;
+  }
+
+  set(storeId: string): void {
+    this.activeStore$.next(storeId);
   }
 }
