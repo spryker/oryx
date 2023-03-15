2bb76b152358bee5777b0242065a466a816e08c3
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-03-15 20:19:02 +0100

    chore: add most common labels for demo purpose (#148)
    closes [HRZ-2432](https://spryker.atlassian.net/browse/HRZ-2432)

---
+++ libs/template/labs/src/i18n/index.ts
@@ -0,0 +1,16 @@
+import { I18nFeature } from '@spryker-oryx/i18n';
+
+export * from './translations/de';
+
+// to avoid console errors, we provide a list of supported languages
+const supported = ['de', 'ar'];
+
+export const labsI18nFeature = new I18nFeature({
+  load: (localeId) => {
+    if (supported.includes(localeId)) {
+      return import(`./translations/${localeId}.ts`);
+    } else {
+      return Promise.resolve();
+    }
+  },
+});
