b846fdc36fb10c0a344cdd5a985f0f8581c9a3cc
aleespryker <alvina.lee@spryker.com>
2023-03-16 16:56:22 +0100

    fix: formatting

--- libs/domain/picking/src/mocks/src/mock-picking-list.ts
+++ libs/domain/picking/src/mocks/src/mock-picking-list.ts
@@ -1,4 +1,8 @@
-import { ItemsFilters, PickingList, PickingListStatus } from '@spryker-oryx/picking';
+import {
+  ItemsFilters,
+  PickingList,
+  PickingListStatus,
+} from '@spryker-oryx/picking';
 
 export const mockPickingListData: PickingList[] = [
   {
@@ -27,7 +31,7 @@ export const mockPickingListData: PickingList[] = [
           image: '',
           imageLarge: '',
         },
-        status: ItemsFilters.NotPicked
+        status: ItemsFilters.NotPicked,
       },
     ],
   },
