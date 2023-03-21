015d1b59a3221cf9cf9d7f307ae89ce20f56bb3c
taras.hailuk <tarashaliuk@gmail.com>
2023-03-21 13:59:58 +0200

    fix: remove deprecated comment

--- libs/template/themes/src/fulfillment.theme.ts
+++ libs/template/themes/src/fulfillment.theme.ts
@@ -2,7 +2,6 @@ import { Theme } from '@spryker-oryx/core';
 import { defaultBreakpoints } from '@spryker-oryx/themes/breakpoints';
 import { fulfillmentIcons } from '@spryker-oryx/themes/icons';
 
-// TODO: This should be dropped after HRZ-2239
 export const fulfillmentTheme: Theme = {
   name: 'fulfillment',
   breakpoints: defaultBreakpoints,
