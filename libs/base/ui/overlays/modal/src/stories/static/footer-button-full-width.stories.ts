0263cbe2f56ea2721913182bdddd8a8927e36b37
aleespryker <alvina.lee@spryker.com>
2023-03-22 18:12:44 +0100

    fix: add more cases to story

--- libs/base/ui/overlays/modal/src/stories/static/footer-button-full-width.stories.ts
+++ libs/base/ui/overlays/modal/src/stories/static/footer-button-full-width.stories.ts
@@ -10,11 +10,60 @@ export default {
 
 const Template: Story = (): TemplateResult => {
   return html`
-    <oryx-modal open enableFooter footerButtonFullWidth>
+    <script>
+      (() => {
+        const openDefaultModalBtn = document.querySelector(
+          '#openDefaultModalBtn'
+        );
+        const openFullWidthModalBtn = document.querySelector(
+          '#openFullWidthModalBtn'
+        );
+        const openTwoModalBtn = document.querySelector('#openTwoModalBtn');
+
+        const defaultModal = document.querySelector('oryx-modal');
+        const fullWidthModal = document.querySelectorAll('oryx-modal')[1];
+        const twoModal = document.querySelectorAll('oryx-modal')[2];
+
+        openDefaultModalBtn?.addEventListener('click', () => {
+          defaultModal?.open();
+        });
+
+        openFullWidthModalBtn?.addEventListener('click', () => {
+          fullWidthModal?.open();
+        });
+
+        openTwoModalBtn?.addEventListener('click', () => {
+          twoModal?.open();
+        });
+      })();
+    </script>
+
+    <button id="openDefaultModalBtn">Open default</button>
+    <button id="openFullWidthModalBtn">Open full width</button>
+    <button id="openTwoModalBtn">Open two buttons</button>
+    <oryx-modal enableFooter>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua
       </p>
+    </oryx-modal>
+    <oryx-modal enableFooter footerButtonFullWidth>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+    </oryx-modal>
+    <oryx-modal enableCloseButtonInHeader enableFooter footerButtonFullWidth>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+      <oryx-button slot="footer">
+        <button>Save</button>
+      </oryx-button>
+      <oryx-button slot="footer">
+        <button>Ok</button>
+      </oryx-button>
     </oryx-modal>
   `;
 };
