c950dc081582bf21ab4724aeceb6e36098314dee
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-03-23 10:19:28 +0200

    Fixed script.

--- tools/release/release-labs.js
+++ tools/release/release-labs.js
@@ -1,13 +1,49 @@
-const { getVersion } = require('./lerna');
+const { execSync } = require("child_process");
+const libsVersion = require('../../libs/package.json').version;
+const labsVersion  = require('../../libs/template/labs/package.json').version;
 
 function main() {
-  const libsVersion = getVersion();
+  setLabsVersion(getNewLabsVersion());
 
-  releaseLabs(libsVersion);
+  publishToNpm();
 }
 
-function releaseLabs(libsVersion) {
+function getNewLabsVersion() {
+  const undottedLibsVersion = getUndottedLibsVersion(libsVersion);
+  const labsPatch = Number(getLabsPatch());
+  const isNewLibsRelease = getLabsMinor() !== undottedLibsVersion;
 
+  return isNewLibsRelease ? `0.${undottedLibsVersion}.0` : `0.${undottedLibsVersion}.${labsPatch + 1}`;
+}
+
+function getLabsPatch() {
+  return labsVersion.split('.').pop();
+}
+
+function getLabsMinor() {
+  return Number(labsVersion.split('.').at(1));
+}
+
+function getUndottedLibsVersion(libsVersion) {
+  return Number(libsVersion.replaceAll('.', ''));
+}
+
+function setLabsVersion(labsVersion) {
+  runCmd(`npm --prefix ./libs/template/labs version ${labsVersion}`);
+  runCmd(`npm --prefix ./dist/libs/template/labs version ${labsVersion}`);
+}
+
+function publishToNpm() {
+  runCmd(`npm --prefix ./dist/libs/template/labs publish`);
+}
+
+function runCmd(command, options = {}) {
+  return execSync(command, {
+    ...options,
+    env: { ...process.env, ...options.env },
+    stdio: 'inherit',
+    encoding: 'utf-8',
+  });
 }
 
 main();
