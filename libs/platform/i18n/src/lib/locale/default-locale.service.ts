91bda493347c08cb5c95cecdc4fd647645c8d375
taras.hailuk <tarashaliuk@gmail.com>
2023-03-29 12:27:45 +0300

    feat: cache optimisation

--- libs/platform/i18n/src/lib/locale/default-locale.service.ts
+++ libs/platform/i18n/src/lib/locale/default-locale.service.ts
@@ -18,6 +18,9 @@ export class DefaultLocaleService implements LocaleService {
   protected setActive$ = new ReplaySubject<string>(1);
   protected active$;
   protected all$;
+  private dateFormat$: Observable<Intl.DateTimeFormat>;
+  private timeFormat$: Observable<Intl.DateTimeFormat>;
+  private dateTimeFormat$: Observable<Intl.DateTimeFormat>;
 
   constructor(
     protected adapter = inject(LocaleAdapter, null) ??
@@ -43,6 +46,34 @@ export class DefaultLocaleService implements LocaleService {
     this.all$ = this.adapter
       .getAll()
       .pipe(shareReplay({ refCount: true, bufferSize: 1 }));
+
+    this.dateFormat$ = this.get().pipe(
+      map((locale) => Intl.DateTimeFormat(locale.replace('_', '-'))),
+      shareReplay({ refCount: true, bufferSize: 1 })
+    );
+
+    this.timeFormat$ = this.get().pipe(
+      map((locale) =>
+        Intl.DateTimeFormat(locale.replace('_', '-'), {
+          hour: '2-digit',
+          minute: '2-digit',
+        })
+      ),
+      shareReplay({ refCount: true, bufferSize: 1 })
+    );
+
+    this.dateTimeFormat$ = this.get().pipe(
+      map((locale) =>
+        Intl.DateTimeFormat(locale.replace('_', '-'), {
+          hour: '2-digit',
+          minute: '2-digit',
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric',
+        })
+      ),
+      shareReplay({ refCount: true, bufferSize: 1 })
+    );
   }
 
   getAll(): Observable<Locale[]> {
@@ -58,34 +89,23 @@ export class DefaultLocaleService implements LocaleService {
   }
 
   formatDate(stamp: string | number | Date): Observable<string> {
-    return this.get().pipe(
-      map((locale) =>
-        Intl.DateTimeFormat(locale.replace('_', '-')).format(new Date(stamp))
-      )
+    return this.dateFormat$.pipe(
+      map((dateFormat) => dateFormat.format(new Date(stamp)))
     );
   }
 
   formatTime(stamp: string | number | Date): Observable<string> {
-    return this.get().pipe(
-      map((locale) =>
-        Intl.DateTimeFormat(locale.replace('_', '-'), {
-          hour: '2-digit',
-          minute: '2-digit',
-        }).format(stamp instanceof Date ? stamp : new Date(stamp))
+    return this.timeFormat$.pipe(
+      map((timeFormat) =>
+        timeFormat.format(stamp instanceof Date ? stamp : new Date(stamp))
       )
     );
   }
 
   formatDateTime(stamp: string | number | Date): Observable<string> {
-    return this.get().pipe(
-      map((locale) =>
-        Intl.DateTimeFormat(locale.replace('_', '-'), {
-          hour: '2-digit',
-          minute: '2-digit',
-          day: 'numeric',
-          month: 'short',
-          year: 'numeric',
-        }).format(stamp instanceof Date ? stamp : new Date(stamp))
+    return this.dateTimeFormat$.pipe(
+      map((dateTimeFormat) =>
+        dateTimeFormat.format(stamp instanceof Date ? stamp : new Date(stamp))
       )
     );
   }
