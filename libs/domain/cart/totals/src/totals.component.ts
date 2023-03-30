e5dbbe6016f010bc3f09f677ca5878d02c0087cb
Aleksey Belan <alexey.belan@spryker.com>
2023-03-30 15:39:44 +0300

    fix

--- libs/domain/cart/totals/src/totals.component.ts
+++ libs/domain/cart/totals/src/totals.component.ts
@@ -10,6 +10,6 @@ export class CartTotalsComponent extends CartComponentMixin(
   protected override render(): TemplateResult | void {
     if (!this.totals) return;
     return html` <h2>${i18n('cart.totals.summary')}</h2>
-      <experience-composition></experience-composition>`;
+      <experience-composition .uid=${this.uid}></experience-composition>`;
   }
 }
