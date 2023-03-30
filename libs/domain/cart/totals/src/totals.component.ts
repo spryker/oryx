07dadfad013e368c5b067cefd80de69a63d4e079
Aleksey Belan <alexey.belan@spryker.com>
2023-03-30 11:55:26 +0300

    uid

--- libs/domain/cart/totals/src/totals.component.ts
+++ libs/domain/cart/totals/src/totals.component.ts
@@ -1,6 +1,7 @@
 import { ContentMixin } from '@spryker-oryx/experience';
 import { hydratable, i18n } from '@spryker-oryx/utilities';
 import { html, LitElement, TemplateResult } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { CartComponentMixin } from '../../src/mixins/cart.mixin';
 
 @hydratable('window:load')
@@ -10,6 +11,8 @@ export class CartTotalsComponent extends CartComponentMixin(
   protected override render(): TemplateResult | void {
     if (!this.totals) return;
     return html` <h2>${i18n('cart.totals.summary')}</h2>
-      <experience-composition .uid=${this.uid}></experience-composition>`;
+      <experience-composition
+        uid=${ifDefined(this.uid)}
+      ></experience-composition>`;
   }
 }
