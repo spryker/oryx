ba5f84e7252e1d9ac1eb0bafd4e21a4716dd859d
dunqan <dunqan@w9.pl>
2023-03-31 09:21:55 +0200

    formatting

--- libs/base/utilities/no-lit/index.ts
+++ libs/base/utilities/no-lit/index.ts
@@ -3,6 +3,6 @@ export * from '../src/guards';
 export * from '../src/injectables';
 export * from '../src/misc';
 export * from '../src/model';
+export * from '../src/signals';
 export * from '../src/ssr';
 export * from './decorators-stub';
-export * from '../src/signals';
