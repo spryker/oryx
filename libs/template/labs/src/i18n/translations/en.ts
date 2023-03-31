f4014e40b8e57c1db0db522707324ba2bb502cdd
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-03-31 12:25:45 +0200

    plural discounts

--- libs/template/labs/src/i18n/translations/en.ts
+++ libs/template/labs/src/i18n/translations/en.ts
@@ -1,6 +1,8 @@
 export default {
   'cart.totals.<count>-items':
     'My cart ({count, plural, one {one item} other {{count} items}})',
+  'cart.totals.<count>-discounts':
+    '{count, plural, one { Discount} other {Discounts ({count})}}',
   'cart.entry.<quantity>-items': 'x {quantity}',
   'cart.entry.confirm-remove-<sku>': 'Do you want to remove "{sku}"?',
   'cart.confirm-removed': 'Item is successfully removed',
