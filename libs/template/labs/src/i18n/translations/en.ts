45057517cfd3159a0f8b1edbaa5da571028c2bcb
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-03-31 13:16:53 +0200

    pluralisation on discount heading

--- libs/template/labs/src/i18n/translations/en.ts
+++ libs/template/labs/src/i18n/translations/en.ts
@@ -2,7 +2,7 @@ export default {
   'cart.totals.<count>-items':
     'My cart ({count, plural, one {one item} other {{count} items}})',
   'cart.totals.<count>-discounts':
-    '{count, plural, one { Discount} other {Discounts ({count})}}',
+    '{count, plural, one { Discount} other {Discounts}}',
   'cart.entry.<quantity>-items': 'x {quantity}',
   'cart.entry.confirm-remove-<sku>': 'Do you want to remove "{sku}"?',
   'cart.confirm-removed': 'Item is successfully removed',
