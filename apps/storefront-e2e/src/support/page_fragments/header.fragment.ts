62812055aa255790d7c57d08063ccb3b12f9444e
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-04-04 16:52:26 +0300

    add currency e2e tests for sf

--- apps/storefront-e2e/src/support/page_fragments/header.fragment.ts
+++ apps/storefront-e2e/src/support/page_fragments/header.fragment.ts
@@ -1,6 +1,16 @@
 export class HeaderFragment {
   getWrapper = () => cy.get('[uid="header"]');
 
+  getContactLink = () => this.getWrapper().find('oryx-content-link').find('a');
+
+  getCurrencySelector = () =>
+    this.getWrapper().find('oryx-site-currency-selector');
+  getCurrencyButton = () =>
+    this.getCurrencySelector().find('oryx-button').find('button');
+
+  getLogo = () =>
+    this.getWrapper().find('oryx-content-banner').find('a[href="/"]');
+
   getUserSummary = () => cy.get('oryx-user-summary');
   getUserSummaryHeading = () => this.getUserSummary().find('oryx-heading');
   getUserSummaryMenu = () => this.getUserSummary().find('oryx-dropdown');
@@ -11,14 +21,19 @@ export class HeaderFragment {
   getCartSummary = () => cy.get('oryx-cart-summary');
   getCartCount = () => this.getCartSummary().find('mark');
 
-  getLogo = () =>
-    this.getWrapper().find('oryx-content-banner').find('a[href="/"]');
-  getContactLink = () => this.getWrapper().find('oryx-content-link').find('a');
-
   logout = () => {
     this.getOpenUserMenuButton().click();
     this.getUserSummaryMenu().should('have.attr', 'open');
 
     this.getLogoutButton().click();
+  };
+
+  changeCurrency = (currency: string) => {
+    // hydrate
+    this.getCurrencyButton().click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
+    this.getCurrencyButton().click();
+    this.getCurrencySelector().find(`oryx-option[value="${currency}"]`).click();
   };
 }
