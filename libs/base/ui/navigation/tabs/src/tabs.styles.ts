54a4a3f7de36c4ae7fb58dd0da2593b3bce8a2b2
aleespryker <alvina.lee@spryker.com>
2023-04-05 16:58:17 +0200

    fix: make picking tabs sticky

--- libs/base/ui/navigation/tabs/src/tabs.styles.ts
+++ libs/base/ui/navigation/tabs/src/tabs.styles.ts
@@ -16,6 +16,13 @@ export const baseStyles = css`
     cursor: pointer;
   }
 
+  :host([sticky]) slot:not([name]) {
+    position: sticky;
+    inset-block-start: 0;
+    z-index: 1;
+    background-color: var(--oryx-color-canvas-100);
+  }
+
   slot:not([name])::-webkit-scrollbar {
     display: none;
   }
