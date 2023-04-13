91f4672e3468f7ac253126b900180e0f2d29f584
Sinucid <Sinucid@users.noreply.github.com>
2023-04-13 16:51:59 +0300

    feat: clean up old navigation components (#268)
    Dropping of account and cart summaries components and replacing them by navigation-items
    
    closes to HRZ-2672

--- libs/domain/user/summary/src/summary.def.ts
+++
@@ -1,8 +0,0 @@
-import { componentDef } from '@spryker-oryx/core';
-
-export const accountSummaryComponent = componentDef({
-  name: 'oryx-user-summary',
-  impl: () => import('./summary.component').then((m) => m.UserSummaryComponent),
-  schema: () =>
-    import('./summary.schema').then((m) => m.userSummaryComponentSchema),
-});
