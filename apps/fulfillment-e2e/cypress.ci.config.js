bf82cbbef8f2be2d2e078431fcb9c1db2620e256
tarashaliuk <43376934+tarashaliuk@users.noreply.github.com>
2023-04-13 15:09:30 +0300

    feat: run FA e2e tests on pipeline (#266)
    * feat: run FA e2e tests on pipeline
    
    * fix: env name
    
    * fix: change command
    
    * fix: added env var

---
+++ apps/fulfillment-e2e/cypress.ci.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('cypress');
+const config = require('./cypress.config');
+
+module.exports = defineConfig({
+  ...config,
+  projectId: 'bxiyv4',
+  retries: 2,
+});
