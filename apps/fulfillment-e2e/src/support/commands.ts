422b1138635ad4d61801842a9117d41d18df80a2
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-04-14 13:14:50 +0300

    fix FA e2e issues

--- apps/fulfillment-e2e/src/support/commands.ts
+++ apps/fulfillment-e2e/src/support/commands.ts
@@ -1,1 +1,20 @@
 export {};
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      clearIndexedDB(): void;
+    }
+  }
+}
+
+Cypress.Commands.add('clearIndexedDB', () => {
+  cy.window().then((win) => {
+    const databases = win.indexedDB.databases();
+    databases.then((dbs) => {
+      dbs.forEach((db) => {
+        win.indexedDB.deleteDatabase(db.name);
+      });
+    });
+  });
+});
