411feba981d7920735b982153ec35610b1287e66
aleespryker <alvina.lee@spryker.com>
2023-04-17 13:28:13 +0200

    fix: refactoring

--- libs/domain/picking/offline/services/picking-sync-action-handler.service.spec.ts
+++ libs/domain/picking/offline/services/picking-sync-action-handler.service.spec.ts
@@ -7,7 +7,7 @@ import {
   PickingSyncAction,
 } from '@spryker-oryx/picking/offline';
 import { nextTick } from '@spryker-oryx/utilities';
-import { Collection, Table } from 'dexie';
+import { Table } from 'dexie';
 import { of } from 'rxjs';
 import { PickingListOnlineAdapter } from './adapter';
 import { PickingSyncActionHandlerService } from './picking-sync-action-handler.service';
@@ -22,20 +22,9 @@ class MockPickingListOnlineAdapter
   finishPicking = vi.fn().mockReturnValue(of([]));
 }
 
-class MockCollection implements Partial<Collection> {
-  toArray = vi.fn().mockReturnValue([]);
-}
-
-const mockCollection = new MockCollection();
-
 const mockContent = { items: [] };
 
 class MockTable implements Partial<Table> {
-  get = vi.fn().mockReturnValue(mockContent);
-  where = vi.fn().mockReturnValue(mockContent);
-  equals = vi.fn().mockReturnValue(mockContent);
-  toCollection = vi.fn().mockReturnValue(mockCollection);
-  bulkGet = vi.fn().mockReturnValue(mockContent);
   update = vi.fn().mockReturnValue(mockContent);
 }
 
