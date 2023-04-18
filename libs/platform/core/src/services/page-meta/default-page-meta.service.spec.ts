77785f425b45a2cd9d329c0a6ef9998b2ece84b8
Aleksey Belan <alexey.belan@spryker.com>
2023-04-18 14:08:02 +0300

    default-page-meta.service.spec

---
+++ libs/platform/core/src/services/page-meta/default-page-meta.service.spec.ts
@@ -0,0 +1,125 @@
+import { DefaultPageMetaService } from './default-page-meta.service';
+
+describe('DefaultPageMetaService', () => {
+  const service = new DefaultPageMetaService();
+
+  beforeEach(() => {
+    document.head.innerHTML = '';
+  });
+
+  describe('add', () => {
+    it('should add proper tag to the head of document', () => {
+      service.add([
+        {
+          name: 'title',
+          attrs: {
+            text: 'Composable Storefront',
+          },
+        },
+        {
+          name: 'style',
+          attrs: {
+            text: 'style B',
+          },
+        },
+      ]);
+      const title = document.head.querySelector('title');
+      const style = document.head.querySelector('style');
+      expect(title?.textContent).toContain('Composable Storefront');
+      expect(style?.textContent).toContain('style B');
+    });
+
+    it('should add meta tag to the head of document', () => {
+      service.add([
+        {
+          name: 'og:img',
+          attrs: {
+            content: 'a',
+          },
+        },
+        {
+          name: 'meta',
+          attrs: {
+            charset: 'b',
+            content: 'b',
+          },
+        },
+      ]);
+      const imgMeta = document.head.querySelector('meta[name="og:img"]');
+      const charsetMeta = document.head.querySelector('meta[charset="b"]');
+      expect(imgMeta).toBeDefined();
+      expect(imgMeta).toHaveProperty('content', 'a');
+      expect(charsetMeta).toBeDefined();
+      expect(charsetMeta).toHaveProperty('content', 'b');
+    });
+
+    it('should add attributes to the html tag', () => {
+      service.add({
+        name: 'html',
+        attrs: {
+          lang: 'en',
+        },
+      });
+
+      expect(document.documentElement).toHaveProperty('lang', 'en');
+    });
+
+    it('should recreate element add attributes when force is true', () => {
+      service.add({
+        name: 'title',
+        attrs: {
+          text: 'a',
+        },
+      });
+
+      let titles = document.head.querySelectorAll('title');
+      expect(titles.length).toBe(1);
+      expect(titles[0]?.textContent).toContain('a');
+
+      service.add(
+        {
+          name: 'title',
+          attrs: {
+            text: 'b',
+          },
+        },
+        true
+      );
+
+      titles = document.head.querySelectorAll('title');
+      expect(titles.length).toBe(1);
+      expect(titles[0]?.textContent).toContain('b');
+    });
+  });
+
+  describe('setHtmlAttributes', () => {
+    it('should add attributes to the html tag', () => {
+      service.setHtmlAttributes({
+        lang: 'de',
+      });
+
+      expect(document.documentElement).toHaveProperty('lang', 'de');
+    });
+  });
+
+  describe('update', () => {
+    it('should create element if its not existed', () => {
+      service.update({
+        name: 'meta',
+        attrs: {
+          content: 'a',
+        },
+      });
+      const content = document.head.querySelector('meta');
+      expect(content).toHaveProperty('content', 'a');
+
+      service.update({
+        name: 'meta',
+        attrs: {
+          content: 'b',
+        },
+      });
+      expect(content).toHaveProperty('content', 'b');
+    });
+  });
+});
