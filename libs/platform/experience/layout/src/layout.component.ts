fa130354d7a57135782b325f5326d2f9cf71a08b
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-04-19 10:35:09 +0200

    reuse layout mixin

--- libs/platform/experience/layout/src/layout.component.ts
+++ libs/platform/experience/layout/src/layout.component.ts
@@ -1,55 +1,45 @@
-import { resolve } from '@spryker-oryx/di';
-import {
-  computed,
-  hydratable,
-  signalAware,
-  signalProperty,
-  ssrShim,
-} from '@spryker-oryx/utilities';
+import { hydratable, signalAware, ssrShim } from '@spryker-oryx/utilities';
 import { html, LitElement, TemplateResult } from 'lit';
-import { property } from 'lit/decorators.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
-import {
-  CompositionLayout,
-  CompositionLayoutOrientation,
-} from '../../src/models';
-import { LayoutBuilder } from '../../src/services';
+import { LayoutMixin } from './layout.mixin';
 import { LayoutAttributes } from './layout.model';
-import { LayoutService } from './services';
 import { styles } from './styles/base.styles';
 
 @ssrShim('style')
 @hydratable()
 @signalAware()
-export class LayoutComponent extends LitElement implements LayoutAttributes {
+export class LayoutComponent
+  extends LayoutMixin(LitElement)
+  implements LayoutAttributes
+{
   static styles = styles;
 
-  @signalProperty({ reflect: true }) layout?: CompositionLayout;
-  @signalProperty({ reflect: true, attribute: 'layout-sm' })
-  layoutSm?: CompositionLayout;
-  @signalProperty({ reflect: true, attribute: 'layout-md' })
-  layoutMd?: CompositionLayout;
-  @signalProperty({ reflect: true, attribute: 'layout-lg' })
-  layoutLg?: CompositionLayout;
+  // @signalProperty({ reflect: true }) layout?: CompositionLayout;
+  // @signalProperty({ reflect: true, attribute: 'layout-sm' })
+  // layoutSm?: CompositionLayout;
+  // @signalProperty({ reflect: true, attribute: 'layout-md' })
+  // layoutMd?: CompositionLayout;
+  // @signalProperty({ reflect: true, attribute: 'layout-lg' })
+  // layoutLg?: CompositionLayout;
 
-  @property({ reflect: true, type: Boolean, attribute: 'bleed-out' })
-  bleed?: boolean;
-  @property({ reflect: true, type: Boolean }) sticky?: boolean;
+  // @property({ reflect: true, type: Boolean, attribute: 'bleed-out' })
+  // bleed?: boolean;
+  // @property({ reflect: true, type: Boolean }) sticky?: boolean;
 
-  @property({ reflect: true }) orientation?: CompositionLayoutOrientation;
-  @property({ reflect: true, type: Boolean }) vertical?: boolean;
+  // @property({ reflect: true }) orientation?: CompositionLayoutOrientation;
+  // @property({ reflect: true, type: Boolean }) vertical?: boolean;
 
-  protected layoutBuilder = resolve(LayoutBuilder);
-  protected layoutService = resolve(LayoutService);
+  // protected layoutBuilder = resolve(LayoutBuilder);
+  // protected layoutService = resolve(LayoutService);
 
-  protected layoutStyles = computed(() => {
-    const layouts: { [key: string]: CompositionLayout } = {};
-    if (this.layoutSm) layouts.sm = this.layoutSm;
-    if (this.layoutMd) layouts.md = this.layoutMd;
-    if (this.layoutLg) layouts.lg = this.layoutLg;
+  // protected layoutStyles = computed(() => {
+  //   const layouts: { [key: string]: CompositionLayout } = {};
+  //   if (this.layoutSm) layouts.sm = this.layoutSm;
+  //   if (this.layoutMd) layouts.md = this.layoutMd;
+  //   if (this.layoutLg) layouts.lg = this.layoutLg;
 
-    return this.layoutService.getStyles(this.layout, layouts);
-  });
+  //   return this.layoutService.getStyles({});
+  // });
 
   protected override render(): TemplateResult {
     return html`${unsafeHTML(`<style>${this.layoutStyles()}</style>`)}<slot
