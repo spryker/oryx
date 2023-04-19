89736559bdb01eb05c7d084b5699920650293993
taras.hailuk <tarashaliuk@gmail.com>
2023-04-19 17:59:31 +0300

    fix: refactor code according CR suggestions

--- apps/fulfillment-e2e/src/support/page_fragments/picking-lists.fragment.ts
+++ apps/fulfillment-e2e/src/support/page_fragments/picking-lists.fragment.ts
@@ -1,14 +1,22 @@
 export class PickingListsFragment {
-  private getElement = (selector: string, withoutWrapper?: boolean) => {
-    return withoutWrapper ? cy.get(selector) : this.getWrapper().find(selector);
-  };
-
   getWrapper = () => cy.get('oryx-picking-lists');
+
   getPickingListsItem = () => this.getWrapper().find('oryx-picking-list-item');
+
   getCustomerNoteButton = () =>
     this.getWrapper().find('oryx-icon-button button');
-  getStartPickingButton = (withoutWrapper?: boolean) =>
-    this.getElement('oryx-button button', withoutWrapper);
-  getIdentifier = (withoutWrapper?: boolean) =>
-    this.getElement('.identifier', withoutWrapper);
+
+  getStartPickingButton = () => cy.get('oryx-button button');
+
+  getIdentifier = () =>
+    this.getCustomerNoteButton()
+      .parents('oryx-picking-list-item')
+      .within(() => {
+        return cy
+          .get('.identifier')
+          .invoke('text')
+          .then((identifierText) => {
+            cy.wrap(identifierText).as('identifier');
+          });
+      });
 }
