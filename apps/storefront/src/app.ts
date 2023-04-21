5cf20dd2fcffa74edd73f35e55bdca0ccdbbb47e
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-04-21 21:18:51 +0300

    change env import logic

--- apps/storefront/src/app.ts
+++ apps/storefront/src/app.ts
@@ -2,14 +2,15 @@ import { appBuilder } from '@spryker-oryx/core';
 import { labsFeature } from '@spryker-oryx/labs';
 import { b2cFeatures, b2cTheme } from '@spryker-oryx/presets';
 
+const env = import.meta.env
 const features = [...b2cFeatures];
 
-if (import.meta.env.ORYX_LABS) {
+if (env.ORYX_LABS) {
   features.push(...labsFeature);
 }
 
 export const app = appBuilder()
   .withFeature(features)
   .withTheme(b2cTheme)
-  .withEnvironment(import.meta.env)
+  .withEnvironment(env)
   .create();
