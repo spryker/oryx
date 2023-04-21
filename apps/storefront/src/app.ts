d170e1cb4b933f967cceafa402d369d47aa7835a
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-04-21 15:34:24 +0300

    add ORYX_LABS env support

--- apps/storefront/src/app.ts
+++ apps/storefront/src/app.ts
@@ -1,8 +1,15 @@
 import { appBuilder } from '@spryker-oryx/core';
+import { labsFeature } from '@spryker-oryx/labs';
 import { b2cFeatures, b2cTheme } from '@spryker-oryx/presets';
 
+const features = [...b2cFeatures];
+
+if (import.meta.env.ORYX_LABS) {
+  features.push(...labsFeature);
+}
+
 export const app = appBuilder()
-  .withFeature(b2cFeatures)
+  .withFeature(features)
   .withTheme(b2cTheme)
   .withEnvironment(import.meta.env)
   .create();
