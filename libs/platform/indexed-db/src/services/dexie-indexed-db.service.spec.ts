92210b175a6e473ff8190eef48dcbcb38e1ad5c6
Aleksey Belan <alexey.belan@spryker.com>
2023-05-09 15:56:01 +0300

    lint

--- libs/platform/indexed-db/src/services/dexie-indexed-db.service.spec.ts
+++ libs/platform/indexed-db/src/services/dexie-indexed-db.service.spec.ts
@@ -213,6 +213,7 @@ describe('DefaultExperienceService', () => {
         service.getStoreName(
           class C {
             static migration = vi.fn();
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             public static get name() {
               return 'classC';
