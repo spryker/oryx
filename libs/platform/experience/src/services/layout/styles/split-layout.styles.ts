4147bb6afea94ed3b52d237bfcfccbecc4fbaeca
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-05-11 08:53:47 +0200

    stylelint

--- libs/platform/experience/src/services/layout/styles/split-layout.styles.ts
+++ libs/platform/experience/src/services/layout/styles/split-layout.styles.ts
@@ -2,28 +2,29 @@ import { css, unsafeCSS } from 'lit';
 import { LayoutStyles } from '../layout.model';
 import { gridSystem } from './grid-system.styles';
 
-export const splitLayout = (
-  type: 'equal' | 'main' | 'aside'
-): LayoutStyles => ({
-  styles: css`
-    ${gridSystem}
+export const splitLayout = (type: 'equal' | 'main' | 'aside'): LayoutStyles => {
+  const cssVar = unsafeCSS(`var(--oryx-column-split-${type})`);
+  return {
+    styles: css`
+      ${gridSystem}
 
-    :host {
-      --split: var(--oryx-column-split-${unsafeCSS(type)});
+      :host {
+        --split: ${cssVar};
 
-      grid-template-columns:
-        calc(
-          var(--split) * var(--_item-size) + (var(--split) - 1) *
-            var(--column-gap, var(--oryx-gap-column, 0px))
-        )
-        auto;
-    }
-  `,
-  sm: css`
-    :host {
-      grid-template-columns: auto;
-    }
-  `,
-});
+        grid-template-columns:
+          calc(
+            var(--split) * var(--_item-size) + (var(--split) - 1) *
+              var(--column-gap, var(--oryx-gap-column, 0px))
+          )
+          auto;
+      }
+    `,
+    sm: css`
+      :host {
+        grid-template-columns: auto;
+      }
+    `,
+  };
+};
 
 export const styles = splitLayout('equal');
