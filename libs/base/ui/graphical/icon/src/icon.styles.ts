52f0dbd80b2ae9ff69813eeab6ecaf9d425598bb
Aleksey Belan <alexey.belan@spryker.com>
2023-05-16 14:24:14 +0300

    revert _margin

--- libs/base/ui/graphical/icon/src/icon.styles.ts
+++ libs/base/ui/graphical/icon/src/icon.styles.ts
@@ -24,9 +24,9 @@ export const styles = css`
   svg,
   ::slotted(svg) {
     fill: var(--oryx-icon-color, currentColor);
-    width: calc(var(--oryx-icon-size, 24px) - (var(--_margin, 0) * 2));
+    width: calc(var(--oryx-icon-size, 24px) - (var(--_margin) * 2));
     aspect-ratio: 1 / 1;
-    margin: var(--_margin, 0);
+    margin: var(--_margin);
     transition: var(--oryx-transition-time);
   }
 
