3c0b2aa41c77d93c78e6f99964a06b24ffe1dc0f
Aleksey Belan <alexey.belan@spryker.com>
2023-05-16 14:04:03 +0300

    comment code

--- libs/template/themes/src/storefront.theme.ts
+++ libs/template/themes/src/storefront.theme.ts
@@ -1,23 +1,18 @@
 import { Theme } from '@spryker-oryx/experience';
 import { defaultBreakpoints } from '@spryker-oryx/themes/breakpoints';
-import {
-  fontAwesomeIcons,
-  IconTypes,
-  materialIcons,
-} from '@spryker-oryx/themes/icons';
 
 export const storefrontTheme: Theme = {
   name: 'storefront',
   breakpoints: defaultBreakpoints,
   designTokens: () =>
     import('../design-tokens/src/storefront').then((s) => s.storefrontTokens),
-  icons: {
-    resource: materialIcons,
-    resources: [
-      {
-        resource: fontAwesomeIcons,
-        types: [IconTypes.User],
-      },
-    ],
-  },
+  // icons: {
+  //   resource: materialIcons,
+  //   resources: [
+  //     {
+  //       resource: fontAwesomeIcons,
+  //       types: [IconTypes.User],
+  //     },
+  //   ],
+  // },
 };
