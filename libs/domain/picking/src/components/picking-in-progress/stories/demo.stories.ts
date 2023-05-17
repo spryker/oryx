f5731eb33f532b459c707962e10464687ea8eccd
aleespryker <alvina.lee@spryker.com>
2023-05-17 18:03:28 +0200

    fix: refactoring

--- libs/domain/picking/src/components/picking-in-progress/stories/demo.stories.ts
+++ libs/domain/picking/src/components/picking-in-progress/stories/demo.stories.ts
@@ -1,28 +1,17 @@
+import { OverlaysDecorator } from '@spryker-oryx/ui';
 import { Meta, Story } from '@storybook/web-components';
 import { html, TemplateResult } from 'lit';
 import { storybookPrefix } from '../../../../.constants';
 
 export default {
   title: `${storybookPrefix}/Picking In Progress Modal`,
+  decorators: [OverlaysDecorator()],
 } as Meta;
 
 const Template: Story<unknown> = (): TemplateResult => {
-  return html`<script>
-      (() => {
-        const modalBtn = document.querySelector('#modalBtn');
-
-        modalBtn?.addEventListener('click', () => {
-          const modal = document
-            .querySelector('oryx-picking-in-progress-modal')
-            ?.renderRoot.querySelector('oryx-modal');
-          modal?.open();
-        });
-      })();
-    </script>
-
-    <button id="modalBtn">Open modal</button>
-
-    <oryx-picking-in-progress-modal></oryx-picking-in-progress-modal> `;
+  return html`<oryx-picking-in-progress-modal
+    open
+  ></oryx-picking-in-progress-modal> `;
 };
 
 export const Demo = Template.bind({});
