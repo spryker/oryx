8fbb94160ee5420b58d36bc766e4c5c78265b997
Aleksey Belan <alexey.belan@spryker.com>
2023-05-18 02:38:53 +0300

    test

--- apps/storybook/.storybook/app/chromatic/plugins.ts
+++ apps/storybook/.storybook/app/chromatic/plugins.ts
@@ -1,23 +1,9 @@
 import { resolveLazyLoadable } from '@spryker-oryx/core/utilities';
 import { ResourcePlugin, Resources } from '@spryker-oryx/experience';
-import { graphicInjectable } from '@spryker-oryx/utilities';
-import { ChromaticGraphicInjectable } from './injectables';
 
 export class ResourcesChromaticPlugin extends ResourcePlugin {
   constructor(resources: Resources) {
     super(resources);
-
-    // Promise.all(
-    //   Object.values(this.resources.icons ?? {}).map(resolveLazyLoadable)
-    // ).then((s) => {
-    //   this.resources.icons = Object.keys(this.resources.icons ?? {}).reduce(
-    //     (acc, prev, index) => ({
-    //       ...acc,
-    //       [prev]: s[index],
-    //     }),
-    //     {}
-    //   );
-    // });
 
     for (const [key, value] of Object.entries(this.resources?.graphics ?? {})) {
       Promise.all(Object.values(value).map(resolveLazyLoadable)).then((s) => {
@@ -35,7 +21,6 @@ export class ResourcesChromaticPlugin extends ResourcePlugin {
       });
     }
 
-    graphicInjectable.inject(new ChromaticGraphicInjectable());
-    // iconInjectable.inject(new ChromaticIconInjectable());
+    // graphicInjectable.inject(new ChromaticGraphicInjectable());
   }
 }
