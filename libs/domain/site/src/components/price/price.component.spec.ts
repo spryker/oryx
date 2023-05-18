67dc09354a4542385556753072b59c0b7dbfb3c2
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-05-18 20:15:35 +0300

    Formatting.

--- libs/domain/site/src/components/price/price.component.spec.ts
+++ libs/domain/site/src/components/price/price.component.spec.ts
@@ -4,9 +4,9 @@ import { createInjector, destroyInjector } from '@spryker-oryx/di';
 import { LocaleService } from '@spryker-oryx/i18n';
 import { html } from 'lit';
 import { of } from 'rxjs';
-import { priceComponent } from './price.def';
 import { CurrencyService, siteProviders } from '../../services';
 import { PriceComponent } from './price.component';
+import { priceComponent } from './price.def';
 
 class MockLocaleService implements Partial<LocaleService> {
   get = vi.fn().mockReturnValue(of('en'));
