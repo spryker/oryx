40971f7d0f445c3d920ee656722ddd1564268166
Aleksey Belan <alexey.belan@spryker.com>
2023-05-19 16:45:57 +0300

    icons

--- libs/template/presets/resources/icons/backoffice/icons.ts
+++ libs/template/presets/resources/icons/backoffice/icons.ts
@@ -48,4 +48,6 @@ export const backofficeIcons: ResourceIcons = {
   [IconTypes.Back]: () => import('./icons/back').then((s) => s.default),
   [IconTypes.Front]: () => import('./icons/front').then((s) => s.default),
   [IconTypes.Mark]: () => import('./icons/mark').then((s) => s.default),
+  [IconTypes.DropUp]: () => import('./icons/drop-up').then((s) => s.default),
+  [IconTypes.Dropdown]: () => import('./icons/dropdown').then((s) => s.default),
 };
