5f337d3a65f1948d59665db9dcc6242d033dd4b3
aleespryker <94365900+aleespryker@users.noreply.github.com>
2023-05-19 11:02:08 +0200

    fix: don't load offline data plugin in service worker (#347)
    * fix: try data plugin as AppPlugin
    
    * fix: don't load offline data-plugin in service worker
    
    * fix: refactoring

--- libs/template/presets/src/fulfillment/offline.ts
+++ libs/template/presets/src/fulfillment/offline.ts
@@ -54,11 +54,14 @@ export function offlineServiceWorkerFulfillmentFeatures(
     ...config,
   };
 
+  const offlinePickingFeatures = new OfflinePickingFeature();
+  offlinePickingFeatures.plugins = [];
+
   return [
     coreFeature,
     new I18nFeature(config?.i18n),
     new IndexedDbFeature(config?.indexedDb),
     new OfflineServiceWorkerFeature(),
-    new OfflinePickingFeature(),
+    offlinePickingFeatures,
   ];
 }
