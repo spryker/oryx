af7c15fa28c26981fc34e7f212dd8d70054302e7
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-05-19 07:04:44 +0200

    review

--- libs/domain/checkout/src/services/state/default-checkout-state.service.ts
+++ libs/domain/checkout/src/services/state/default-checkout-state.service.ts
@@ -1,13 +1,25 @@
 import { StorageService, StorageType } from '@spryker-oryx/core';
 import { inject } from '@spryker-oryx/di';
-import { BehaviorSubject, map, Observable, take, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  map,
+  Observable,
+  take,
+  tap,
+} from 'rxjs';
 import { Checkout, checkoutDataStorageKey } from '../../models';
 import { CheckoutStateService } from './checkout-state.service';
 
-type m = { valid?: boolean; value?: Partial<Checkout[keyof Checkout]> | null };
+type CheckoutValue = {
+  valid?: boolean;
+  value?: Partial<Checkout[keyof Checkout]> | null;
+};
 
 export class DefaultCheckoutStateService implements CheckoutStateService {
-  protected subject = new BehaviorSubject<Map<keyof Checkout, m>>(new Map());
+  protected subject = new BehaviorSubject<Map<keyof Checkout, CheckoutValue>>(
+    new Map()
+  );
 
   constructor(protected storage = inject(StorageService)) {
     this.restore();
@@ -38,7 +50,8 @@ export class DefaultCheckoutStateService implements CheckoutStateService {
       map((data) => {
         if (!data.get(key)) this.set(key, {});
         return data.get(key)?.value as Checkout[K] | null;
-      })
+      }),
+      distinctUntilChanged()
     );
   }
 
@@ -61,8 +74,6 @@ export class DefaultCheckoutStateService implements CheckoutStateService {
   }
 
   protected populateData(data: Partial<Checkout>): Partial<Checkout> {
-    console.log('inp', data);
-    // const data = { ...inp };
     if (data.customer) {
       if (!data.customer.salutation && data.shippingAddress?.salutation)
         data.customer.salutation = data.shippingAddress.salutation;
@@ -83,7 +94,10 @@ export class DefaultCheckoutStateService implements CheckoutStateService {
    */
   protected restore(): void {
     this.storage
-      .get<Map<keyof Checkout, m>>(checkoutDataStorageKey, StorageType.SESSION)
+      .get<Map<keyof Checkout, CheckoutValue>>(
+        checkoutDataStorageKey,
+        StorageType.SESSION
+      )
       .pipe(
         take(1),
         tap((stored) => this.subject.next(new Map(stored)))
