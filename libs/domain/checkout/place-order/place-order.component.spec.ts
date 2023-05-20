0b4d48208ee88a2098dcaa6e61400d6c5b787d1b
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-05-20 11:46:02 +0200

    Finalise place order tests

--- libs/domain/checkout/place-order/place-order.component.spec.ts
+++ libs/domain/checkout/place-order/place-order.component.spec.ts
@@ -4,6 +4,7 @@ import {
   CheckoutDataService,
   CheckoutResponse,
   CheckoutService,
+  CheckoutState,
   CheckoutStateService,
 } from '@spryker-oryx/checkout';
 import { CheckoutPlaceOrderComponent } from '@spryker-oryx/checkout/place-order';
@@ -87,31 +88,93 @@ describe('PlaceOrderComponent', () => {
     vi.clearAllMocks();
   });
 
-  describe('when button is clicked', () => {
+  describe('when the checkout process is not ready', () => {
     beforeEach(async () => {
+      checkoutService.getProcessState.mockReturnValue(of(CheckoutState.Empty));
+      element = await fixture(
+        html`<oryx-checkout-place-order></oryx-checkout-place-order>`
+      );
+    });
+
+    it('should not render the place order button', () => {
+      expect(element).not.toContainElement('oryx-button');
+    });
+  });
+
+  describe('when the checkout process is ready', () => {
+    beforeEach(async () => {
+      checkoutService.getProcessState.mockReturnValue(of(CheckoutState.Ready));
       element = await fixture(
         html`<oryx-checkout-place-order></oryx-checkout-place-order>`
       );
       element.renderRoot.querySelector('button')?.click();
     });
 
-    it('should call CheckoutService.placeOrder', () => {
-      expect(checkoutService.placeOrder).toHaveBeenCalled();
+    it('should render the place order button', () => {
+      expect(element).toContainElement('oryx-button');
     });
 
-    describe('and a redirect is returned', () => {
-      beforeEach(() => {
-        checkoutService.placeOrder.mockReturnValue(
-          of({ redirectUrl: 'http://foo.com/bar' } as CheckoutResponse)
-        );
+    it('should not render the loading attribute', () => {
+      expect(element).not.toContainElement('oryx-button[loading]');
+    });
+
+    it('should not set the inert state on the button', () => {
+      expect(element).not.toContainElement('oryx-button[inert]');
+    });
+
+    describe('and the button is clicked', () => {
+      beforeEach(async () => {
         element.renderRoot.querySelector('button')?.click();
       });
 
-      it('should navigate to the redirect route', () => {
-        expect(routerService.navigate).toHaveBeenCalledWith(
-          'http://foo.com/bar'
-        );
+      it('should call CheckoutService.placeOrder', () => {
+        expect(checkoutService.placeOrder).toHaveBeenCalled();
       });
+
+      describe('and when a redirect is returned', () => {
+        beforeEach(() => {
+          checkoutService.placeOrder.mockReturnValue(
+            of({ redirectUrl: 'http://foo.com/bar' } as CheckoutResponse)
+          );
+          element.renderRoot.querySelector('button')?.click();
+        });
+
+        it('should navigate to the redirect route', () => {
+          expect(routerService.navigate).toHaveBeenCalledWith(
+            'http://foo.com/bar'
+          );
+        });
+      });
+
+      describe('and when no redirect is returned', () => {
+        beforeEach(() => {
+          checkoutService.placeOrder.mockReturnValue(
+            of({} as CheckoutResponse)
+          );
+          element.renderRoot.querySelector('button')?.click();
+        });
+
+        it('should not navigate to the redirect route', () => {
+          expect(routerService.navigate).not.toHaveBeenCalled();
+        });
+      });
+    });
+  });
+
+  describe('when the checkout process is busy', () => {
+    beforeEach(async () => {
+      checkoutService.getProcessState.mockReturnValue(of(CheckoutState.Busy));
+      element = await fixture(
+        html`<oryx-checkout-place-order></oryx-checkout-place-order>`
+      );
+    });
+
+    it('should render a loading button', () => {
+      expect(element).toContainElement('oryx-button[loading]');
+    });
+
+    it('should set the inert state on the button', () => {
+      expect(element).toContainElement('oryx-button[inert]');
     });
   });
 });
