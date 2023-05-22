956d0adbd19146f67c5ebda92ab22b3aa6e95686
Aleksey Belan <alexey.belan@spryker.com>
2023-05-22 17:37:34 +0300

    fix story

--- libs/domain/product/images/src/stories/static.stories.ts
+++ libs/domain/product/images/src/stories/static.stories.ts
@@ -6,6 +6,7 @@ import {
   ProductImagesMainLayout,
   ProductImagesNavigationAlignment as PINA,
   ProductImagesNavigationDisplay as PIND,
+  ProductImagesNavigationDisplay,
   ProductImagesNavigationLayout as PINL,
   ProductImagesNavigationPosition as PINP,
   ProductImagesNavigationPosition,
@@ -47,7 +48,7 @@ const Template: Story<unknown> = (): TemplateResult => {
       <div class="static">
         ${[
           ProductImagesMainLayout.Carousel,
-          ProductImagesMainLayout.Toggle,
+          ProductImagesMainLayout.Grid,
           ProductImagesMainLayout.None,
         ].map((mainLayout) => render({ imageLayout: mainLayout }))}
       </div>
@@ -146,13 +147,29 @@ const Template: Story<unknown> = (): TemplateResult => {
 
       <h2>Grid items per column (inline)</h2>
       <div class="static">
-        ${[3, 4, 5, 6].map((gridItemsPerColumn) =>
+        ${[3, 4, 5, 6].map((imagesColumns) =>
           render(
             {
               navigationLayout: PINL.Grid,
               navigationPosition: ProductImagesNavigationPosition.Start,
-              gridItemsPerColumn,
-              navigationHeight: 40,
+              imagesColumns,
+              navigationHeight: '40px',
+            },
+            '3'
+          )
+        )}
+      </div>
+
+      <h2>Grid items per column (floating)</h2>
+      <div class="static">
+        ${[3, 4, 5, 6].map((imagesColumns) =>
+          render(
+            {
+              navigationLayout: PINL.Grid,
+              navigationPosition: ProductImagesNavigationPosition.Start,
+              navigationDisplay: ProductImagesNavigationDisplay.Floating,
+              imagesColumns,
+              navigationHeight: '40px',
             },
             '3'
           )
