ba6d1ddaafbfd6d0dd84f3b82d530a548fdc4e9e
pavel.gulvanskiy <pavlo.hulvanskiy@spryker.com>
2023-05-22 14:56:02 +0300

    hrz-2916: minors

--- libs/template/presets/src/fulfillment/offline.ts
+++ libs/template/presets/src/fulfillment/offline.ts
@@ -1,4 +1,4 @@
-import { AppFeature, coreFeature } from '@spryker-oryx/core';
+import { AppFeature, HttpInterceptor, coreFeature, injectEnv } from '@spryker-oryx/core';
 import { I18nFeature, I18nFeatureOptions } from '@spryker-oryx/i18n';
 import {
   IndexedDbFeature,
@@ -13,6 +13,12 @@ import {
   SwOfflinePickingFeature,
 } from '@spryker-oryx/picking/offline';
 import { fulfillmentFeatures, FulfillmentFeaturesConfig } from './app';
+import { BapiAuthFeature } from '@spryker-oryx/application';
+import { AuthService, AuthTokenInterceptorConfig, AuthTokenService, DefaultOauthProviderFactoryService, OauthFeature, OauthProviderFactoryService, OauthService, OauthServiceConfig, OauthTokenInterceptor } from '@spryker-oryx/auth';
+import urlJoin from 'url-join';
+import { RouterFeature } from '@spryker-oryx/router';
+import { WebPushNotificationFeature } from '@spryker-oryx/push-notification/web';
+import { inject } from '@spryker-oryx/di';
 
 export interface SharedOfflineFulfillmentFeaturesConfig {
   indexedDb?: IndexedDbFeatureConfig;
@@ -64,5 +70,66 @@ export function offlineServiceWorkerFulfillmentFeatures(
     new OfflineServiceWorkerFeature(),
     new OfflinePickingFeature(),
     new SwOfflinePickingFeature(),
+    // new RouterFeature(),
+    // {
+    //   providers: [
+    //     { provide: OauthService, useClass: OauthService },
+    //     { provide: OauthServiceConfig, useFactory: () => ({
+    //         loginRoute: '/login',
+    //         providers: [
+    //           {
+    //             id: 'spryker',
+    //             clientId: 'frontend',
+    //             grantType: 'authorization_code',
+    //             authUrl: new URL('/login', globalThis.location.origin).toString(),
+    //             tokenUrl: urlJoin(
+    //               injectEnv('ORYX_FULFILLMENT_BACKEND_URL') ?? '',
+    //               '/token'
+    //             ),
+    //             redirectUrl: new URL(
+    //               '/oauth/cb/spryker',
+    //               globalThis.location.origin
+    //             ).toString(),
+    //           },
+    //         ],
+    //         defaultProvider: 'spryker',
+    //       }) },
+    //     { provide: AuthService, useFactory: () => inject(OauthService) },
+    //     { provide: AuthTokenService, useFactory: () => inject(OauthService) },
+    //     { provide: HttpInterceptor, useClass: OauthTokenInterceptor },
+    //     {
+    //       provide: OauthProviderFactoryService,
+    //       useClass: DefaultOauthProviderFactoryService,
+    //     },
+    //   //   {
+    //   //     provide: AuthTokenInterceptorConfig,
+    //   //     useFactory: () => configFactory().tokenInterceptor,
+    //   // },
+    //   ],
+    // },
+
+    // new WebPushNotificationFeature(),
+    // new RouterFeature(),
+    // new OauthFeature(() => ({
+    //   loginRoute: '/login',
+    //   providers: [
+    //     {
+    //       id: 'spryker',
+    //       clientId: 'frontend',
+    //       grantType: 'authorization_code',
+    //       authUrl: new URL('/login', globalThis.location.origin).toString(),
+    //       tokenUrl: urlJoin(
+    //         injectEnv('ORYX_FULFILLMENT_BACKEND_URL') ?? '',
+    //         '/token'
+    //       ),
+    //       redirectUrl: new URL(
+    //         '/oauth/cb/spryker',
+    //         globalThis.location.origin
+    //       ).toString(),
+    //     },
+    //   ],
+    //   defaultProvider: 'spryker',
+    // }))
+    // new BapiAuthFeature(),
   ];
 }
