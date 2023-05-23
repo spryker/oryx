2b7c7e7c09786f02995ecf3070396fd93a9542c5
Aleksey Belan <alexey.belan@spryker.com>
2023-05-23 11:47:45 +0300

    format

--- libs/template/presets/resources/icons/index.ts
+++ libs/template/presets/resources/icons/index.ts
@@ -1,6 +1,6 @@
 export * from './backoffice';
 export * from './backoffice-ng';
+export * from './fonts';
 export * from './fulfillment';
 export * from './icon.model';
 export * from './storefront';
-export * from './fonts';
