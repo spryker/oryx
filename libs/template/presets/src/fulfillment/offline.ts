0fda0d7ab8d5ef025fcc50b94c1d16304a124840
Aleksey Belan <alexey.belan@spryker.com>
2023-05-24 12:35:13 +0300

    fix: fulfillment provider for service worker (#365)
    fixes fulfillment provider for service worker

--- libs/template/presets/src/fulfillment/offline.ts
+++ libs/template/presets/src/fulfillment/offline.ts
@@ -1,4 +1,5 @@
-import { AppFeature, coreFeature } from '@spryker-oryx/core';
+import { AppFeature, coreFeature, PageMetaService } from '@spryker-oryx/core';
+import { ServerPageMetaService } from '@spryker-oryx/core/server';
 import { I18nFeature, I18nFeatureOptions } from '@spryker-oryx/i18n';
 import {
   IndexedDbFeature,
@@ -63,5 +64,13 @@ export function offlineServiceWorkerFulfillmentFeatures(
     new IndexedDbFeature(config?.indexedDb),
     new OfflineServiceWorkerFeature(),
     offlinePickingFeatures,
+    {
+      providers: [
+        {
+          provide: PageMetaService,
+          useClass: ServerPageMetaService,
+        },
+      ],
+    },
   ];
 }
