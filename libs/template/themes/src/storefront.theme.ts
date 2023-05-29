6e0499ffdcbafe34784fbedf1d5ce927ce54219c
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-05-29 08:47:52 +0200

    refactor: facet navigation ui (#383)
    closes [HRZ-2963](https://spryker.atlassian.net/browse/HRZ-2963)
    
    [HRZ-2963]: https://spryker.atlassian.net/browse/HRZ-2963?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- libs/template/themes/src/storefront.theme.ts
+++ libs/template/themes/src/storefront.theme.ts
@@ -3,6 +3,7 @@ import { materialDesignIcons, storefrontIcons } from '@spryker-oryx/resources';
 import { defaultBreakpoints } from '@spryker-oryx/themes/breakpoints';
 
 export const storefrontTheme: Theme = {
+  name: 'storefront',
   breakpoints: defaultBreakpoints,
   designTokens: () =>
     import('../design-tokens/src/storefront').then((s) => s.storefrontTokens),
