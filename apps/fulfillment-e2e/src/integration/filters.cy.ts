197b4c9f3fddd81b1b4da6c5fb067c9758de6cc9
pavel.gulvanskiy <pavlo.hulvanskiy@spryker.com>
2023-05-29 10:16:01 +0300

    hrz-2865: cr fixes

--- apps/fulfillment-e2e/src/integration/filters.cy.ts
+++ apps/fulfillment-e2e/src/integration/filters.cy.ts
@@ -1,83 +1,93 @@
 import { FiltersFragment } from '../support/page_fragments/filters.fragment';
-import { PickingListsFragment } from '../support/page_fragments/picking-lists.fragment';
 
-const pickingListsFragment = new PickingListsFragment();
 const filtersFragment = new FiltersFragment();
 
-describe('When user opens the filters', () => {
-  const largeAndEarliestOrderFirst = (exists = true) =>
-    pickingListsFragment
-      .getPickingListsItems()
-      .eq(0)
-      .find('.total oryx-icon-button button')
-      .should(exists ? 'exist' : 'not.exist');
-
+describe('When user interacts with the filters', () => {
   beforeEach(() => {
     cy.clearIndexedDB();
     cy.login();
-
-    filtersFragment.getFilterButtonTrigger().click();
   });
 
-  it('should apply correct sorting configuration', () => {
-    //should open the filters modal
-    filtersFragment.getFiltersModal().should('have.attr', 'open');
-    //should have default selected sorting option
-    filtersFragment.getFiltersModal().find('input').eq(0).should('be.checked');
+  describe('And filters is closed', () => {
+    it('should reset unapplied changes', () => {
+      //check whether default configuration is applied
+      filtersFragment.getFilterButtonTrigger().should('not.be.checked');
+      filtersFragment.isPickingListsOrderChanged();
 
-    //When close button is clicked
-    filtersFragment.getFiltersModal().find('input').eq(1).click();
-    filtersFragment.getFiltersModal().find('input').eq(1).should('be.checked');
-
-    filtersFragment.getFiltersClose().click();
-
-    //should close the modal
-    filtersFragment.getFiltersModal().should('not.have.attr', 'open');
-
-    //should not make sort button active
-    filtersFragment.getFilterButtonTrigger().should('not.be.checked');
-
-    //should reset the form
-    filtersFragment.getFiltersModal().find('input').eq(0).should('be.checked');
-
-    //should not change the order
-    largeAndEarliestOrderFirst();
-
-    //When sorting is applied
-    [false, true, false].forEach((expect, index) => {
       filtersFragment.getFilterButtonTrigger().click();
 
-      filtersFragment
-        .getFiltersModal()
-        .find('input')
-        .eq(index + 1)
-        .click();
-      filtersFragment.getFiltersApply().click();
+      //should open the filters modal
+      filtersFragment.getFiltersModal().should('be.visible');
+      //should have default selected sorting option
+      filtersFragment.getSortingOption(0).should('be.checked');
+
+      //When close button is clicked
+      filtersFragment.getSortingOption(1).click();
+      filtersFragment.getSortingOption(1).should('be.checked');
+
+      filtersFragment.getFiltersCloseButton().click();
 
       //should close the modal
-      filtersFragment.getFiltersModal().should('not.have.attr', 'open');
+      filtersFragment.getFiltersModal().should('not.be.visible');
 
-      //should apply sorting order
-      largeAndEarliestOrderFirst(expect);
+      //should not make sort button active
+      filtersFragment.getFilterButtonTrigger().should('not.be.checked');
 
-      //should make sort button active
-      filtersFragment.getFilterButtonTrigger().should('be.checked');
+      //should reset the form
+      filtersFragment.getSortingOption(0).should('be.checked');
+
+      //should not change the order
+      filtersFragment.isPickingListsOrderChanged();
     });
+  });
 
-    //And filters are reset
-    filtersFragment.getFilterButtonTrigger().click();
-    filtersFragment.getFiltersReset().click();
+  describe('And filters are applied', () => {
+    it('should apply correct sorting configuration', () => {
+      [
+        true, //should change the picking list's order
+        false, //should not change the picking list's order
+        true, //should change the picking list's order
+      ].forEach((isChanged, index) => {
+        filtersFragment.getFilterButtonTrigger().click();
 
-    //should close the modal
-    filtersFragment.getFiltersModal().should('not.have.attr', 'open');
+        filtersFragment.getSortingOption(index + 1).click();
 
-    //should not make sort button active
-    filtersFragment.getFilterButtonTrigger().should('not.be.checked');
+        filtersFragment.getFiltersApply().click();
 
-    //should reset the form
-    filtersFragment.getFiltersModal().find('input').eq(0).should('be.checked');
+        //should close the modal
+        filtersFragment.getFiltersModal().should('be.visible');
 
-    //should not change the order
-    largeAndEarliestOrderFirst();
+        //should apply sorting order
+        filtersFragment.isPickingListsOrderChanged(isChanged);
+
+        //should make sort button active
+        filtersFragment.getFilterButtonTrigger().should('be.checked');
+      });
+    });
+  });
+
+  describe('And preselected filters are reset', () => {
+    it('should restore default sorting configuration', () => {
+      filtersFragment.getFilterButtonTrigger().click();
+
+      //preselects second sorting option
+      filtersFragment.getSortingOption(1).click();
+      filtersFragment.getFiltersApply().click();
+      filtersFragment.getFilterButtonTrigger().click();
+
+      filtersFragment.getFiltersResetButton().click();
+
+      //should close the modal
+      filtersFragment.getFiltersModal().should('.not.be.visible');
+
+      //should not make sort button active
+      filtersFragment.getFilterButtonTrigger().should('not.be.checked');
+
+      //should reset the form
+      filtersFragment.getSortingOption(0).should('be.checked');
+
+      //should not change the order
+      filtersFragment.isPickingListsOrderChanged();
+    });
   });
 });
--- apps/fulfillment-e2e/src/support/page_fragments/filters.fragment.ts
+++ apps/fulfillment-e2e/src/support/page_fragments/filters.fragment.ts
@@ -1,14 +1,18 @@
+import { PickingListsFragment } from './picking-lists.fragment';
+
+const pickingListsFragment = new PickingListsFragment();
+
 export class FiltersFragment {
   getFilterButton = () => cy.get('oryx-picking-filter-button');
   getFilterButtonTrigger = () =>
     this.getFilterButton().find('oryx-toggle-icon input');
   getFiltersModal = () =>
     this.getFilterButton().find('oryx-picking-filters').find('oryx-modal');
-  getFiltersReset = () =>
+  getFiltersResetButton = () =>
     this.getFilterButton()
       .find('oryx-picking-filters')
       .find('[slot="navigate-back"] button');
-  getFiltersClose = () =>
+  getFiltersCloseButton = () =>
     this.getFiltersModal().find(
       'header oryx-icon-button button[value="cancel"]'
     );
@@ -16,4 +20,12 @@ export class FiltersFragment {
     this.getFilterButton()
       .find('oryx-picking-filters')
       .find('oryx-button[slot="footer"] button');
+  getSortingOption = (eq: number) =>
+    this.getFiltersModal().find('input').eq(eq);
+  isPickingListsOrderChanged = (changed = false) =>
+    pickingListsFragment
+      .getPickingListsItems()
+      .eq(0)
+      .find('.total oryx-icon-button button')
+      .should(!changed ? 'exist' : 'not.exist');
 }
