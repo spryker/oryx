c2cdf696e200f3a0e2262ed95cc97d2274567e28
aleespryker <alvina.lee@spryker.com>
2023-05-30 18:07:46 +0200

    chore: expand fulfillment login e2e tests

--- apps/fulfillment-e2e/src/support/page_fragments/picking-lists.fragment.ts
+++ apps/fulfillment-e2e/src/support/page_fragments/picking-lists.fragment.ts
@@ -2,10 +2,14 @@ import { PickingInProgressModalFragment } from './picking-in-progress-modal.frag
 
 export class PickingListsFragment {
   getWrapper = () => cy.get('oryx-picking-lists');
+  getHeadline = () => cy.get('oryx-picking-lists-header').find('oryx-heading');
   getSearch = () => cy.get('oryx-search');
+  getSearchIcon = () => this.getSearch().find('oryx-icon[type="search"]');
   getSearchInput = () => this.getSearch().find('input[placeholder="Order ID"]');
   getSearchBackButton = () => this.getSearch().find('.back-button');
   getSearchClearButton = () => this.getSearch().find('.clear-button');
+  getUserIcon = () => cy.get('oryx-site-navigation-item');
+  getSortButton = () => this.getWrapper().find('oryx-picking-filter-button');
   getNoItemsFallback = () => cy.get('.no-items-fallback');
   getNoItemsFallbackHeading = () =>
     this.getNoItemsFallback().find('oryx-heading');
