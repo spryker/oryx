a1b28d2ae0f1963508680949178ce0bbe31a1c35
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-06-09 08:07:08 +0200

    refactor: color facet align with component conventions (#425)
    closes [HRZ-2979](https://spryker.atlassian.net/browse/HRZ-2979)

--- libs/domain/search/src/components.ts
+++ libs/domain/search/src/components.ts
@@ -1,5 +1,5 @@
 export * from '../box/src/box.def';
-export * from '../facet-color/src/facet-color.def';
+export * from '../facet-color/facet-color.def';
 export * from '../facet-navigation/src/facet-navigation.def';
 export * from '../facet-value-navigation/facet-value-navigation.def';
 export * from '../facet/src/facet.def';
