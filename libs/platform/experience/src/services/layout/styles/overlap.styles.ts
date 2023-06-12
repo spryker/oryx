40e3b800f9ecb49d4acc33b0c95ea6332f73e011
Aleksey Belan <alexey.belan@spryker.com>
2023-06-13 00:53:38 +0300

    styles

--- libs/platform/experience/src/services/layout/styles/overlap.styles.ts
+++ libs/platform/experience/src/services/layout/styles/overlap.styles.ts
@@ -3,12 +3,12 @@ import { LayoutStyles } from '../layout.model';
 
 export const styles: LayoutStyles = {
   styles: css`
-    :host([overlap]) {
+    :host([oryx-layout-overlap]) {
       display: grid;
     }
 
-    :host([overlap]) > *,
-    :host([overlap]) ::slotted(*) {
+    :host([oryx-layout-overlap]) > *,
+    :host([oryx-layout-overlap]) ::slotted(*) {
       grid-row: 1;
       grid-column: 1;
     }
