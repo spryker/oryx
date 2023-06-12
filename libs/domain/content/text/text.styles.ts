ef8af5e63e598ed4a4210557692e517479d5c96a
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-06-12 16:14:40 +0200

    style lint

--- libs/domain/content/text/text.styles.ts
+++ libs/domain/content/text/text.styles.ts
@@ -3,19 +3,11 @@ import { css } from 'lit';
 export const contentTextStyles = css`
   :host {
     line-height: 1em;
-    /* font-family: 'Kablammo', sans-serif; */
   }
 
   :host > * {
     font-size: inherit;
     font-weight: inherit;
     line-height: inherit;
-    /* -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent; */
-    /* background-image: linear-gradient(
-      45deg,
-      var(--oryx-color-secondary-9),
-      var(--oryx-color-primary-9)
-    ); */
   }
 `;
