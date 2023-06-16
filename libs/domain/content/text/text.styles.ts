ef31f00a56b5dc00e2ce6c1db83e6363c6135e2a
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-06-16 17:24:20 +0200

    stylelint

--- libs/domain/content/text/text.styles.ts
+++ libs/domain/content/text/text.styles.ts
@@ -2,21 +2,19 @@ import { css } from 'lit';
 
 export const contentTextStyles = css`
   :host {
-    /* --oryx-transition-time: 2s; */
-
     font-size: var(--oryx-typography-size);
     line-height: var(--oryx-typography-line);
     font-weight: var(--oryx-typography-weight);
   }
 
   :host([overflow]) {
-    /* max-height: var(--max-height); */
     max-height: calc(var(--oryx-typography-line, 1rem) * var(--max-lines));
     overflow: hidden;
     transition: height var(--oryx-transition-time) ease;
   }
 
   :host([overflow='clamp']) {
+    /* stylelint-disable-next-line */
     display: -webkit-box;
     -webkit-line-clamp: var(--max-lines);
     -webkit-box-orient: vertical;
@@ -27,7 +25,7 @@ export const contentTextStyles = css`
     position: relative;
   }
 
-  :host([overflow='fade']):before {
+  :host([overflow='fade'])::before {
     content: '';
     position: absolute;
     inset-block-end: 0;
@@ -36,6 +34,7 @@ export const contentTextStyles = css`
     height: 100%;
     z-index: 1;
     transition: opacity var(--oryx-transition-time) ease;
+    /* stylelint-disable-next-line */
     background: linear-gradient(
       to bottom,
       transparent,
@@ -44,7 +43,7 @@ export const contentTextStyles = css`
     opacity: 0;
   }
 
-  :host([overflow='fade']:not([toggle])):before {
+  :host([overflow='fade']:not([toggle]))::before {
     opacity: 1;
   }
 `;
