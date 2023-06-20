f96d819703c65b9d81e491c25eccbca7e406560e
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-06-20 20:56:31 +0300

    Fix

--- tools/post-build/fix-dts-paths.js
+++ tools/post-build/fix-dts-paths.js
@@ -8,7 +8,7 @@ function replace(filePath, outFilePath) {
 
   // 1. We are replacing wrong relative paths to di package, changing domain relative folder structure to the flat to make it working inside node_modules
   // 2. We are adding js extension to the `lit-html/directives/ref` path
-  // 2. We are adding js extension to the `lit-html/directive` path
+  // 3. We are adding js extension to the `lit-html/directive` path
   const newCode = code
     .replaceAll(/..\/base\//gs, '')
     .replaceAll('"lit-html/directives/ref"', '"lit-html/directives/ref.js"')
