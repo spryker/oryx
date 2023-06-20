1718304267a8ee0a65cbbcdf47e2db39773a85b1
Alexander Kovalenko <aleksander.kovalenko@spryker.com>
2023-06-20 21:33:14 +0300

    chore: Fixed d.ts paths (#483)
    * Fixed d.ts paths.
    
    * Fix
    
    ---------
    
    Co-authored-by: Alexander Kovalenko <alexander.kovalenko@spryker.com>

--- tools/post-build/fix-dts-paths.js
+++ tools/post-build/fix-dts-paths.js
@@ -8,9 +8,11 @@ function replace(filePath, outFilePath) {
 
   // 1. We are replacing wrong relative paths to di package, changing domain relative folder structure to the flat to make it working inside node_modules
   // 2. We are adding js extension to the `lit-html/directives/ref` path
+  // 3. We are adding js extension to the `lit-html/directive` path
   const newCode = code
     .replaceAll(/..\/base\//gs, '')
-    .replaceAll('"lit-html/directives/ref"', "lit-html/directives/ref.js");
+    .replaceAll('"lit-html/directives/ref"', '"lit-html/directives/ref.js"')
+    .replaceAll('"lit-html/directive"', '"lit-html/directive.js"');
 
   if (code !== newCode) {
     fs.writeFileSync(outFilePath, newCode);
