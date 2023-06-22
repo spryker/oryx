aaf21e3f91881d2535c73f63c11a378802009a20
Aleksey Belan <alexey.belan@spryker.com>
2023-06-23 02:08:29 +0300

    ssr

--- libs/domain/content/article/article.component.ts
+++ libs/domain/content/article/article.component.ts
@@ -1,12 +1,18 @@
 import { ArticleContext, ContentService } from '@spryker-oryx/content';
 import { ContextController } from '@spryker-oryx/core';
 import { resolve } from '@spryker-oryx/di';
-import { computed, signal, signalAware } from '@spryker-oryx/utilities';
+import {
+  computed,
+  hydratable,
+  signal,
+  signalAware,
+} from '@spryker-oryx/utilities';
 import { html, LitElement, TemplateResult } from 'lit';
 import { of } from 'rxjs';
 
 // Temporary implementation
 @signalAware()
+@hydratable()
 export class ArticleComponent extends LitElement {
   protected contentService = resolve(ContentService);
   protected contextController = new ContextController(this);
