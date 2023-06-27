28904aef140e8ea8779c988051e7b14219fd1a9c
Alex Malkevich <3644678+gund@users.noreply.github.com>
2023-06-27 10:38:26 +0200

    feat(i18n): add support for links in translations (#492)
    * feat(i18n): add support for links in translations
    
    * test(i18n): update unit tests for filters feature
    
    * chore(i18n): add demo story for link example
    
    * fix(ssr): add timers API to server runtime

--- libs/platform/i18n/src/lib/i18n/i18n-service-injectable.adapter.ts
+++ libs/platform/i18n/src/lib/i18n/i18n-service-injectable.adapter.ts
@@ -1,5 +1,11 @@
-import { asyncValue, I18nInjectable } from '@spryker-oryx/utilities';
+import {
+  asyncValue,
+  I18nContext,
+  I18nInjectable,
+} from '@spryker-oryx/utilities';
+import { html } from 'lit';
 import { DirectiveResult } from 'lit/directive.js';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { I18nService } from './i18n.service';
 
 /**
@@ -8,7 +14,8 @@ import { I18nService } from './i18n.service';
 export class I18nServiceInjectableAdapter implements I18nInjectable {
   constructor(
     protected i18nService: I18nService,
-    protected asyncDirective = asyncValue
+    protected asyncDirective = asyncValue,
+    protected unsafeHTMLDirective = unsafeHTML
   ) {}
 
   /**
@@ -17,8 +24,14 @@ export class I18nServiceInjectableAdapter implements I18nInjectable {
    */
   translate(
     token: string | readonly string[],
-    context?: Record<string, unknown> | undefined
+    context?: I18nContext
   ): string | DirectiveResult {
-    return this.asyncDirective(this.i18nService.translate(token, context));
+    return this.asyncDirective(
+      this.i18nService.translate(token, context),
+      (text) =>
+        text.hasHtml
+          ? html`${this.unsafeHTMLDirective(text.toString())}`
+          : text.toString()
+    );
   }
 }
