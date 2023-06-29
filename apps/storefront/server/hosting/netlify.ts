19d6ec892fb4407cb7f5a928e613bde4624b0285
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-06-29 16:11:49 +0300

    fix dist folder

--- apps/storefront/server/hosting/netlify.ts
+++ apps/storefront/server/hosting/netlify.ts
@@ -4,7 +4,7 @@ import { storefrontHandler } from '@spryker-oryx/application/lambda';
 const handler = builder((event, context) =>
   storefrontHandler(event, {
     ...context,
-    root: 'file:///var/task/dist/apps/storefront/functions/ssr/index.js',
+    root: 'file:///var/task/apps/storefront/dist/functions/ssr/index.js',
     index: '../../client/index.html',
     entry: '../../server/render.js',
     component: '<oryx-app></oryx-app>',
