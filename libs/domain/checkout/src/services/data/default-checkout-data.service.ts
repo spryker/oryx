156e5c3a58fbee5698fe73505b14bbce41715ad9
dunqan <dunqan@w9.pl>
2023-06-29 18:11:59 +0200

    cart data totals workaround

--- libs/domain/checkout/src/services/data/default-checkout-data.service.ts
+++ libs/domain/checkout/src/services/data/default-checkout-data.service.ts
@@ -1,19 +1,37 @@
-import { CartModificationEnd, CartService } from '@spryker-oryx/cart';
-import { createQuery } from '@spryker-oryx/core';
+import {
+  Cart,
+  CartModificationEnd,
+  CartQualifier,
+  CartQuery,
+  CartService,
+} from '@spryker-oryx/cart';
+import { createQuery, injectQuery } from '@spryker-oryx/core';
 import { inject } from '@spryker-oryx/di';
-import { map, Observable, of, skip, switchMap, take } from 'rxjs';
+import {
+  combineLatest,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  shareReplay,
+  skip,
+  switchMap,
+  take,
+  using,
+} from 'rxjs';
 import { CheckoutData } from '../../models';
 import { CheckoutAdapter } from '../adapter';
+import { CheckoutStateService } from '../state/checkout-state.service';
 import { CheckoutDataService } from './checkout-data.service';
 
 // TODO: on logout, we should clear the data (could be part of logout -> storage)
 export class DefaultCheckoutDataService implements CheckoutDataService {
-  constructor(
-    protected adapter = inject(CheckoutAdapter),
-    protected cartService = inject(CartService)
-  ) {}
+  protected cartId$ = this.cartService.getCart().pipe(
+    map((cart) => cart?.id),
+    distinctUntilChanged()
+  );
 
-  protected cartId$ = this.cartService.getCart().pipe(map((cart) => cart?.id));
+  protected checkoutState$ = this.checkoutState.getAll(false);
 
   protected query$ = createQuery({
     loader: () =>
@@ -24,14 +42,59 @@ export class DefaultCheckoutDataService implements CheckoutDataService {
         )
       ),
     resetOn: [this.cartId$.pipe(skip(1))],
-    refreshOn: [CartModificationEnd],
   });
+
+  protected queryTotals$ = createQuery({
+    loader: () =>
+      combineLatest([this.cartId$, this.checkoutState$]).pipe(
+        take(1),
+        switchMap(([cartId, checkoutState]) => {
+          if (!cartId) return of(null);
+          return this.adapter.get({ ...checkoutState, cartId });
+        })
+      ),
+    resetOn: [this.cartId$.pipe(skip(1))],
+    refreshOn: [CartModificationEnd, this.checkoutState$.pipe(skip(1))],
+    onLoad: [
+      (data) => {
+        const cart = data?.data?.carts?.[0];
+        if (cart) {
+          // we have the most recent cart data, so we can update the cart query
+          this.cartQuery.set({
+            data: (current) => ({ ...(current ?? {}), ...cart }),
+            qualifier: { cartId: cart.id },
+          });
+        }
+      },
+    ],
+  });
+
+  /**
+   * Workaround to receive shipments together with totals
+   * @protected
+   */
+  protected queryTotalsSharedSideEffect$ = this.queryTotals$
+    .get()
+    .pipe(shareReplay({ refCount: true, bufferSize: 1 }));
+
+  constructor(
+    protected adapter = inject(CheckoutAdapter),
+    protected cartService = inject(CartService),
+    protected cartQuery = injectQuery<Cart, CartQualifier>(CartQuery),
+    protected checkoutState = inject(CheckoutStateService)
+  ) {}
 
   get<K extends keyof CheckoutData>(
     key: K
   ): Observable<CheckoutData[K] | undefined> {
-    return this.query$
+    const result = this.query$
       .get()
       .pipe(map((data) => data?.[key] as CheckoutData[K] | undefined));
+
+    return using(
+      // wire in cart totals workaround into cart data subscription
+      () => this.queryTotalsSharedSideEffect$.subscribe(),
+      () => result
+    );
   }
 }
