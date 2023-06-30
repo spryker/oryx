82468f0aa1aa812d5eb9c3e0a9219fee073eca81
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-06-30 12:40:27 +0300

    fix some plugin issues

--- tools/netlify-plugins/cypress-tests-runner/index.js
+++ tools/netlify-plugins/cypress-tests-runner/index.js
@@ -6,23 +6,17 @@ export const onSuccess = ({ inputs }) => {
   console.log(`Deployed site URL: ${siteUrl}`);
   console.log(`Executing "npm run ${npmRunCommand}"...`);
 
+  // go to the root directory
+  process.chdir('../../../');
+
   // sync call is needed here, because if async is used -> netlify will kill child process
   // when the main process is over
-  const cdCommand = spawnSync('cd', ['../../']);
+  const childProcess = spawnSync('npm', ['run', npmRunCommand], { stdio: 'inherit' });
 
-  if (cdCommand.status === 0) {
-    console.log('cd succeeded')
-
-    const childProcess = spawnSync('npm', ['run', npmRunCommand], { stdio: 'inherit' });
-
-    if (childProcess.status === 0) {
-      console.log(`"npm run ${npmRunCommand}" completed successfully.`);
-    } else {
-      console.error(`"npm run ${npmRunCommand}" failed.`);
-      console.error(childProcess.error);
-    }
+  if (childProcess.status === 0) {
+    console.log(`"npm run ${npmRunCommand}" completed successfully.`);
   } else {
-    console.error('cd failed');
-    console.error(cdCommand.error);
+    console.error(`"npm run ${npmRunCommand}" failed.`);
+    console.error(childProcess.error);
   }
 };
