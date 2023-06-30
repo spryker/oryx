49c20dca0eb8a7edfbfcac1385de8616d09238d5
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-06-30 13:08:12 +0300

    change path

--- tools/netlify-plugins/cypress-tests-runner/index.js
+++ tools/netlify-plugins/cypress-tests-runner/index.js
@@ -7,9 +7,7 @@ export const onSuccess = ({ inputs }) => {
   console.log(`Executing "npm run ${npmRunCommand}"...`);
 
   // go to the root directory
-  console.log(__filename)
-  console.log(__dirname)
-  process.chdir('../../../');
+  process.chdir('../../');
 
   // sync call is needed here, because if async is used -> netlify will kill child process
   // when the main process is over
