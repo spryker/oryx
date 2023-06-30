f5a4fb99e1941d082372c782d9d4bb129ad76b5c
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-06-30 14:18:31 +0300

    update scripts, variables, add logs

--- tools/netlify-plugins/cypress-tests-runner/index.js
+++ tools/netlify-plugins/cypress-tests-runner/index.js
@@ -1,23 +1,29 @@
 import { spawnSync } from 'child_process';
 
 export const onSuccess = ({ inputs, constants }) => {
-  const { npmRunCommand } = inputs;
-  const { DEPLOY_URL } = constants;
+  const { nxRunCommand } = inputs;
+  const { DEPLOY_PRIME_URL } = constants;
 
-  console.log(`Deployed site URL: ${DEPLOY_URL}`);
-  console.log(`Executing "npm run ${npmRunCommand}"...`);
+  console.log(constants)
+  console.log(inputs)
+  console.log(`Deployed site URL: ${DEPLOY_PRIME_URL}`);
+  console.log(`Executing "npm run ${nxRunCommand}"...`);
 
   // go to the root directory
   process.chdir('../../');
 
+  // override default Cypress variables
+  process.env.CYPRESS_BASE_URL = DEPLOY_PRIME_URL;
+  process.env.CYPRESS_BROWSER = 'electron';
+
   // sync call is needed here, because if async is used -> netlify will kill child process
   // when the main process is over
-  const childProcess = spawnSync('npm', ['run', npmRunCommand], { stdio: 'inherit' });
+  const childProcess = spawnSync('npx', ['nx', 'run', nxRunCommand], { stdio: 'inherit' });
 
   if (childProcess.status === 0) {
-    console.log(`"npm run ${npmRunCommand}" completed successfully.`);
+    console.log(`"npm run ${nxRunCommand}" completed successfully.`);
   } else {
-    console.error(`"npm run ${npmRunCommand}" failed.`);
+    console.error(`"npm run ${nxRunCommand}" failed.`);
     console.error(childProcess.error);
   }
 };
