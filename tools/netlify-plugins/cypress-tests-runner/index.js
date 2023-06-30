d1a7c047e1140f0c2c1d0bd144a08b072f1e064d
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-06-30 16:26:02 +0300

    cleanup

--- tools/netlify-plugins/cypress-tests-runner/index.js
+++ tools/netlify-plugins/cypress-tests-runner/index.js
@@ -12,16 +12,15 @@ export const onSuccess = ({ inputs }) => {
 
   // override default Cypress variables
   process.env.CYPRESS_BASE_URL = deployURL;
-  process.env.CYPRESS_BROWSER = 'electron';
 
   // sync call is needed here, because if async is used -> netlify will kill child process
   // when the main process is over
   const childProcess = spawnSync('npx', ['nx', 'run', nxRunCommand], { stdio: 'inherit' });
 
   if (childProcess.status === 0) {
-    console.log(`"npm run ${nxRunCommand}" completed successfully.`);
+    console.log(`"npx nx run ${nxRunCommand}" completed successfully.`);
   } else {
-    console.error(`"npm run ${nxRunCommand}" failed.`);
+    console.error(`"npx nx run ${nxRunCommand}" failed.`);
     console.error(childProcess.error);
   }
 };
