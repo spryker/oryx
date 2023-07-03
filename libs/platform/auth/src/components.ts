b89aa575de8b71681f7d701e0000a47ec654412f
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-07-03 10:20:11 +0300

    remove

--- libs/platform/auth/src/components.ts
+++
@@ -1,4 +0,0 @@
-export * from '../login/login.def';
-export * from '../login-link/login-link.def';
-export * from '../oauth-handler/oauth-handler.def';
-
--- libs/platform/auth/src/feature.ts
+++
@@ -1,8 +0,0 @@
-import { AppFeature } from '@spryker-oryx/core';
-import * as components from './components';
-
-export const authComponents = Object.values(components);
-
-export const checkoutFeature: AppFeature = {
-  components: authComponents,
-};
