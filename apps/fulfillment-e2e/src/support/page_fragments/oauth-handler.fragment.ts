28cc95785555c9325548b2f9fff23e37cfd25d16
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-07-03 19:35:23 +0300

    Fix

--- apps/fulfillment-e2e/src/support/page_fragments/oauth-handler.fragment.ts
+++ apps/fulfillment-e2e/src/support/page_fragments/oauth-handler.fragment.ts
@@ -1,5 +1,5 @@
 export class OauthHandlerFragment {
-  getWrapper = () => cy.get('oryx-oauth-handler');
+  getWrapper = () => cy.get('oryx-auth-oauth-handler');
   getLogo = () => this.getWrapper().find('oryx-image[resource="logo"]');
   getTitle = () => this.getWrapper().find('oryx-heading');
   getSpinner = () => this.getWrapper().find('oryx-spinner');
