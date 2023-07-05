f716860e869b4221f6d84809251cd8113d9a204e
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-07-05 16:41:53 +0300

    One chunk

--- apps/testfront/vite.config.ts
+++ apps/testfront/vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig((config) => {
     build: {
       outDir: join(viteConfig.build.outDirRoot, viteConfig.build.index),
       emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          manualChunks: () => 'index'
+        }
+      }
     },
     publicDir: '../../../libs/template/presets/public',
     plugins: [splitVendorChunkPlugin()],
