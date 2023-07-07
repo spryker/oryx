66ee445974c7f8fd0ad5a133c6f4bdf2359ea949
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-07-07 14:03:28 +0300

    Fix

--- libs/template/application/server/src/server.prod.ts
+++ libs/template/application/server/src/server.prod.ts
@@ -31,7 +31,7 @@ export async function createProdSever(
 
       res.status(200).end(html);
     } catch (e) {
-      res.status(500).end((e as Event).toString());
+      res.status(500).end(`Message: ${(e as Error).message}, Stack: ${(e as Error).stack}`);
     }
   });
 }
