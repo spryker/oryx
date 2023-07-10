e6d4326caca5f6863bc7fa1e5a526e385a364975
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-07-10 10:42:50 +0200

    simplifications

--- libs/template/themes/design-tokens/src/backoffice/typography.tokens.ts
+++ libs/template/themes/design-tokens/src/backoffice/typography.tokens.ts
@@ -3,20 +3,24 @@ import { ThemeToken } from '@spryker-oryx/experience';
 export const typographyTokens: ThemeToken = {
   typography: {
     body: { font: '"Montserrat", sans-serif', weight: '500' },
+    subtitle: { size: '1rem', line: '1.5714285714em', weight: '500' },
+    'subtitle-small': {
+      size: '0.8571428571rem',
+      line: '1.1428571429rem',
+      weight: '400',
+    },
+    caption: { size: '0.8571428571rem', line: '1.3333333333em', weight: '600' },
   },
 };
 
 export const typographySmallTokens: ThemeToken = {
   typography: {
-    h1: { size: '1.285rem', line: '1.3636363636em', weight: '600' },
+    h1: { size: '1.5714285714rem', line: '1.3636363636em', weight: '600' },
     h2: { size: '1.2857142857rem', line: '1.4444444444em', weight: '700' },
     h3: { size: '1.1428571429rem', line: '1.375em', weight: '600' },
     h4: { size: '1rem', line: '1.5714285714em', weight: '600' },
     h5: { size: '1rem', line: '1.5714285714em', weight: '700' },
     h6: { size: '0.8571428571rem', line: '1.3333333333em', weight: '600' },
-    subtitle: { size: '1rem', line: '1.5714285714em', weight: '500' },
-    'subtitle-small': { size: '0.857rem', line: '1.143em', weight: '400' },
-    caption: { size: '0.8571428571rem', line: '1.33em', weight: '600' },
   },
 };
 
@@ -28,8 +32,5 @@ export const typographyMediumAndLargerTokens: ThemeToken = {
     h4: { size: '1.2857142857rem', line: '1.4444444444em', weight: '600' },
     h5: { size: '1.1428571429rem', line: '1.375em', weight: '600' },
     h6: { size: '1.1428571429rem', line: '1.375em', weight: '500' },
-    subtitle: { size: '1rem', line: '1.5714285714em', weight: '500' },
-    'subtitle-small': { size: '0.857rem', line: '1.143em', weight: '400' },
-    caption: { size: '0.8571428571rem', line: '1.33em', weight: '600' },
   },
 };
--- libs/template/themes/design-tokens/src/storefront/typography.tokens.ts
+++ libs/template/themes/design-tokens/src/storefront/typography.tokens.ts
@@ -3,6 +3,18 @@ import { ThemeToken } from '@spryker-oryx/experience';
 export const typographyTokens: ThemeToken = {
   typography: {
     body: { font: `'Montserrat', sans-serif`, weight: '500', line: '22px' },
+    subtitle: {
+      size: '0.8571428571rem',
+      line: '1.3333333333em',
+      weight: '600',
+    },
+    'subtitle-small': {
+      size: '0.8571428571rem',
+      line: '1.3333333333em',
+      weight: '600',
+    },
+    small: { size: '0.8571428571rem', line: '1.3333333333em', weight: '500' },
+    caption: { size: '0.8571428571rem', line: '1.3333333333em', weight: '600' },
   },
 };
 
@@ -19,18 +31,6 @@ export const typographySmallTokens: ThemeToken = {
     h4: { size: '1rem', line: '1.5714285714em', weight: '600' },
     h5: { size: '1rem', line: '1.5714285714em', weight: '700' },
     h6: { size: '0.8571428571rem', line: '1.3333333333em', weight: '600' },
-    subtitle: {
-      size: '0.8571428571rem',
-      line: '1.3333333333em',
-      weight: '600',
-    },
-    'subtitle-small': {
-      size: '0.8571428571rem',
-      line: '1.3333333333em',
-      weight: '600',
-    },
-    caption: { size: '0.8571428571rem', line: '1.3333333333em', weight: '600' },
-    small: { size: '0.8571428571rem', line: '1.3333333333em', weight: '500' },
     bold: { size: '1.1428571429rem', line: '1em', weight: '600' },
   },
 };
@@ -43,18 +43,6 @@ export const typographyMediumAndLargerTokens: ThemeToken = {
     h4: { size: '1.2857142857rem', line: '1.4444444444em', weight: '500' },
     h5: { size: '1.1428571429rem', line: '1.5em', weight: '600' },
     h6: { size: '1.1428571429rem', line: '1.5em', weight: '500' },
-    subtitle: {
-      size: '0.8571428571rem',
-      line: '1.3333333333em',
-      weight: '600',
-    },
-    'subtitle-small': {
-      size: '0.8571428571rem',
-      line: '1.3333333333em',
-      weight: '600',
-    },
-    caption: { size: '0.8571428571rem', line: '1.3333333333em', weight: '600' },
-    small: { size: '0.8571428571rem', line: '1.3333333333em', weight: '500' },
     bold: { size: '1rem', weight: '600' },
   },
 };
