3c58b9c6622ecdbe4d7c57d5e98b689b22c14884
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-07-11 11:13:21 +0200

    fix

--- libs/domain/cart/add/src/add.component.ts
+++ libs/domain/cart/add/src/add.component.ts
@@ -83,7 +83,7 @@ export class CartAddComponent extends ProductMixin(
   protected $hasStock = computed(() => {
     return (
       this.$product()?.availability?.isNeverOutOfStock ||
-      this.$max() > this.$min()
+      (this.$max() && this.$max() >= this.$min())
     );
   });
 
