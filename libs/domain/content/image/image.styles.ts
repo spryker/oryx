218c624d0db08230eaf04b5d367335a271986a47
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-07-18 14:06:34 +0200

    fix image layout

--- libs/domain/content/image/image.styles.ts
+++ libs/domain/content/image/image.styles.ts
@@ -2,10 +2,12 @@ import { css } from 'lit';
 
 export const contentImageStyles = css`
   :host {
+    display: flex;
     height: inherit;
   }
 
   a {
+    display: contents;
     color: inherit;
   }
 `;
