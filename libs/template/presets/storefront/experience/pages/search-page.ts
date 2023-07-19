ca78d9ae5edbe9527fd8d078f238c80dad608377
Aleksey Belan <alexey.belan@spryker.com>
2023-07-19 19:29:43 +0300

    refactor(experience): removes data from options\content from the component interface (#625)
    Removes data from options\content from the Component interface
    
    closes: HRZ-3278

--- libs/template/presets/storefront/experience/pages/search-page.ts
+++ libs/template/presets/storefront/experience/pages/search-page.ts
@@ -7,56 +7,52 @@ export const searchPage: StaticComponent = {
     route: '/search',
   },
   options: {
-    data: {
-      rules: [
-        {
-          layout: 'split-aside',
-          padding: '30px 0 0',
-        },
-        { query: { breakpoint: 'md' }, splitColumnFactor: 1 / 3 },
-      ],
-    },
+    rules: [
+      {
+        layout: 'split-aside',
+        padding: '30px 0 0',
+      },
+      { query: { breakpoint: 'md' }, splitColumnFactor: 1 / 3 },
+    ],
   },
   components: [
     {
       type: 'oryx-search-facet-navigation',
       options: {
-        data: {
-          rules: [
-            {
-              layout: 'grid',
-              gap: '1px',
-              divider: true,
-              sticky: true,
-              top: '108px',
-              margin: '0 0 30px',
-            },
-          ],
-        },
+        rules: [
+          {
+            layout: 'grid',
+            gap: '1px',
+            divider: true,
+            sticky: true,
+            top: '108px',
+            margin: '0 0 30px',
+          },
+        ],
       },
     },
     {
       type: 'oryx-composition',
       name: 'Product listing',
       options: {
-        data: { rules: [{ layout: 'flex', vertical: true, gap: '20px' }] },
+        rules: [{ layout: 'flex', vertical: true, gap: '20px' }],
       },
       components: [
         {
           type: 'oryx-composition',
           name: 'Product list header',
           components: [{ type: 'oryx-search-product-sort' }],
-          options: { data: { rules: [{ layout: 'flex', justify: 'end' }] } },
+          options: { rules: [{ layout: 'flex', justify: 'end' }] },
         },
         {
           type: 'oryx-product-list',
           options: {
-            data: { rules: [{ layout: 'grid', gap: '30px' }] },
+            rules: [{ layout: 'grid', gap: '30px' }],
           },
         },
         {
           type: 'oryx-search-pagination',
-          options: { data: { rules: [{ margin: '0 auto 20px' }] } },
+          options: { rules: [{ margin: '0 auto 20px' }] },
         },
       ],
     },
