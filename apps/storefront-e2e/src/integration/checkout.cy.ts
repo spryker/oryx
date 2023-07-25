5fbb060d3d2b46e6185aa97bf8b5dc7212c257bf
aleespryker <alvina.lee@spryker.com>
2023-07-25 17:52:15 +0200

    fix: fix e2e test

--- apps/storefront-e2e/src/integration/checkout.cy.ts
+++ apps/storefront-e2e/src/integration/checkout.cy.ts
@@ -75,6 +75,7 @@ describe('Checkout suite', () => {
 
         // check that the cart is cleared
         thankYouPage.header.getCartCount().should('not.exist');
+        thankYouPage.header.getCartSummary().click();
 
         cartPage.hasEmptyCart();
       });
