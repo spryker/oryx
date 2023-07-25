604620b6a29b20d27ef96498f3666859bc2d10c6
Aleksey Belan <alexey.belan@spryker.com>
2023-07-25 12:13:02 +0300

    fix

--- libs/template/application/oryx-app/oryx-app.component.ts
+++ libs/template/application/oryx-app/oryx-app.component.ts
@@ -11,6 +11,7 @@ export class OryxAppComponent extends LitElement {
 
   protected override render(): TemplateResult {
     return html`
+      <oryx-composition uid="header"></oryx-composition>
       ${this.router.outlet()}
       <oryx-composition uid="footer"></oryx-composition>
     `;
