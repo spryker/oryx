56e1e6a5dbf39f3dfbb194a8579bf2d20edd253f
Aleksey Belan <alexey.belan@spryker.com>
2023-07-27 11:02:26 +0300

    fix

--- libs/template/presets/storefront/experience/experience-data.ts
+++ libs/template/presets/storefront/experience/experience-data.ts
@@ -21,21 +21,6 @@ import {
 export const StaticExperienceFeature: AppFeature = {
   providers: [
     provideExperienceData([
-      {
-        merge: {
-          type: 'replace',
-          selector:
-            'footer.footerLinks.oryx-composition.topFooter.selfService.oryx-content-text',
-        },
-        type: 'oryx-content-text',
-        content: {
-          data: {
-            text: 'I am after all occurrences of the oryx-product-title component',
-          },
-        },
-      },
-    ]),
-    provideExperienceData([
       HeaderTemplate,
       FooterTemplate,
       cartPage,
