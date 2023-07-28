02ec411feafa7465739dfb8847012dfa8bc33396
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-07-28 15:20:14 +0300

    Formatting

--- tools/utils/multi-copy.js
+++ tools/utils/multi-copy.js
@@ -28,9 +28,6 @@ async function main(files) {
     throw new NoDestError();
   }
 
-  log('DEBUG! Copy sources:', ...sources);
-  log('DEBUG! Copy destinations:', destinations);
-
   info(`Expanded destinations:\n\t${destinations.join('\n\t')}`);
 
   await chainAsync(destinations, (destination) => {
