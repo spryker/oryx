e4afdab2fbdd89d19e5500ab70f41122d09e7e28
Aleksey Belan <alexey.belan@spryker.com>
2023-08-10 12:11:48 +0300

    fix

--- libs/domain/order/src/services/order.routes.ts
+++ libs/domain/order/src/services/order.routes.ts
@@ -1,7 +1,7 @@
 import { resolve } from '@spryker-oryx/di';
 import { RouteType } from '@spryker-oryx/router';
 import { RouteConfig } from '@spryker-oryx/router/lit';
-import { map, take } from 'rxjs';
+import { combineLatest, map, take } from 'rxjs';
 import { OrderService } from './order.service';
 
 export const orderRoutes: RouteConfig[] = [
@@ -9,12 +9,16 @@ export const orderRoutes: RouteConfig[] = [
     path: '/order/:id',
     type: RouteType.Order,
     enter: ({ id }) =>
-      resolve(OrderService)
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        .get({ id: id! })
-        .pipe(
-          take(1),
-          map((order) => (order ? true : RouteType.NotFound))
-        ),
+      combineLatest([
+        resolve(OrderService)
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          .get({ id: id! }),
+        resolve(OrderService).getLastOrder(),
+      ]).pipe(
+        take(1),
+        map(([order, last]) =>
+          order || last?.id === id ? true : RouteType.NotFound
+        )
+      ),
   },
 ];
