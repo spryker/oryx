79e11cbf76ff740baef12c085922195d8bcac8da
Alexander Kovalenko <aleksander.kovalenko@spryker.com>
2023-08-11 10:49:25 +0300

    feat(preset): add b2b preset (#700)
    closes: [HRZ-89479](https://spryker.atlassian.net/browse/HRZ-89479),
    [HRZ-89480](https://spryker.atlassian.net/browse/HRZ-89480)
    
    
    [HRZ-89479]:
    https://spryker.atlassian.net/browse/HRZ-89479?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [HRZ-89480]:
    https://spryker.atlassian.net/browse/HRZ-89480?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- apps/storefront/src/app.ts
+++ apps/storefront/src/app.ts
@@ -1,12 +1,13 @@
 import { appBuilder } from '@spryker-oryx/application';
 import { labsFeatures } from '@spryker-oryx/labs';
+import { b2bStorefrontFeatures } from '@spryker-oryx/presets/b2b-storefront';
 import { storefrontFeatures } from '@spryker-oryx/presets/storefront';
 import { storefrontTheme } from '@spryker-oryx/themes';
 
 const env = import.meta.env;
 
 const features = [
-  ...storefrontFeatures,
+  ...(env.ORYX_PRESET ? b2bStorefrontFeatures : storefrontFeatures),
   ...(env.ORYX_LABS ? labsFeatures : []),
 ];
 
---
+++ libs/template/presets/b2b-storefront/app.ts
@@ -0,0 +1,4 @@
+import { AppFeature } from '@spryker-oryx/core';
+import { storefrontFeatures } from '@spryker-oryx/presets/storefront';
+
+export const b2bStorefrontFeatures: AppFeature[] = [...storefrontFeatures];
