c9220c7b06c7a62bbdecdcfe140dfd6df0bfa019
Nuno Silva <nuno.silva@spryker.com>
2023-08-15 18:30:54 +0100

    feat: update unit test

--- libs/domain/site/price-mode-selector/price-mode-selector.component.spec.ts
+++ libs/domain/site/price-mode-selector/price-mode-selector.component.spec.ts
@@ -1,14 +1,35 @@
 import { fixture, html } from '@open-wc/testing-helpers';
-import { destroyInjector } from '@spryker-oryx/di';
+import { createInjector, destroyInjector } from '@spryker-oryx/di';
 import { useComponent } from '@spryker-oryx/utilities';
+import { PriceModeService } from '../src/services';
 import { SitePriceModeSelectorComponent } from './price-mode-selector.component';
 import { sitePriceModeSelectorComponent } from './price-mode-selector.def';
 
-describe('SiteCurrencySelectorComponent', () => {
+class MockPriceModeService implements Partial<PriceModeService> {
+  get = vi.fn().mockReturnValue('GROSS_MODE');
+  set = vi.fn();
+}
+
+describe('SitePriceModeSelectorComponent', () => {
   let element: SitePriceModeSelectorComponent;
+  let priceModeService: MockPriceModeService;
 
   beforeAll(async () => {
     await useComponent([sitePriceModeSelectorComponent]);
+  });
+
+  beforeEach(async () => {
+    const injector = createInjector({
+      providers: [
+        {
+          provide: PriceModeService,
+          useClass: MockPriceModeService,
+        },
+      ],
+    });
+    priceModeService = injector.inject(
+      PriceModeService
+    ) as unknown as MockPriceModeService;
   });
 
   afterEach(() => {
