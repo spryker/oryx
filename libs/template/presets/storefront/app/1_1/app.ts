517a87c47d88d858d68b9d3c47a081501e9b2541
Aleksey Belan <alexey.belan@spryker.com>
2023-08-17 12:43:52 +0300

    fix

--- libs/template/presets/storefront/app/1_1/app.ts
+++ libs/template/presets/storefront/app/1_1/app.ts
@@ -5,13 +5,17 @@ import { checkoutFeature } from '@spryker-oryx/checkout';
 import { contentFeature } from '@spryker-oryx/content';
 import { AppFeature, coreFeature } from '@spryker-oryx/core';
 import { coreServerProviders } from '@spryker-oryx/core/server';
-import { experienceRoutesFeature_1_1 } from '@spryker-oryx/experience';
+import {
+  experienceFeature_1_1,
+  experiencePreviewFeature_1_1,
+  experienceRoutesFeature_1_1,
+} from '@spryker-oryx/experience';
 import { formFeature } from '@spryker-oryx/form';
 import { I18nFeature } from '@spryker-oryx/i18n';
 import { orderFeature } from '@spryker-oryx/order';
 import { productFeature } from '@spryker-oryx/product';
 import { RouterFeature } from '@spryker-oryx/router';
-import { searchFeature } from '@spryker-oryx/search';
+import { searchFeature, searchPreviewProviders } from '@spryker-oryx/search';
 import { siteFeature } from '@spryker-oryx/site';
 import { uiFeature } from '@spryker-oryx/ui';
 import { userFeature } from '@spryker-oryx/user';
@@ -19,6 +23,10 @@ import { isServer } from 'lit';
 import 'urlpattern-polyfill';
 import { storefrontResources } from '../1_0';
 import { staticExperienceLatestFeature } from './experience';
+
+const isPreview = new URLSearchParams(
+  new URL(globalThis.location?.href).search
+).has('ebPreview');
 
 export const storefrontResources_1_1 = storefrontResources;
 
@@ -40,6 +48,10 @@ export const storefrontFeatures_1_1: AppFeature[] = [
   applicationFeature,
   userFeature,
   isServer ? { providers: coreServerProviders } : {},
+  ...(isPreview
+    ? [experiencePreviewFeature_1_1, { providers: searchPreviewProviders }]
+    : [{}]),
+  experienceFeature_1_1,
   experienceRoutesFeature_1_1,
   {
     resources: storefrontResources_1_1,
