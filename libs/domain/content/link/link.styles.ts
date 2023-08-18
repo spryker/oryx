bc520fa37a5936dd3182be6a44bd9c9ad3769748
aleespryker <alvina.lee@spryker.com>
2023-08-18 13:48:43 +0200

    fix: add missing file

---
+++ libs/domain/content/link/link.styles.ts
@@ -0,0 +1,13 @@
+import { css } from 'lit';
+
+export const styles = css`
+  :host([dropdown]) {
+    --oryx-button-color: var(--oryx-color-neutral-12);
+
+    padding: 9px 15px;
+  }
+
+  :host([dropdown]:hover) {
+    background-color: var(--oryx-color-neutral-3);
+  }
+`;
