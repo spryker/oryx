72a4ea3456d8bc4d3299264336c3fccbebacbc8a
aleespryker <alvina.lee@spryker.com>
2023-08-23 16:19:00 +0200

    chore: add missing jsdocs for SiteMenuItemOptions

--- libs/domain/site/menu-item/menu-item.model.ts
+++ libs/domain/site/menu-item/menu-item.model.ts
@@ -3,8 +3,23 @@ export interface SiteMenuItemContent {
 }
 
 export interface SiteMenuItemOptions {
+  /**
+   * url item should link to.
+   */
   url?: string;
+
+  /**
+   * route for url.
+   */
   type?: string;
+
+  /**
+   * id after the type in the url.
+   */
   id?: string;
+
+  /**
+   * Supports displaying a prefixed icon.
+   */
   icon?: string;
 }
