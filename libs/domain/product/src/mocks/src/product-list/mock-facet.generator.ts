79b50b5836f161b3b5dad16e215cbd1a932383b2
taras.hailuk <tarashaliuk@gmail.com>
2023-08-24 14:39:06 +0200

    chore: revert changes

--- libs/domain/product/src/mocks/src/product-list/mock-facet.generator.ts
+++ libs/domain/product/src/mocks/src/product-list/mock-facet.generator.ts
@@ -4,7 +4,6 @@ export function generateValues(
   count: number,
   prefix = 'Mock',
   selectedValues?: string[],
-  disabledValues?: string[],
   children?: boolean
 ): FacetValue[] {
   const values: FacetValue[] = [];
@@ -18,9 +17,6 @@ export function generateValues(
       count: Number(`${i}0`),
       name: `${prefix}${i}`,
       ...(children ? { children: generateValues(3, `Sub-${prefix}`) } : {}),
-      disabled:
-        (disabledValues && disabledValues?.indexOf(`${prefix}${i}`) >= 0) ??
-        false,
     });
   }
 
@@ -32,7 +28,6 @@ export const generateFacet = (
   parameter: string,
   valuesLength: number,
   selectedValues?: string[],
-  disabledValues?: string[],
   children = false
 ): Facet => {
   return {
@@ -40,13 +35,7 @@ export const generateFacet = (
     parameter,
     valuesTreeLength: valuesLength,
     ...(selectedValues && { selectedValues }),
-    values: generateValues(
-      valuesLength,
-      name,
-      selectedValues,
-      disabledValues,
-      children
-    ),
+    values: generateValues(valuesLength, name, selectedValues, children),
   };
 };
 
--- libs/domain/product/src/mocks/src/product-list/mock-product-list.adapter.ts
+++ libs/domain/product/src/mocks/src/product-list/mock-product-list.adapter.ts
@@ -56,14 +56,7 @@ export class MockProductListAdapter implements ProductListAdapter {
           10,
           qualifier.category?.split(',')
         ),
-        generateFacet(
-          'Label',
-          'label',
-          3,
-          qualifier.label?.split(','),
-          [],
-          true
-        ),
+        generateFacet('Label', 'label', 3, qualifier.label?.split(','), true),
         generateRatingFacet(),
         generateFacet('Color', 'color', 6, qualifier.color?.split(',')),
       ],
