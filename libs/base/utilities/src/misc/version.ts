7b5a5f23a075adb7ebd4fa1ea074e91b8cc8b8dc
dunqan <dunqan@w9.pl>
2023-08-25 13:54:48 +0200

    fix version setting

--- libs/base/utilities/src/misc/version.ts
+++ libs/base/utilities/src/misc/version.ts
@@ -1,7 +1,7 @@
 declare const __ORYX_FEATURE_LEVEL__: number;
 
 // this one should be adjusted per major version
-const defaultLevel = 100;
+const defaultLevel = 1_0;
 const latest = 999999;
 
 export const OryxFeatureLevel =
