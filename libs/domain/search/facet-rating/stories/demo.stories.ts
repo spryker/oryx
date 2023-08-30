9f4b942c48ec7417972ca91ba455a8ecf4b95386
tarashaliuk <43376934+tarashaliuk@users.noreply.github.com>
2023-08-30 16:44:25 +0300

    test(product): integrate facet rating component in storybook (#728)
    Created demo and static stories of facet rating component
    
    closes: https://spryker.atlassian.net/browse/HRZ-89594

---
+++ libs/domain/search/facet-rating/stories/demo.stories.ts
@@ -0,0 +1,32 @@
+import { resolve } from '@spryker-oryx/di';
+import { MockRouterService } from '@spryker-oryx/experience/mocks';
+import { RouterService } from '@spryker-oryx/router';
+import { SearchFacetComponentAttributes } from '@spryker-oryx/search/facet';
+import { Story } from '@storybook/web-components';
+import { TemplateResult, html } from 'lit';
+import { storybookPrefix } from '../../.constants';
+
+export default {
+  title: `${storybookPrefix}/Facet Rating`,
+  args: {
+    open: true,
+  },
+  parameters: {
+    chromatic: {
+      disableSnapshot: true,
+    },
+  },
+};
+
+const Template: Story<SearchFacetComponentAttributes> = (
+  attrs
+): TemplateResult => {
+  resolve<MockRouterService>(RouterService).params$.next({});
+
+  return html`<oryx-search-facet-rating
+    name="Rating"
+    ?open=${attrs.open}
+  ></oryx-search-facet-rating>`;
+};
+
+export const Demo = Template.bind({});
