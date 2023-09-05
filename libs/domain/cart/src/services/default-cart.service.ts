889090bb967539c6f023bf520571fb365175dfcb
Nuno Silva <nuno.silva@spryker.com>
2023-09-05 15:54:06 +0100

    feat: update cart update subscription

--- libs/domain/cart/src/services/default-cart.service.ts
+++ libs/domain/cart/src/services/default-cart.service.ts
@@ -116,14 +116,16 @@ export class DefaultCartService implements CartService {
     PriceModeChanged,
     ({ event }) => {
       if (event.data) {
-        return this.getCart().pipe(
-          take(1),
-          switchMap((cart) => {
-            return this.adapter.update({
-              cartId: cart?.id,
-              priceMode: event.data?.priceMode,
-            });
-          })
+        return subscribeReplay(
+          this.getCart().pipe(
+            take(1),
+            switchMap((cart) => {
+              return this.adapter.update({
+                cartId: cart?.id,
+                priceMode: event.data?.priceMode,
+              });
+            })
+          )
         );
       }
 
