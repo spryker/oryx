456955744ff38a9a126c69e80b9284ccd5b3226b
Aleksey Belan <alexey.belan@spryker.com>
2023-09-10 18:51:05 +0300

    storybook

--- libs/domain/content/video/video.stories.ts
+++ libs/domain/content/video/video.stories.ts
@@ -1,6 +1,6 @@
 import { Meta, Story } from '@storybook/web-components';
 import { TemplateResult, html } from 'lit';
-import { storybookPrefix } from '../../.constants';
+import { storybookPrefix } from '../.constants';
 import { ContentVideoOptions } from './video.model';
 
 export default {
