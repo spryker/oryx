fdb4fcd70098ef13e358e69ae982aa6bfb384eb8
Aleksey Belan <alexey.belan@spryker.com>
2023-09-11 11:35:47 +0300

    drop guards

--- libs/template/labs/src/articles/article-routes.ts
+++ libs/template/labs/src/articles/article-routes.ts
@@ -1,40 +1,38 @@
-import { ContentFields, ContentService } from '@spryker-oryx/content';
-import { resolve } from '@spryker-oryx/di';
-import { RouteType } from '@spryker-oryx/router';
+import { ContentFields } from '@spryker-oryx/content';
 import { RouteConfig } from '@spryker-oryx/router/lit';
-import { map, take } from 'rxjs';
 
 export const articleRoutes: RouteConfig[] = [
   {
     path: '/faq/:id',
     type: ContentFields.Faq,
-    enter: ({ id }) =>
-      resolve(ContentService)
-        .get({
-          id,
-          type: ContentFields.Faq,
-          entities: [ContentFields.Faq],
-        })
-        .pipe(
-          take(1),
-          map((faq) => (faq ? true : RouteType.NotFound))
-        ),
+    // TODO: commented out
+    // enter: ({ id }) =>
+    //   resolve(ContentService)
+    //     .get({
+    //       id,
+    //       type: ContentFields.Faq,
+    //       entities: [ContentFields.Faq],
+    //     })
+    //     .pipe(
+    //       take(1),
+    //       map((faq) => (faq ? true : RouteType.NotFound))
+    //     ),
   },
   {
     path: '/article/:id',
     type: ContentFields.Article,
-    enter: ({ id }) =>
-      resolve(ContentService)
-        .get({
-          id,
-          type: ContentFields.Article,
-          entities: [ContentFields.Article],
-        })
-        .pipe(
-          take(1),
-          map((article) => {
-            return article ? true : RouteType.NotFound;
-          })
-        ),
+    // enter: ({ id }) =>
+    //   resolve(ContentService)
+    //     .get({
+    //       id,
+    //       type: ContentFields.Article,
+    //       entities: [ContentFields.Article],
+    //     })
+    //     .pipe(
+    //       take(1),
+    //       map((article) => {
+    //         return article ? true : RouteType.NotFound;
+    //       })
+    //     ),
   },
 ];
