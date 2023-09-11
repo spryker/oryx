db9d64fc5e23c2e409f02c6bcfec08771ce3592b
Aleksey Belan <alexey.belan@spryker.com>
2023-09-11 11:41:48 +0300

    fix

--- libs/domain/product/src/services/routes.ts
+++ libs/domain/product/src/services/routes.ts
@@ -1,19 +1,17 @@
-import { resolve } from '@spryker-oryx/di';
 import { RouteType } from '@spryker-oryx/router';
 import { RouteConfig } from '@spryker-oryx/router/lit';
-import { map, take } from 'rxjs';
-import { ProductService } from './product.service';
 
 export const productRoutes: RouteConfig[] = [
   {
     path: '/product/:sku',
     type: RouteType.Product,
-    enter: ({ sku }) =>
-      resolve(ProductService)
-        .get({ sku })
-        .pipe(
-          take(1),
-          map((product) => (product ? true : RouteType.NotFound))
-        ),
+    // TODO: uncomment when performance issue will be solved
+    // enter: ({ sku }) =>
+    //   resolve(ProductService)
+    //     .get({ sku })
+    //     .pipe(
+    //       take(1),
+    //       map((product) => (product ? true : RouteType.NotFound))
+    //     ),
   },
 ];
