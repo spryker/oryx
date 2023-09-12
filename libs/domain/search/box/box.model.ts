134c8d953d9ef56a9e28e1bd913df3cbd5c272c1
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-09-12 15:43:59 +0200

    docs

--- libs/domain/search/box/box.model.ts
+++ libs/domain/search/box/box.model.ts
@@ -12,6 +12,10 @@ export interface SearchBoxOptions extends SuggestionRendererOptions {
    * Minimum amount of characters required for querying the suggestion
    */
   minChars?: number;
-
+  /**
+   * Provides responsive behavior of the search input element, so that the
+   * search box leaves more space to other elements. This is especially
+   * useful when there's a lack of space, eg. on small screens.
+   */
   float?: boolean;
 }
