b38b8b9d2252242dc1d5e5d0f7f061671f9024df
aleespryker <alvina.lee@spryker.com>
2023-09-15 16:47:03 +0200

    chore: fix formatting

--- libs/domain/picking/product-card/product-card.def.ts
+++ libs/domain/picking/product-card/product-card.def.ts
@@ -3,7 +3,11 @@ import { componentDef } from '@spryker-oryx/utilities';
 export const pickingProductCardComponent = componentDef({
   name: 'oryx-picking-product-card',
   impl: () =>
-    import('./product-card.component').then((m) => m.PickingProductCardComponent),
+    import('./product-card.component').then(
+      (m) => m.PickingProductCardComponent
+    ),
   schema: () =>
-    import('./product-card.schema').then((m) => m.pickingProductCardComponentSchema),
+    import('./product-card.schema').then(
+      (m) => m.pickingProductCardComponentSchema
+    ),
 });
