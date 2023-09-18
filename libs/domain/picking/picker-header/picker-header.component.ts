6c02f83ee89fb6cd49b8cb5bab1204e577435e58
aleespryker <alvina.lee@spryker.com>
2023-09-18 17:14:18 +0200

    chore: use elementalEffect

--- libs/domain/picking/picker-header/picker-header.component.ts
+++ libs/domain/picking/picker-header/picker-header.component.ts
@@ -4,7 +4,7 @@ import { PickingDiscardModalComponent } from '@spryker-oryx/picking/discard-moda
 import { RouterService } from '@spryker-oryx/router';
 import { ButtonSize, ButtonType } from '@spryker-oryx/ui/button';
 import { IconTypes } from '@spryker-oryx/ui/icon';
-import { I18nMixin, signal, signalAware } from '@spryker-oryx/utilities';
+import { I18nMixin, elementEffect, signalAware } from '@spryker-oryx/utilities';
 import { LitElement, TemplateResult, html } from 'lit';
 import { query, state } from 'lit/decorators.js';
 import { tap } from 'rxjs';
@@ -24,7 +24,8 @@ export class PickingPickerHeaderComponent extends I18nMixin(
 
   @state() isCartNoteVisible?: boolean;
 
-  protected $showDialog = signal(
+  @elementEffect()
+  protected showDialog = (): void => {
     this.pickingHeaderService
       .showDialog()
       .pipe(
@@ -32,7 +33,8 @@ export class PickingPickerHeaderComponent extends I18nMixin(
           showDialog ? this.openDiscardModal() : this.closeDiscardModal()
         )
       )
-  );
+      .subscribe();
+  };
 
   protected renderCartNoteButton(): TemplateResult {
     return html`${this.$pickingList()?.cartNote
@@ -55,7 +57,6 @@ export class PickingPickerHeaderComponent extends I18nMixin(
   }
 
   protected override render(): TemplateResult {
-    this.$showDialog();
     return html`
       <oryx-picking-header>
         <oryx-button
