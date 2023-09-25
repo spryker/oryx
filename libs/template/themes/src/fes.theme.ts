9199dbc7e6bc4874910faf759db01ae89df459be
Nuno Silva <nuno.silva@spryker.com>
2023-09-25 12:17:11 +0100

    chore: deprecate fes theme in oryx repo

--- libs/template/themes/src/fes.theme.ts
+++ libs/template/themes/src/fes.theme.ts
@@ -1,20 +1,14 @@
 import { Theme } from '@spryker-oryx/experience';
-import { backofficeIcons, fontawesomeIcons } from '@spryker-oryx/resources';
+import { materialDesignIcons } from '@spryker-oryx/resources';
 import { defaultBreakpoints } from '@spryker-oryx/themes/breakpoints';
 
+/** @deprecated fes theme will be deprecated in upcoming major versions */
 export const fesTheme: Theme = {
   name: 'fes',
   breakpoints: defaultBreakpoints,
   designTokens: () =>
     import('../design-tokens/src/backoffice').then((s) => s.backofficeTokens),
   icons: {
-    resource: fontawesomeIcons,
-    resources: [
-      {
-        resource: backofficeIcons,
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        types: Object.keys(backofficeIcons.mapping!),
-      },
-    ],
+    resource: materialDesignIcons,
   },
 };
