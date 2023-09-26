2d74b0ebf77f28d08d75972031a302ca0e289135
dunqan <dunqan@w9.pl>
2023-09-26 09:18:41 +0200

    fix deprecation notes

--- libs/domain/checkout/src/deprecated-reexports.ts
+++ libs/domain/checkout/src/deprecated-reexports.ts
@@ -4,35 +4,35 @@ import * as services from '../services/src/index';
 const reexports: typeof services =
   featureVersion < '1.1' ? services : (undefined as any);
 
-/** @deprecated since 1.1, use DefaultCartService from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use DefaultCheckoutService from @spryker-oryx/checkout/services */
 export const DefaultCheckoutService = reexports?.DefaultCheckoutService;
-/** @deprecated since 1.1, use DefaultCheckoutAdapter from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use DefaultCheckoutAdapter from @spryker-oryx/checkout/services */
 export const DefaultCheckoutAdapter = reexports?.DefaultCheckoutAdapter;
-/** @deprecated since 1.1, use DefaultCheckoutDataService from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use DefaultCheckoutDataService from @spryker-oryx/checkout/services */
 export const DefaultCheckoutDataService = reexports?.DefaultCheckoutDataService;
-/** @deprecated since 1.1, use DefaultCheckoutStateService from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use DefaultCheckoutStateService from @spryker-oryx/checkout/services */
 export const DefaultCheckoutStateService =
   reexports?.DefaultCheckoutStateService;
-/** @deprecated since 1.1, use checkoutAttributesNormalizer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use checkoutAttributesNormalizer from @spryker-oryx/checkout/services */
 export const checkoutAttributesNormalizer =
   reexports?.checkoutAttributesNormalizer;
-/** @deprecated since 1.1, use checkoutShipmentsNormalizer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use checkoutShipmentsNormalizer from @spryker-oryx/checkout/services */
 export const checkoutShipmentsNormalizer =
   reexports?.checkoutShipmentsNormalizer;
-/** @deprecated since 1.1, use checkoutPaymentsNormalizer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use checkoutPaymentsNormalizer from @spryker-oryx/checkout/services */
 export const checkoutPaymentsNormalizer = reexports?.checkoutPaymentsNormalizer;
-/** @deprecated since 1.1, use  from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use  from @spryker-oryx/checkout/services */
 export const checkoutCartsNormalizer = reexports?.checkoutCartsNormalizer;
-/** @deprecated since 1.1, use checkoutResponseAttributesNormalizer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use checkoutResponseAttributesNormalizer from @spryker-oryx/checkout/services */
 export const checkoutResponseAttributesNormalizer =
   reexports?.checkoutResponseAttributesNormalizer;
-/** @deprecated since 1.1, use paymentsNormalizer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use paymentsNormalizer from @spryker-oryx/checkout/services */
 export const paymentsNormalizer = reexports?.paymentsNormalizer;
-/** @deprecated since 1.1, use shipmentsNormalizer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use shipmentsNormalizer from @spryker-oryx/checkout/services */
 export const shipmentsNormalizer = reexports?.shipmentsNormalizer;
-/** @deprecated since 1.1, use checkoutAttributesSerializer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use checkoutAttributesSerializer from @spryker-oryx/checkout/services */
 export const checkoutAttributesSerializer =
   reexports?.checkoutAttributesSerializer;
-/** @deprecated since 1.1, use checkoutDataAttributesSerializer from @spryker-oryx/cart/services */
+/** @deprecated since 1.1, use checkoutDataAttributesSerializer from @spryker-oryx/checkout/services */
 export const checkoutDataAttributesSerializer =
   reexports?.checkoutDataAttributesSerializer;
