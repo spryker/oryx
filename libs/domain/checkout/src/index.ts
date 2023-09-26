d00b0acd0391a8b0204e86fedf35c3deb6cb8813
dunqan <dunqan@w9.pl>
2023-09-26 09:22:35 +0200

    fix deprecation notes

--- libs/domain/checkout/src/index.ts
+++ libs/domain/checkout/src/index.ts
@@ -1,6 +1,6 @@
 export * from './checkout.providers';
-export * from './deprecated-reexports';
 export * from './feature';
 export * from './mixins';
 export * from './models';
 export * from './services';
+export * from './services-reexports';
