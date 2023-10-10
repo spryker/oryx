043965b6bc7fdfafc674b0ebe1a375515ee7cb77
dunqan <dunqan@w9.pl>
2023-10-10 11:33:01 +0200

    lazy cart providers

--- libs/domain/cart/src/services/cart.providers.ts
+++ libs/domain/cart/src/services/cart.providers.ts
@@ -1,4 +1,5 @@
 import { Provider } from '@spryker-oryx/di';
+import { featureVersion } from '@spryker-oryx/utilities';
 import {
   DefaultCartAdapter,
   DefaultCartService,
@@ -11,35 +12,86 @@ import { CartService } from './cart.service';
 import { CartResourceResolver } from './resolver';
 import { CartTotalsProvider, TotalsService } from './totals';
 
-export const cartNormalizer: Provider[] = [
-  {
-    provide: CartNormalizer,
-    useValue: cartAttributesNormalizer,
-  },
-];
+export const cartNormalizer: Provider[] =
+  featureVersion < '1.2'
+    ? [
+        {
+          provide: CartNormalizer,
+          useValue: cartAttributesNormalizer,
+        },
+      ]
+    : [
+        {
+          provide: CartNormalizer,
+          useValue: () =>
+            import('@spryker-oryx/cart/services').then(
+              (m) => m.cartAttributesNormalizer
+            ),
+        },
+      ];
 
-export const cartsNormalizer: Provider[] = [
-  {
-    provide: CartsNormalizer,
-    useValue: cartsItemsNormalizer,
-  },
-];
+export const cartsNormalizer: Provider[] =
+  featureVersion < '1.2'
+    ? [
+        {
+          provide: CartsNormalizer,
+          useValue: cartsItemsNormalizer,
+        },
+      ]
+    : [
+        {
+          provide: CartsNormalizer,
+          useValue: () =>
+            import('@spryker-oryx/cart/services').then(
+              (m) => m.cartsItemsNormalizer
+            ),
+        },
+      ];
 
-export const cartProviders: Provider[] = [
-  {
-    provide: CartAdapter,
-    useClass: DefaultCartAdapter,
-  },
-  {
-    provide: CartService,
-    useClass: DefaultCartService,
-  },
-  {
-    provide: TotalsService,
-    useClass: DefaultTotalsService,
-  },
-  CartResourceResolver,
-  CartTotalsProvider,
-  ...cartNormalizer,
-  ...cartsNormalizer,
-];
+export const cartProviders: Provider[] =
+  featureVersion < '1.2'
+    ? [
+        {
+          provide: CartAdapter,
+          useClass: DefaultCartAdapter,
+        },
+        {
+          provide: CartService,
+          useClass: DefaultCartService,
+        },
+        {
+          provide: TotalsService,
+          useClass: DefaultTotalsService,
+        },
+        CartResourceResolver,
+        CartTotalsProvider,
+        ...cartNormalizer,
+        ...cartsNormalizer,
+      ]
+    : [
+        {
+          provide: CartAdapter,
+          asyncClass: () =>
+            import('@spryker-oryx/cart/services').then(
+              (m) => m.DefaultCartAdapter
+            ),
+        },
+        {
+          provide: CartService,
+          asyncClass: () =>
+            import('@spryker-oryx/cart/services').then(
+              (m) => m.DefaultCartService
+            ),
+        },
+        {
+          provide: TotalsService,
+          asyncClass: () =>
+            import('@spryker-oryx/cart/services').then(
+              (m) => m.DefaultTotalsService
+            ),
+        },
+        CartResourceResolver,
+        CartTotalsProvider,
+        ...cartNormalizer,
+        ...cartsNormalizer,
+      ];
