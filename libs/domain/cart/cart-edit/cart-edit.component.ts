a31d08fe85186fa2a77058309dda1ca4f9d5804b
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-10-12 08:03:18 +0200

    add default checkbox

--- libs/domain/cart/cart-edit/cart-edit.component.ts
+++ libs/domain/cart/cart-edit/cart-edit.component.ts
@@ -3,7 +3,7 @@ import { LitElement, TemplateResult, css, html } from 'lit';
 
 export class CartEditComponent extends ContentMixin(LitElement) {
   static styles = css`
-    :host {
+    form {
       display: grid;
       grid-template-columns: 1fr 1fr;
       gap: 20px;
@@ -12,29 +12,40 @@ export class CartEditComponent extends ContentMixin(LitElement) {
     oryx-input {
       grid-column: 1 / span 2;
     }
+
+    oryx-button {
+      grid-column: 1;
+    }
   `;
 
   protected override render(): TemplateResult {
     return html`
-      <oryx-input label="Name"
-        ><input placeholder="The name of the shopping cart"
-      /></oryx-input>
+      <oryx-heading tag="h1" as="h3"> Create cart </oryx-heading>
+      <p>You can create multiple carts to organize your running orders.</p>
+      <form>
+        <oryx-input label="Name"
+          ><input placeholder="The name of the shopping cart"
+        /></oryx-input>
 
-      <oryx-select label="currency">
-        <select>
-          <option>EUR</option>
-          <option selected>USD</option>
-        </select>
-      </oryx-select>
+        <oryx-select label="currency">
+          <select>
+            <option>EUR</option>
+            <option selected>USD</option>
+          </select>
+        </oryx-select>
 
-      <oryx-select label="price mode">
-        <select>
-          <option>Gross</option>
-          <option selected>Net</option>
-        </select>
-      </oryx-select>
+        <oryx-select label="price mode">
+          <select>
+            <option>Gross</option>
+            <option selected>Net</option>
+          </select>
+        </oryx-select>
 
-      <oryx-button>create</oryx-button>
+        <oryx-checkbox>
+          <input type="checkbox" checked /> Make this cart my current cart
+        </oryx-checkbox>
+        <oryx-button>create</oryx-button>
+      </form>
     `;
   }
 }
