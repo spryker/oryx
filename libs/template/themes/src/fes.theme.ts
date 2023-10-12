d2d79146d3c083989959a1c8e44cb73a51084f03
nsilva-spryker <105730125+nsilva-spryker@users.noreply.github.com>
2023-10-12 09:41:23 +0100

    chore: update fes theme with icons (#778)
    closes: [HRZ-89914](https://spryker.atlassian.net/browse/HRZ-89914)
    
    
    [HRZ-89874]:
    https://spryker.atlassian.net/browse/HRZ-89874?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    [HRZ-89914]:
    https://spryker.atlassian.net/browse/HRZ-89914?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- libs/template/themes/src/fes.theme.ts
+++ libs/template/themes/src/fes.theme.ts
@@ -2,6 +2,9 @@ import { Theme } from '@spryker-oryx/experience';
 import { materialDesignIcons } from '@spryker-oryx/resources';
 import { defaultBreakpoints } from '@spryker-oryx/themes/breakpoints';
 
+/**
+ * @deprecated since version 1.1 use backofficeTheme instead
+ * */
 export const fesTheme: Theme = {
   name: 'fes',
   breakpoints: defaultBreakpoints,
