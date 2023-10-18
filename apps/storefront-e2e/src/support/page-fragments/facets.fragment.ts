b7d240b7dc269144d06e0e4c15bd4b1704562e0a
taras.hailuk <tarashaliuk@gmail.com>
2023-10-18 23:05:57 +0200

    chore: improve e2e tests

--- apps/storefront-e2e/src/support/page-fragments/facets.fragment.ts
+++ apps/storefront-e2e/src/support/page-fragments/facets.fragment.ts
@@ -1,51 +1,65 @@
 export class FacetsFragment {
   getWrapper = () => cy.get('oryx-search-facet-navigation');
   getSearchFacets = () =>
-    cy.get(
+    this.getWrapper().find(
       'oryx-search-facet, oryx-search-color-facet, oryx-search-facet-rating'
     );
 
   setTouchscreen = (value: 'Yes' | 'No') => {
-    cy.get(`input[type="radio"][name="touchscreen"][value="${value}"]`).check();
+    this.getWrapper()
+      .find(`input[type="radio"][name="touchscreen"][value="${value}"]`)
+      .check();
   };
 
   setBrand = (value: string) => {
-    cy.get(`input[type="radio"][name="brand"][value="${value}"]`).click();
+    this.getWrapper()
+      .find(`input[type="radio"][name="brand"][value="${value}"]`)
+      .click();
   };
 
   resetBrand = () => {
-    cy.contains('slot', 'Brand')
+    this.getWrapper()
+      .contains('slot', 'Brand')
       .parent()
       .find('button[aria-label="Clear"]')
       .click();
   };
 
   setColor = (value: string) => {
-    cy.get(`input[type="checkbox"][name="color"][value="${value}"]`).check();
+    this.getWrapper()
+      .find(`input[type="checkbox"][name="color"][value="${value}"]`)
+      .check();
   };
 
   resetColor = () => {
-    cy.get('oryx-search-color-facet')
+    this.getWrapper()
+      .find('oryx-search-color-facet')
       .find('button[aria-label="Clear"]')
       .click();
   };
 
-  setRating = () => {
-    cy.get(`input[type="radio"][name="rating"][value="4"]`).check();
+  setRating = (value: string) => {
+    this.getWrapper()
+      .find(`input[type="radio"][name="rating"][value="${value}"]`)
+      .check();
   };
 
   resetRating = () => {
-    cy.get('oryx-search-facet-rating')
+    this.getWrapper()
+      .find('oryx-search-facet-rating')
       .find('button[aria-label="Clear"]')
       .click();
   };
 
   setLabel = (value: string) => {
-    cy.get(`input[type="checkbox"][name="label"][value="${value}"]`).check();
+    this.getWrapper()
+      .find(`input[type="checkbox"][name="label"][value="${value}"]`)
+      .check();
   };
 
   resetLabel = () => {
-    cy.contains('slot', 'Product Labels')
+    this.getWrapper()
+      .contains('slot', 'Product Labels')
       .parent()
       .find('button[aria-label="Clear"]')
       .click();
