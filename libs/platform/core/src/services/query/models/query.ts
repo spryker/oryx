e7e77824fffc991f3a19a8e5c1ff2176fe058fc4
dunqan <dunqan@w9.pl>
2023-10-19 13:53:11 +0200

    improve query with cacheKey and postTransform options

--- libs/platform/core/src/services/query/models/query.ts
+++ libs/platform/core/src/services/query/models/query.ts
@@ -3,6 +3,15 @@ import { QueryEventHandler } from './query-event';
 
 export type QueryTrigger = string | Observable<any>;
 
+export type QueryQualifierEnhancer<Qualifier> = (
+  partialQualifier: Partial<Qualifier>
+) => Qualifier /*| Observable<Qualifier>*/;
+
+export type QueryTransformer<ValueType, Qualifier> = (
+  data: ValueType,
+  qualifier: Qualifier
+) => ValueType /*| Observable<ValueType>*/ | void;
+
 export interface QueryOptions<
   ValueType,
   Qualifier extends object | undefined = undefined
@@ -15,6 +24,27 @@ export interface QueryOptions<
 
   onLoad?: QueryEventHandler<ValueType, Qualifier>[];
   onError?: QueryEventHandler<ValueType, Qualifier>[];
+
+  /**
+   * Determines the cache key based on the provided qualifier.
+   * This is useful to cache different data under the same key if they represent the same entity.
+   * If not provided, the default behavior would be using the qualifier itself.
+   */
+  cacheKey?: (qualifier: Qualifier) => string | Partial<Qualifier>;
+
+  /**
+   * TODO
+   * Automatically completes a qualifier if some of its fields are missing.
+   * This is especially useful when only a subset of the qualifier is provided and
+   * the rest can be inferred or fetched from elsewhere.
+   */
+  // qualifierEnhancers?: QueryQualifierEnhancer<Qualifier>[];
+
+  /**
+   * Transforms the data returned by the loader based on the qualifier or the model itself.
+   * Useful for applying context-specific modifications to the model before it's consumed.
+   */
+  postTransforms?: QueryTransformer<ValueType, Qualifier>[];
 
   /**
    * Query is not cached between subscriptions
