5d2edfb613f09a747188f04e5930164cce42dded
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-10-24 16:58:10 +0300

    Adjusted locale import so it's a static string now.

--- libs/template/labs/src/i18n/index.ts
+++ libs/template/labs/src/i18n/index.ts
@@ -1,16 +1,11 @@
 import { I18nFeature } from '@spryker-oryx/i18n';
 
-export * from './translations/de';
-
-// to avoid console errors, we provide a list of supported languages
-const supported = ['en', 'de', 'ar'];
-
 export const labsI18nFeature = new I18nFeature({
   load: (localeId) => {
-    if (supported.includes(localeId)) {
-      return import(`./translations/${localeId}.ts`);
-    } else {
-      return Promise.resolve();
+    switch (localeId) {
+      case 'de': return import(`./translations/de.js`);
+      case 'ar': return import(`./translations/ar.js`);
+      default: return import(`./translations/en.js`);
     }
   },
 });
