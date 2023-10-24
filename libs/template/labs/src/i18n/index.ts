5edcdcc1a105ccb87ab4e8ac6e466f1a612e4719
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-10-24 17:09:39 +0300

    Formatting

--- libs/template/labs/src/i18n/index.ts
+++ libs/template/labs/src/i18n/index.ts
@@ -3,9 +3,12 @@ import { I18nFeature } from '@spryker-oryx/i18n';
 export const labsI18nFeature = new I18nFeature({
   load: (localeId) => {
     switch (localeId) {
-      case 'de': return import(`./translations/de.js`);
-      case 'ar': return import(`./translations/ar.js`);
-      default: return import(`./translations/en.js`);
+      case 'de':
+        return import(`./translations/de.js`);
+      case 'ar':
+        return import(`./translations/ar.js`);
+      default:
+        return import(`./translations/en.js`);
     }
   },
 });
