fdecdf32e33437c680de824be25c6db18a457705
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-10-24 17:13:06 +0300

    Add some explanation.

--- libs/template/labs/src/i18n/index.ts
+++ libs/template/labs/src/i18n/index.ts
@@ -1,5 +1,8 @@
 import { I18nFeature } from '@spryker-oryx/i18n';
 
+// We have to use static strings for imports, as long as vite is differently optimize imports in node_modules and
+// outside of it. This is a workaround for the issue.
+
 export const labsI18nFeature = new I18nFeature({
   load: (localeId) => {
     switch (localeId) {
