19a41eee8f68179b3788ab9899c9c8d1e7345b89
Aleksey Belan <alexey.belan@spryker.com>
2023-11-01 11:29:04 +0200

    fix comment

--- libs/platform/experience/src/static.ts
+++ libs/platform/experience/src/static.ts
@@ -1,1 +1,6 @@
+/**
+ * Do NOT export any async services files in the main entry points, as we'd break lazy loading otherwise.
+ * All services artifacts are publicly available in a static entry point, to allow for customizations.
+ */
+
 export * from './services/layout/plugins/static';
