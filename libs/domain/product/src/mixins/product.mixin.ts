b4142cc21d732d192b6981cb8254269e0a52c733
dunqan <dunqan@w9.pl>
2023-11-08 09:25:40 +0100

    refactor product context to use qualifier

--- libs/domain/product/src/mixins/product.mixin.ts
+++ libs/domain/product/src/mixins/product.mixin.ts
@@ -2,6 +2,7 @@ import { ContextController } from '@spryker-oryx/core';
 import { resolve } from '@spryker-oryx/di';
 import {
   computed,
+  featureVersion,
   Signal,
   signal,
   signalAware,
@@ -9,8 +10,8 @@ import {
   Type,
 } from '@spryker-oryx/utilities';
 import { LitElement } from 'lit';
-import { of } from 'rxjs';
-import type { Product } from '../models';
+import { map, of } from 'rxjs';
+import type { Product, ProductQualifier } from '../models';
 import { ProductComponentProperties } from '../models';
 import { ProductContext, ProductService } from '../services';
 
@@ -19,6 +20,7 @@ export declare class ProductMixinInterface
 {
   sku?: string;
   protected $product: Signal<Product | null>;
+  protected $productQualifier: Signal<ProductQualifier | null>;
 }
 
 export const ProductMixin = <
@@ -34,13 +36,19 @@ export const ProductMixin = <
 
     protected contextController = new ContextController(this);
 
-    protected $productContext = signal(
-      this.contextController.get<string>(ProductContext.SKU)
+    protected $productQualifier: Signal<ProductQualifier | undefined> = signal(
+      this.contextController
+        .get<ProductQualifier>(ProductContext.SKU)
+        .pipe(
+          map((sku) =>
+            featureVersion >= '1.3' ? sku : ({ sku } as ProductQualifier)
+          )
+        )
     );
 
     protected $product = computed(() => {
-      const sku = this.sku ?? this.$productContext();
-      return sku ? this.productService?.get({ sku }) : of(null);
+      const qualifier = this.sku ? { sku: this.sku } : this.$productQualifier();
+      return qualifier ? this.productService?.get(qualifier) : of(null);
     });
   }
   return ProductMixinClass as unknown as Type<ProductMixinInterface> & T;
--- libs/domain/product/src/services/product-context.ts
+++ libs/domain/product/src/services/product-context.ts
@@ -1,16 +1,26 @@
-import { ContextFallback } from '@spryker-oryx/core';
-import { inject, Provider } from '@spryker-oryx/di';
+import { ContextFallback, ContextService } from '@spryker-oryx/core';
+import { Provider, inject } from '@spryker-oryx/di';
 import { RouterService } from '@spryker-oryx/router';
-import { map } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 
 export const enum ProductContext {
   SKU = 'sku',
 }
 
+export function productContextFallbackFactory(
+  router = inject(RouterService),
+  context = inject(ContextService)
+): Observable<unknown> {
+  return router
+    .currentParams()
+    .pipe(
+      switchMap((params) =>
+        context.deserialize(ProductContext.SKU, (params?.sku as string) ?? '')
+      )
+    );
+}
+
 export const ProductContextFallback: Provider = {
   provide: `${ContextFallback}${ProductContext.SKU}`,
-  useFactory: () =>
-    inject(RouterService)
-      .currentParams()
-      .pipe(map((params) => params?.sku)),
+  useFactory: productContextFallbackFactory,
 };
