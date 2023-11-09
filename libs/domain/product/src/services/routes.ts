0318d72361f3c8395e2737c15e9d5bd9109245cc
Aleksey Belan <alexey.belan@spryker.com>
2023-11-09 17:59:42 +0200

    test

--- libs/domain/product/src/services/routes.ts
+++ libs/domain/product/src/services/routes.ts
@@ -1,17 +1,20 @@
+import { resolve } from '@spryker-oryx/di';
 import { RouteType } from '@spryker-oryx/router';
 import { RouteConfig } from '@spryker-oryx/router/lit';
+import { map, take } from 'rxjs';
+import { ProductService } from './product.service';
 
 export const productRoutes: RouteConfig[] = [
   {
     path: '/product/:sku',
     type: RouteType.Product,
-    // enter: ({ sku }) =>
-    //   resolve(ProductService)
-    //     .get({ sku })
-    //     .pipe(
-    //       take(1),
-    //       map((product) => (product ? true : RouteType.NotFound))
-    //     ),
+    enter: ({ sku }) =>
+      resolve(ProductService)
+        .get({ sku })
+        .pipe(
+          take(1),
+          map((product) => (product ? true : RouteType.NotFound))
+        ),
   },
   {
     path: '/search',
