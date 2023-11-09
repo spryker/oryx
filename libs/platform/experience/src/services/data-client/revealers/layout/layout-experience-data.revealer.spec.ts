0e72c05479391931554c75ec9bb09ddfb3d4c477
Aleksey Belan <alexey.belan@spryker.com>
2023-11-09 19:32:44 +0200

    fix

--- libs/platform/experience/src/services/data-client/revealers/layout/layout-experience-data.revealer.spec.ts
+++ libs/platform/experience/src/services/data-client/revealers/layout/layout-experience-data.revealer.spec.ts
@@ -1,10 +1,13 @@
-import { createInjector, destroyInjector } from '@spryker-oryx/di';
+import { nextFrame } from '@open-wc/testing-helpers';
+import { createInjector, destroyInjector, getInjector } from '@spryker-oryx/di';
+import { MessageType, postMessage } from '@spryker-oryx/experience';
 import { FormFieldType } from '@spryker-oryx/form';
 import { of } from 'rxjs';
 import {
   LayoutPlugin,
   LayoutPropertyPlugin,
   LayoutStylesPlugin,
+  LayoutTypeStyles,
 } from '../../../layout';
 import { LayoutExperienceDataRevealer } from './layout-experience-data.revealer';
 
@@ -118,89 +121,88 @@ describe('LayoutExperienceDataRevealer', () => {
     vi.clearAllMocks();
   });
 
-  // TODO: Temporary disabled to unblock release
-  // describe('reveal', () => {
-  //   it('should send `MessageType.StylesOptions` post message with proper data', async () => {
-  //     getInjector().inject('service').reveal().subscribe();
-  //     postMessage(
-  //       {
-  //         type: MessageType.SelectedStyles,
-  //         data: {
-  //           type: 'bLayout',
-  //           bProperty: true,
-  //         } as LayoutTypeStyles,
-  //       },
-  //       window
-  //     );
-  //     await nextFrame();
-  //     expect(window.parent.postMessage).toHaveBeenCalledWith(
-  //       {
-  //         type: MessageType.StylesOptions,
-  //         data: {
-  //           defaults: {
-  //             bSpecialLayout: 'default',
-  //             bSpecialProperty: 'default',
-  //           },
-  //           fields: {
-  //             container: [
-  //               {
-  //                 id: 'layout-type',
-  //                 label: 'layout',
-  //                 type: FormFieldType.Select,
-  //                 options: [{ value: 'aLayout' }, { value: 'bLayout' }],
-  //               },
-  //               {
-  //                 id: 'layout-aProperty',
-  //                 label: 'aProperty',
-  //                 type: FormFieldType.Boolean,
-  //               },
-  //               {
-  //                 id: 'layout-bProperty',
-  //                 label: 'bProperty',
-  //                 type: FormFieldType.Boolean,
-  //               },
-  //             ],
-  //             special: [
-  //               {
-  //                 id: 'layout-bSpecialLayout',
-  //                 type: FormFieldType.Boolean,
-  //                 label: 'b Special Layout',
-  //               },
-  //               {
-  //                 id: 'layout-bSpecialProperty',
-  //                 type: FormFieldType.Text,
-  //                 label: 'b Special Property',
-  //               },
-  //             ],
-  //             aStyle: [
-  //               {
-  //                 id: 'padding',
-  //                 label: 'padding',
-  //                 type: FormFieldType.Text,
-  //               },
-  //               {
-  //                 id: 'margin',
-  //                 label: 'margin',
-  //                 type: FormFieldType.Text,
-  //               },
-  //             ],
-  //             bStyle: [
-  //               {
-  //                 id: 'zIndex',
-  //                 label: 'z Index',
-  //                 type: FormFieldType.Text,
-  //               },
-  //               {
-  //                 id: 'marginCameCase',
-  //                 label: 'margin Came Case',
-  //                 type: FormFieldType.Text,
-  //               },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //       '*'
-  //     );
-  //   });
-  // });
+  describe('reveal', () => {
+    it('should send `MessageType.StylesOptions` post message with proper data', async () => {
+      getInjector().inject('service').reveal().subscribe();
+      postMessage(
+        {
+          type: MessageType.SelectedStyles,
+          data: {
+            type: 'bLayout',
+            bProperty: true,
+          } as LayoutTypeStyles,
+        },
+        window
+      );
+      await nextFrame();
+      expect(window.parent.postMessage).toHaveBeenCalledWith(
+        {
+          type: MessageType.StylesOptions,
+          data: {
+            defaults: {
+              bSpecialLayout: 'default',
+              bSpecialProperty: 'default',
+            },
+            fields: {
+              container: [
+                {
+                  id: 'layout-type',
+                  label: 'layout',
+                  type: FormFieldType.Select,
+                  options: [{ value: 'aLayout' }, { value: 'bLayout' }],
+                },
+                {
+                  id: 'layout-aProperty',
+                  label: 'aProperty',
+                  type: FormFieldType.Boolean,
+                },
+                {
+                  id: 'layout-bProperty',
+                  label: 'bProperty',
+                  type: FormFieldType.Boolean,
+                },
+              ],
+              special: [
+                {
+                  id: 'layout-bSpecialLayout',
+                  type: FormFieldType.Boolean,
+                  label: 'b Special Layout',
+                },
+                {
+                  id: 'layout-bSpecialProperty',
+                  type: FormFieldType.Text,
+                  label: 'b Special Property',
+                },
+              ],
+              aStyle: [
+                {
+                  id: 'padding',
+                  label: 'padding',
+                  type: FormFieldType.Text,
+                },
+                {
+                  id: 'margin',
+                  label: 'margin',
+                  type: FormFieldType.Text,
+                },
+              ],
+              bStyle: [
+                {
+                  id: 'zIndex',
+                  label: 'z Index',
+                  type: FormFieldType.Text,
+                },
+                {
+                  id: 'marginCameCase',
+                  label: 'margin Came Case',
+                  type: FormFieldType.Text,
+                },
+              ],
+            },
+          },
+        },
+        '*'
+      );
+    });
+  });
 });
