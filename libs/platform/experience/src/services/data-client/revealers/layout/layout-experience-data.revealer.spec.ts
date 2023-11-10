49e67e12fb65749cfa78d931a3b8fac3c8007655
Aleksey Belan <alexey.belan@spryker.com>
2023-11-10 11:04:10 +0200

    tests

--- libs/platform/experience/src/services/data-client/revealers/layout/layout-experience-data.revealer.spec.ts
+++ libs/platform/experience/src/services/data-client/revealers/layout/layout-experience-data.revealer.spec.ts
@@ -1,15 +1,25 @@
 import { nextFrame } from '@open-wc/testing-helpers';
 import { createInjector, destroyInjector, getInjector } from '@spryker-oryx/di';
-import { MessageType, postMessage } from '@spryker-oryx/experience';
 import { FormFieldType } from '@spryker-oryx/form';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import {
   LayoutPlugin,
   LayoutPropertyPlugin,
   LayoutStylesPlugin,
   LayoutTypeStyles,
 } from '../../../layout';
+import { ExperienceLayoutData } from '../../data-client.model';
 import { LayoutExperienceDataRevealer } from './layout-experience-data.revealer';
+
+const utils = {
+  postMessage: vi.fn(),
+  catchMessage: vi.fn(),
+};
+
+vi.mock('../../utilities', () => ({
+  postMessage: (data: unknown) => utils.postMessage(data),
+  catchMessage: (data: unknown) => utils.catchMessage(data),
+}));
 
 const mockALayout: LayoutPlugin = {
   getConfig: vi.fn().mockReturnValue(of({ schema: { name: 'aLayout' } })),
@@ -113,7 +123,6 @@ describe('LayoutExperienceDataRevealer', () => {
         },
       ],
     });
-    vi.spyOn(window.parent, 'postMessage');
   });
 
   afterEach(() => {
@@ -122,87 +131,78 @@ describe('LayoutExperienceDataRevealer', () => {
   });
 
   describe('reveal', () => {
-    it('should send `MessageType.StylesOptions` post message with proper data', async () => {
-      getInjector().inject('service').reveal().subscribe();
-      postMessage(
-        {
-          type: MessageType.SelectedStyles,
-          data: {
-            type: 'bLayout',
-            bProperty: true,
-          } as LayoutTypeStyles,
-        },
-        window
-      );
+    it('should send create proper object depends on active layout styles', async () => {
+      const catchTrigger = new BehaviorSubject<LayoutTypeStyles>({
+        type: 'bLayout',
+        bProperty: true,
+      } as LayoutTypeStyles);
+      utils.catchMessage.mockReturnValue(catchTrigger);
+      let expected = {} as ExperienceLayoutData;
+      getInjector()
+        .inject('service')
+        .reveal()
+        .subscribe((data: ExperienceLayoutData) => {
+          expected = data;
+        });
       await nextFrame();
-      expect(window.parent.postMessage).toHaveBeenCalledWith(
+      expect(expected.defaults).toEqual({
+        bSpecialLayout: 'default',
+        bSpecialProperty: 'default',
+      });
+      expect(expected.fields.container).toEqual([
         {
-          type: MessageType.StylesOptions,
-          data: {
-            defaults: {
-              bSpecialLayout: 'default',
-              bSpecialProperty: 'default',
-            },
-            fields: {
-              container: [
-                {
-                  id: 'layout-type',
-                  label: 'layout',
-                  type: FormFieldType.Select,
-                  options: [{ value: 'aLayout' }, { value: 'bLayout' }],
-                },
-                {
-                  id: 'layout-aProperty',
-                  label: 'aProperty',
-                  type: FormFieldType.Boolean,
-                },
-                {
-                  id: 'layout-bProperty',
-                  label: 'bProperty',
-                  type: FormFieldType.Boolean,
-                },
-              ],
-              special: [
-                {
-                  id: 'layout-bSpecialLayout',
-                  type: FormFieldType.Boolean,
-                  label: 'b Special Layout',
-                },
-                {
-                  id: 'layout-bSpecialProperty',
-                  type: FormFieldType.Text,
-                  label: 'b Special Property',
-                },
-              ],
-              aStyle: [
-                {
-                  id: 'padding',
-                  label: 'padding',
-                  type: FormFieldType.Text,
-                },
-                {
-                  id: 'margin',
-                  label: 'margin',
-                  type: FormFieldType.Text,
-                },
-              ],
-              bStyle: [
-                {
-                  id: 'zIndex',
-                  label: 'z Index',
-                  type: FormFieldType.Text,
-                },
-                {
-                  id: 'marginCameCase',
-                  label: 'margin Came Case',
-                  type: FormFieldType.Text,
-                },
-              ],
-            },
-          },
+          id: 'layout-type',
+          label: 'layout',
+          type: FormFieldType.Select,
+          options: [{ value: 'aLayout' }, { value: 'bLayout' }],
         },
-        '*'
-      );
+        {
+          id: 'layout-aProperty',
+          label: 'aProperty',
+          type: FormFieldType.Boolean,
+        },
+        {
+          id: 'layout-bProperty',
+          label: 'bProperty',
+          type: FormFieldType.Boolean,
+        },
+      ]);
+      expect(expected.fields.special).toEqual([
+        {
+          id: 'layout-bSpecialLayout',
+          type: FormFieldType.Boolean,
+          label: 'b Special Layout',
+        },
+        {
+          id: 'layout-bSpecialProperty',
+          type: FormFieldType.Text,
+          label: 'b Special Property',
+        },
+      ]);
+      expect(expected.fields.aStyle).toEqual([
+        {
+          id: 'padding',
+          label: 'padding',
+          type: FormFieldType.Text,
+        },
+        {
+          id: 'margin',
+          label: 'margin',
+          type: FormFieldType.Text,
+        },
+      ]);
+      expect(expected.fields.bStyle).toEqual([
+        {
+          id: 'zIndex',
+          label: 'z Index',
+          type: FormFieldType.Text,
+        },
+        {
+          id: 'marginCameCase',
+          label: 'margin Came Case',
+          type: FormFieldType.Text,
+        },
+      ]);
     });
   });
 });
