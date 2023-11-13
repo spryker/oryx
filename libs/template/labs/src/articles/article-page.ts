a18be759dfba41633cb886cffc9e42e2683cf388
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-11-13 09:55:14 +0100

    fix: show header and footer on article page in labs package (#890)
    The new header/footer structure are now also applied to the article pages in the labs package.

--- libs/template/labs/src/articles/article-page.ts
+++ libs/template/labs/src/articles/article-page.ts
@@ -6,32 +6,40 @@ export const articlesPage: ExperienceComponent = {
   type: 'Page',
   id: 'articles',
   meta: {
-    title: 'Articles Page',
+    title: 'Articles',
     route: `/${ContentFields.Article}`,
   },
-  options: {
-    rules: [{ layout: 'flex', padding: '30px 0' }],
-  },
   components: [
+    { ref: 'header' },
     {
-      type: 'oryx-search-box',
+      type: 'oryx-composition',
       options: {
-        rules: [{ margin: 'auto', width: '580px' }],
-        [SuggestionField.Suggestions]: null,
-        [SuggestionField.Categories]: null,
-        [SuggestionField.Contents]: null,
-        [SuggestionField.Products]: null,
-        [ContentFields.Article]: {
-          max: 8,
+        rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
+      },
+      components: [
+        {
+          type: 'oryx-site-breadcrumb',
+          options: { rules: [{ colSpan: 2 }] },
         },
-      },
+        {
+          type: 'oryx-search-box',
+          options: {
+            rules: [{ margin: 'auto', width: '580px' }],
+            [SuggestionField.Suggestions]: null,
+            [SuggestionField.Categories]: null,
+            [SuggestionField.Contents]: null,
+            [SuggestionField.Products]: null,
+            [ContentFields.Article]: {
+              max: 8,
+            },
+          },
+        },
+        {
+          type: 'oryx-content-articles',
+        },
+      ],
     },
-    {
-      type: 'oryx-content-articles',
-      options: {
-        rules: [{ width: '80%', margin: 'auto' }],
-      },
-    },
+    { ref: 'footer' },
   ],
 };
 
@@ -39,65 +47,85 @@ export const articlePage: ExperienceComponent = {
   type: 'Page',
   id: 'article',
   meta: {
-    title: 'Article Page',
+    title: 'Article',
     route: `/${ContentFields.Article}/:id`,
   },
-  options: {
-    rules: [{ layout: 'flex', padding: '30px 0' }],
-  },
   components: [
+    { ref: 'header' },
     {
-      type: 'oryx-content-article',
+      type: 'oryx-composition',
       options: {
-        rules: [{ width: '80%', margin: 'auto' }],
+        rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
       },
+      components: [
+        {
+          type: 'oryx-site-breadcrumb',
+          options: { rules: [{ colSpan: 2 }] },
+        },
+        { type: 'oryx-content-article' },
+      ],
     },
+    { ref: 'footer' },
   ],
 };
 
 export const faqsPage: ExperienceComponent = {
   type: 'Page',
   id: 'faqs',
-  meta: { title: 'FAQs Page', route: `/${ContentFields.Faq}` },
-  options: {
-    rules: [{ layout: 'flex', padding: '30px 0' }],
-  },
+  meta: { title: 'FAQs', route: `/${ContentFields.Faq}` },
   components: [
+    { ref: 'header' },
     {
-      type: 'oryx-search-box',
+      type: 'oryx-composition',
       options: {
-        rules: [{ margin: 'auto', width: '580px' }],
-        [SuggestionField.Suggestions]: null,
-        [SuggestionField.Categories]: null,
-        [SuggestionField.Contents]: null,
-        [SuggestionField.Products]: null,
-        [ContentFields.Faq]: {
-          max: 8,
+        rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
+      },
+      components: [
+        {
+          type: 'oryx-site-breadcrumb',
+          options: { rules: [{ colSpan: 2 }] },
         },
-      },
+        {
+          type: 'oryx-search-box',
+          options: {
+            rules: [{ margin: 'auto', width: '580px' }],
+            [SuggestionField.Suggestions]: null,
+            [SuggestionField.Categories]: null,
+            [SuggestionField.Contents]: null,
+            [SuggestionField.Products]: null,
+            [ContentFields.Article]: {
+              max: 8,
+            },
+          },
+        },
+        {
+          type: 'oryx-content-articles',
+        },
+      ],
     },
-    {
-      type: 'oryx-content-articles',
-      options: {
-        rules: [{ width: '80%', margin: 'auto' }],
-      },
-    },
+    { ref: 'footer' },
   ],
 };
 
 export const faqPage: ExperienceComponent = {
   type: 'Page',
   id: 'faq',
-  meta: { title: 'FAQ Page', route: `/${ContentFields.Faq}/:id` },
-  options: {
-    rules: [{ layout: 'flex', padding: '30px 0' }],
-  },
+  meta: { title: 'FAQ', route: `/${ContentFields.Faq}/:id` },
   components: [
+    { ref: 'header' },
     {
-      type: 'oryx-content-article',
+      type: 'oryx-composition',
       options: {
-        rules: [{ width: '80%', margin: 'auto' }],
+        rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
       },
+      components: [
+        {
+          type: 'oryx-site-breadcrumb',
+          options: { rules: [{ colSpan: 2 }] },
+        },
+        { type: 'oryx-content-article' },
+      ],
     },
+    { ref: 'footer' },
   ],
 };
