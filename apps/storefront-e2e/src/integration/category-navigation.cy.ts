3f66daa74827718882640a0cc576d03a533feab3
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-11-14 13:54:46 +0100

    simplify test

--- apps/storefront-e2e/src/integration/category-navigation.cy.ts
+++ apps/storefront-e2e/src/integration/category-navigation.cy.ts
@@ -10,31 +10,13 @@ describe('Category navigation suite', () => {
       landingPage.visit();
     });
 
-    it('should render the category navigation in the header ', () => {
-      const navigation = new CategoryNavigationFragment().get();
-      navigation.should('be.visible');
-    });
-
-    it('should have all products link in the category navigation bar', () => {
-      const link = new CategoryNavigationFragment().getLink(0);
-      link.should('contain.text', 'All products');
-    });
-
-    describe('and a navigation item is clicked', () => {
-      let url: string;
-
-      beforeEach(() => {
-        const link = new CategoryNavigationFragment().getLink(1);
-
-        link.invoke('attr', 'href').then((href) => {
-          url = href;
-          cy.visit(url);
-        });
-      });
-
-      it('should open the associated url', () => {
-        cy.location('pathname').should('eq', url);
-      });
+    it('should have all products link in the category navigation bar', async () => {
+      const navigation = new CategoryNavigationFragment();
+      navigation.getLink(0).should('contain.text', 'All products');
+      navigation
+        .getLink(1)
+        .invoke('attr', 'href')
+        .should('contain', '/category/');
     });
   });
 });
