8e482b4f4def3fb827460fba9f57ad6bac83f932
Stan <dunqan@w9.pl>
2023-11-14 14:14:45 +0100

    fix: featureVersion is not applied in client for development ssr mode (#894)
    FeatureVersion flag is picked from the environment, if it was not part
    of define replacement.
    
    Closes [HRZ-90431](https://spryker.atlassian.net/browse/HRZ-90431)
    
    
    [HRZ-90431]:
    https://spryker.atlassian.net/browse/HRZ-90431?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- libs/base/utilities/src/misc/version.ts
+++ libs/base/utilities/src/misc/version.ts
@@ -3,7 +3,11 @@ declare const __ORYX_FEATURE_VERSION__: string;
 // this one should be adjusted per major version
 const defaultVersion = '1.0';
 
+interface ImportMeta {
+  readonly env?: Record<string, string | undefined>;
+}
+
 export const featureVersion =
   typeof __ORYX_FEATURE_VERSION__ !== 'undefined'
     ? __ORYX_FEATURE_VERSION__ || defaultVersion
-    : defaultVersion;
+    : (import.meta as ImportMeta)?.env?.ORYX_FEATURE_VERSION || defaultVersion;
