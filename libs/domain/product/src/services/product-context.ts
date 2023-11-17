6e9302ad594436db366e36c5585a570d9ada82a0
dunqan <dunqan@w9.pl>
2023-11-17 09:27:48 +0100

    add missing product serializers

--- libs/domain/product/src/services/product-context.ts
+++ libs/domain/product/src/services/product-context.ts
@@ -1,7 +1,13 @@
-import { ContextFallback, ContextService } from '@spryker-oryx/core';
+import {
+  ContextFallback,
+  ContextSerializer,
+  ContextService,
+} from '@spryker-oryx/core';
 import { Provider, inject } from '@spryker-oryx/di';
 import { RouterService } from '@spryker-oryx/router';
-import { Observable, switchMap } from 'rxjs';
+import { featureVersion } from '@spryker-oryx/utilities';
+import { Observable, of, switchMap } from 'rxjs';
+import { ProductQualifier } from '../models';
 
 export const enum ProductContext {
   SKU = 'sku',
@@ -20,7 +26,38 @@ export function productContextFallbackFactory(
     );
 }
 
+export const ProductContextSerializerToken = `${ContextSerializer}${ProductContext.SKU}`;
+
+export class ProductContextSerializer
+  implements ContextSerializer<ProductQualifier>
+{
+  serialize(value: ProductQualifier): Observable<string> {
+    return value?.sku ? of(value.sku) : of('');
+  }
+
+  deserialize(value: string): Observable<ProductQualifier> {
+    return of({
+      sku: value,
+    });
+  }
+}
+
+/** @deprecated since 1.3, use productContextProviders instead */
 export const ProductContextFallback: Provider = {
   provide: `${ContextFallback}${ProductContext.SKU}`,
   useFactory: productContextFallbackFactory,
 };
+
+export const productContextProviders: Provider[] =
+  featureVersion >= '1.3'
+    ? [
+        {
+          provide: `${ContextFallback}${ProductContext.SKU}`,
+          useFactory: productContextFallbackFactory,
+        },
+        {
+          provide: ProductContextSerializerToken,
+          useClass: ProductContextSerializer,
+        },
+      ]
+    : [ProductContextFallback];
