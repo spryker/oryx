924d8ae5ee0a33571e2fcfebb37af193e38b3066
Aleksey Belan <alexey.belan@spryker.com>
2023-11-17 12:28:09 +0200

    test

--- libs/base/utilities/src/misc/version.ts
+++ libs/base/utilities/src/misc/version.ts
@@ -3,7 +3,4 @@ declare const __ORYX_FEATURE_VERSION__: string;
 // this one should be adjusted per major version
 const defaultVersion = '1.0';
 
-export const featureVersion =
-  typeof __ORYX_FEATURE_VERSION__ !== 'undefined'
-    ? __ORYX_FEATURE_VERSION__ || defaultVersion
-    : defaultVersion;
+export const featureVersion = '1.2';
