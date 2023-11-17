d0fd6c9dd63f30571df90e14c0006dfbdd5ab47c
Aleksey Belan <alexey.belan@spryker.com>
2023-11-17 12:43:28 +0200

    fix

--- libs/base/utilities/src/misc/version.ts
+++ libs/base/utilities/src/misc/version.ts
@@ -1,3 +1,17 @@
 declare const __ORYX_FEATURE_VERSION__: string;
 
-export const featureVersion = '1.2';
+// this one should be adjusted per major version
+const defaultVersion = '1.0';
+
+interface ImportMeta {
+  readonly env?: Record<string, string | undefined>;
+}
+
+console.log(__ORYX_FEATURE_VERSION__);
+console.log(defaultVersion);
+console.log((import.meta as ImportMeta)?.env?.ORYX_FEATURE_VERSION);
+
+export const featureVersion =
+  typeof __ORYX_FEATURE_VERSION__ !== 'undefined'
+    ? __ORYX_FEATURE_VERSION__ || defaultVersion
+    : (import.meta as ImportMeta)?.env?.ORYX_FEATURE_VERSION || defaultVersion;
