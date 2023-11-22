11bf965aa18fdbc5a4ee9eac6d4c9854a3ed68d5
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-11-22 14:12:48 +0100

    refactor(picking): use shadow style for header (#928)
    Since we moved the header to experience data, we can leverage the shadow
    style property.

--- libs/template/presets/fulfillment/experience/templates/header.ts
+++ libs/template/presets/fulfillment/experience/templates/header.ts
@@ -1,4 +1,4 @@
-import { ExperienceComponent } from '@spryker-oryx/experience';
+import { ExperienceComponent, ShadowElevation } from '@spryker-oryx/experience';
 import { IconTypes } from '@spryker-oryx/ui/icon';
 
 export const HeaderTemplate: ExperienceComponent = {
@@ -14,11 +14,7 @@ export const HeaderTemplate: ExperienceComponent = {
         triggerType: 'icon',
         rules: [{ style: '--oryx-icon-color: var(--oryx-color-primary-9)' }],
       },
-      components: [
-        {
-          type: 'oryx-picking-user-profile',
-        },
-      ],
+      components: [{ type: 'oryx-picking-user-profile' }],
     },
   ],
   options: {
@@ -30,14 +26,14 @@ export const HeaderTemplate: ExperienceComponent = {
           zIndex: 1,
           bleed: true,
         },
+        shadow: ShadowElevation.Hovering,
         height: '66px',
         background: 'var(--oryx-color-neutral-1)',
         padding: '0 24px',
         gap: '12px',
         align: 'center',
         justify: 'end',
-        style:
-          'box-shadow: var(--oryx-elevation-2) var(--oryx-elevation-color-2); box-sizing: border-box;',
+        style: 'box-sizing: border-box;',
       },
     ],
   },
