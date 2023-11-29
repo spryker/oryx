2f6354bdea51c69c7dc26449e8abecbb3d3b51a4
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-11-29 15:05:57 +0100

    fix tests

--- libs/base/ui/structure/text/text.component.spec.ts
+++ libs/base/ui/structure/text/text.component.spec.ts
@@ -26,16 +26,83 @@ describe('TextComponent', () => {
   });
 
   describe('when content contains tags', () => {
+    describe('featureVersion < 1.3', () => {
+      beforeEach(async () => {
+        mockFeatureVersion('1.3');
+        element = await fixture(
+          html`<oryx-text
+            .content=${'<h1>This is the content</h1><p>with a <a href="/link">link</a></p>'}
+          ></oryx-text>`
+        );
+      });
+
+      it('should render the html as-is', () => {
+        expect(element).toContainElement(`h1 + p > a[href='/link']`);
+      });
+
+      it('should not render oryx design system components', () => {
+        expect(element).not.toContainElement(`oryx-heading`);
+        expect(element).not.toContainElement(`oryx-link`);
+      });
+    });
+  });
+
+  describe('when the featureVersion >= 1.4', () => {
     beforeEach(async () => {
+      mockFeatureVersion('1.4');
       element = await fixture(
         html`<oryx-text
-          .content=${'<h1>This is the content</h1><p>with a <a href="/link">link</a></p>'}
+          .content=${'<h1>This is the content</h1><p>with a <a href="/link">link</a> and a <button>but</button>'}
         ></oryx-text>`
       );
     });
 
-    it('should render the html as-is', () => {
-      expect(element).toContainElement(`h1 + p > a[href='/link']`);
+    describe('and the content contains standard heading tags', () => {
+      beforeEach(async () => {
+        element = await fixture(
+          html`<oryx-text
+            .content=${'<h1 style="margin:10px" class="foo-bar" foo="bar">This is the content</h1><p>with a <a href="/link">link</a></p><p>and a <button>but</button>'}
+          ></oryx-text>`
+        );
+      });
+
+      it('should render oryx design system components', () => {
+        expect(element).toContainElement(`oryx-heading`);
+        expect(element).toContainElement(`oryx-link`);
+        expect(element).toContainElement(`oryx-button`);
+      });
+    });
+
+    describe('and the content contains not standard heading tags', () => {
+      beforeEach(async () => {
+        element = await fixture(
+          html`<oryx-text
+            .content=${'foo <small>small</small> bar <caption>cap</caption> baz <span class="subtitle">subtitle</span> qux  '}
+          ></oryx-text>`
+        );
+      });
+
+      it('should render oryx-heading components', () => {
+        expect(element).toContainElement(`oryx-heading[tag='small']`);
+        expect(element).toContainElement(`oryx-heading[tag='caption']`);
+        expect(element).toContainElement(`oryx-heading[typography='subtitle']`);
+      });
+    });
+
+    describe('and the tags contain attributes', () => {
+      beforeEach(async () => {
+        element = await fixture(
+          html`<oryx-text
+            .content=${'<h1 style="margin:10px" class="foo-bar" foo="bar">This is the content</h1>'}
+          ></oryx-text>`
+        );
+      });
+
+      it('should remain the attributes', () => {
+        expect(element).toContainElement(
+          `oryx-heading[style="margin:10px"][class="foo-bar"][foo="bar"]`
+        );
+      });
     });
   });
 });
