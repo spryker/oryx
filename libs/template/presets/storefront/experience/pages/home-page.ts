2e46e397a36e800e9238d8e4374a0ab3e8b8b2b3
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-11-29 12:42:40 +0100

    fix title

--- libs/template/presets/storefront/experience/pages/home-page.ts
+++ libs/template/presets/storefront/experience/pages/home-page.ts
@@ -52,7 +52,7 @@ export const homePage: ExperienceComponent = {
             data: {
               text: `
               <span class="subtitle">CANON EOS R7 System camera</span>
-              <h1 style="margin:20px 0;">D</h1>
+              <h1 style="margin:20px 0;">Discover everything</h1>
               <div class="h3" style="margin-bottom:20px;">EOS R7 wows with its ability to track fast-moving subjects with its Deep-learning Dual Pixel CMOS AF II focus system.</div>
               <oryx-button href="/category/12">Shop now</oryx-button>
             `,
