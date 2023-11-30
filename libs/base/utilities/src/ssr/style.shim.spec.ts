b69f1bbe673d61b3d2d5a85e72a07f7df9129d0c
dunqan <dunqan@w9.pl>
2023-11-30 09:21:51 +0100

    unit tests coverage

---
+++ libs/base/utilities/src/ssr/style.shim.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+import { ssrStyleShim } from './style.shim';
+
+describe('ssrStyleShim', () => {
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    class MockElement {
+      map = {};
+      hasAttribute = vi.fn((a) => this.map[a] === undefined);
+      setAttribute = vi.fn((a, v) => (this.map[a] = v));
+      getAttribute = vi.fn((a) => this.map[a]);
+      removeAttribute = vi.fn((a) => delete this.map[a]);
+    }
+    ssrStyleShim(MockElement as any);
+    element = new MockElement() as unknown as HTMLElement;
+  });
+
+  it('should correctly toggle an attribute', () => {
+    element.toggleAttribute('test-attribute', true);
+    expect(element.setAttribute).toHaveBeenCalledWith('test-attribute', '');
+
+    element.toggleAttribute('test-attribute', false);
+    expect(element.removeAttribute).toHaveBeenCalledWith('test-attribute');
+  });
+
+  it('should handle style properties correctly', () => {
+    element.style.setProperty('color', 'red');
+    expect(element.style.color).toBe('red');
+  });
+});
