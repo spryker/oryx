ffa322ef0a8800695b9b4fad2f0a7d2e5042e773
Stan <dunqan@w9.pl>
2023-12-01 06:09:12 +0100

    Update libs/domain/product/src/models/category-qualifier.ts
    Co-authored-by: tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>

--- libs/domain/product/src/models/category-qualifier.ts
+++ libs/domain/product/src/models/category-qualifier.ts
@@ -2,6 +2,6 @@ export interface ProductCategoryQualifier {
   id?: string;
   /** limit result to direct children categories of the parent */
   parent?: string;
-  /** exclude category or cateories from the result */
+  /** exclude category or categories from the result */
   exclude?: string | string[];
 }
