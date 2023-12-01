6b3809200febb9bd68f77427bd27917cc2f4d7b5
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-01 16:05:53 +0100

    refactor(ui): improve heading utility to support flexible margins margin (#961)
    Improved the heading margin so that we can provide custom block margin.
    The block margin defaults to `0`.

--- libs/domain/cart/entries/src/entries.styles.ts
+++ libs/domain/cart/entries/src/entries.styles.ts
@@ -7,9 +7,7 @@ const deprecatedHeadingStyles = css`
   }
 
   h1 {
-    ${headingUtil(HeadingTag.H3)}
-
-    margin-block-end: 20px;
+    ${headingUtil(HeadingTag.H3, { margin: '0 0 20px' })}
   }
 `;
 
