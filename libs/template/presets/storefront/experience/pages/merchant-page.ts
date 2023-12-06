aef5d323c92cecf59e4dc19f4c135f50e99852c3
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-06 16:25:53 +0100

    page layout

--- libs/template/presets/storefront/experience/pages/merchant-page.ts
+++ libs/template/presets/storefront/experience/pages/merchant-page.ts
@@ -9,60 +9,70 @@ export const merchantPage: ExperienceComponent = {
     route: '/merchant/:merchant',
     description: 'Default Merchant Page Description',
   },
-  options: {
-    rules: [
-      {
-        layout: 'split-main',
-        padding: '30px 0',
-      },
-    ],
-  },
-
   components: [
+    { ref: 'header' },
+
     {
-      type: 'oryx-site-breadcrumb',
+      type: 'oryx-composition',
+
       options: {
         rules: [
           {
-            colSpan: 2,
+            layout: { type: 'split', columnWidthType: 'main' },
+            padding: '30px 0',
           },
-          { query: { breakpoint: Size.Sm }, hide: true },
         ],
       },
-    },
 
-    {
-      type: 'oryx-merchant-banner',
-      options: {
-        rules: [{ colSpan: 2, height: '250px' }],
-      },
-    },
-
-    {
-      type: 'oryx-composition',
-      components: [
-        { type: 'oryx-merchant-title' },
-        { type: 'oryx-merchant-legal' },
-        { type: 'oryx-merchant-description' },
-      ],
-      options: {
-        rules: [{ layout: 'list' }],
-      },
-    },
-
-    {
-      type: 'oryx-composition',
       components: [
         {
-          type: 'oryx-merchant-logo',
-          options: { rules: [{ width: '100px' }] },
+          type: 'oryx-site-breadcrumb',
+          options: {
+            rules: [
+              {
+                colSpan: 2,
+              },
+              { query: { breakpoint: Size.Sm }, hide: true },
+            ],
+          },
         },
-        { type: 'oryx-merchant-openings-hours' },
-        { type: 'oryx-merchant-contact' },
+
+        {
+          type: 'oryx-merchant-banner',
+          options: {
+            rules: [{ colSpan: 2, height: '250px' }],
+          },
+        },
+
+        {
+          type: 'oryx-composition',
+          components: [
+            { type: 'oryx-merchant-title' },
+            { type: 'oryx-merchant-legal' },
+            { type: 'oryx-merchant-description' },
+          ],
+          options: {
+            rules: [{ layout: 'list' }],
+          },
+        },
+
+        {
+          type: 'oryx-composition',
+          components: [
+            {
+              type: 'oryx-merchant-logo',
+              options: { rules: [{ width: '100px' }] },
+            },
+            { type: 'oryx-merchant-openings-hours' },
+            { type: 'oryx-merchant-contact' },
+          ],
+          options: {
+            rules: [{ layout: 'list' }],
+          },
+        },
       ],
-      options: {
-        rules: [{ layout: 'list' }],
-      },
     },
+
+    { ref: 'footer' },
   ],
 };
