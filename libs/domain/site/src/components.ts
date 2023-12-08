9c589d8f3a74d15fe21f2e8c70c8ec23e4d12607
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-08 17:00:33 +0100

    time component, stories and tests

--- libs/domain/site/src/components.ts
+++ libs/domain/site/src/components.ts
@@ -8,3 +8,4 @@ export * from '../navigation-item/navigation-item.def';
 export * from '../notification-center/notification-center.def';
 export * from '../price-mode-selector/price-mode-selector.def';
 export * from '../price/price.def';
+export * from '../time/time.def';
