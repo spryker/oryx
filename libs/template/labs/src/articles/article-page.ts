a4f7f878567b1212f8a6df30d7d5f8fa6519b025
Aleksey Belan <alexey.belan@spryker.com>
2023-12-12 14:57:31 +0200

    fix

--- libs/template/labs/src/articles/article-page.ts
+++ libs/template/labs/src/articles/article-page.ts
@@ -3,76 +3,106 @@ import { SuggestionField } from '@spryker-oryx/search';
 
 const experienceArticleTags = ['article', 'faq', 'about'];
 
-export const experienceArticlePages = experienceArticleTags
-  .map(
-    (tag) =>
-      [
-        {
-          type: 'Page',
-          id: `${tag}-list`,
-          meta: {
-            title: `${tag} List`,
-            route: `/${tag}`,
-          },
-          components: [
-            { ref: 'header' },
+export const experienceArticlePages = [
+  {
+    merge: {
+      selector: 'legal-links',
+      type: 'patch',
+    },
+    type: 'oryx-composition',
+    components: [
+      {
+        type: 'oryx-content-text',
+        content: { data: { text: `©️ 2023 Spryker` } },
+      },
+      {
+        type: 'oryx-content-list',
+        options: {
+          tags: 'legal',
+          rules: [
             {
-              type: 'oryx-composition',
-              options: {
-                rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
+              layout: {
+                type: 'flex',
+                divider: true,
               },
-              components: [
-                {
-                  type: 'oryx-site-breadcrumb',
-                  options: { rules: [{ colSpan: 2 }] },
-                },
-                {
-                  type: 'oryx-search-box',
-                  options: {
-                    rules: [{ margin: 'auto', width: '580px' }],
-                    [SuggestionField.Suggestions]: null,
-                    [SuggestionField.Categories]: null,
-                    [SuggestionField.Contents]: null,
-                    [SuggestionField.Products]: null,
-                    [tag]: { max: 8 },
-                  },
-                },
-                {
-                  type: 'oryx-content-list',
-                  options: {
-                    rules: [{ layout: { type: 'list' } }],
-                  },
-                },
-              ],
+              width: 'auto',
             },
-            { ref: 'footer' },
           ],
         },
-        {
-          type: 'Page',
-          id: tag,
-          meta: {
-            title: tag,
-            route: `/${tag}/:id`,
-          },
-          components: [
-            { ref: 'header' },
-            {
-              type: 'oryx-composition',
-              options: {
-                rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
-              },
-              components: [
-                {
-                  type: 'oryx-site-breadcrumb',
-                  options: { rules: [{ colSpan: 2 }] },
-                },
-                { type: 'oryx-content-article' },
-              ],
+      },
+    ],
+  },
+  ...experienceArticleTags
+    .map(
+      (tag) =>
+        [
+          {
+            type: 'Page',
+            id: `${tag}-list`,
+            meta: {
+              title: `${tag} List`,
+              route: `/${tag}`,
             },
-            { ref: 'footer' },
-          ],
-        },
-      ] as ExperienceComponent[]
-  )
-  .flat();
+            components: [
+              { ref: 'header' },
+              {
+                type: 'oryx-composition',
+                options: {
+                  rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
+                },
+                components: [
+                  {
+                    type: 'oryx-site-breadcrumb',
+                    options: { rules: [{ colSpan: 2 }] },
+                  },
+                  {
+                    type: 'oryx-search-box',
+                    options: {
+                      rules: [{ margin: 'auto', width: '580px' }],
+                      [SuggestionField.Suggestions]: null,
+                      [SuggestionField.Categories]: null,
+                      [SuggestionField.Contents]: null,
+                      [SuggestionField.Products]: null,
+                      [tag]: { max: 8 },
+                    },
+                  },
+                  {
+                    type: 'oryx-content-list',
+                    options: {
+                      rules: [{ layout: { type: 'list' } }],
+                    },
+                  },
+                ],
+              },
+              { ref: 'footer' },
+            ],
+          },
+          {
+            type: 'Page',
+            id: tag,
+            meta: {
+              title: tag,
+              route: `/${tag}/:id`,
+            },
+            components: [
+              { ref: 'header' },
+              {
+                type: 'oryx-composition',
+                options: {
+                  rules: [{ layout: { type: 'split' }, padding: '30px 0' }],
+                },
+                components: [
+                  {
+                    type: 'oryx-site-breadcrumb',
+                    options: { rules: [{ colSpan: 2 }] },
+                  },
+                  { type: 'oryx-content-article' },
+                ],
+              },
+              { ref: 'footer' },
+            ],
+          },
+        ] as ExperienceComponent[]
+    )
+    .flat(),
+];
