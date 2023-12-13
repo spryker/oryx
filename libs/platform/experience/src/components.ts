5d319fec37e4bd96ee3c4a1c4b0a9e4619e0cf8e
Stan <dunqan@w9.pl>
2023-12-13 08:09:46 +0100

    feat: entity service implementation (#986)
    Introduces `EntityService` and `oryx-entity-text` component
    
    Closes [HRZ-90761](https://spryker.atlassian.net/browse/HRZ-90761)
    
    [HRZ-90761]:
    https://spryker.atlassian.net/browse/HRZ-90761?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- libs/platform/experience/src/components.ts
+++ libs/platform/experience/src/components.ts
@@ -1,3 +1,4 @@
 export * from '../carousel-navigation/carousel-navigation.def';
 export * from '../composition/composition.def';
+export * from '../entity-text/entity-text.def';
 export * from '../layout/layout.def';
