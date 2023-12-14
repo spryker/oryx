f79354d00338acc9a4d2edf0ad73dbd13c14074b
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-14 16:06:16 +0100

    improved EB preview

--- libs/platform/experience/composition/composition.styles.ts
+++ libs/platform/experience/composition/composition.styles.ts
@@ -1,6 +1,60 @@
+import { featureVersion } from '@spryker-oryx/utilities';
 import { css } from 'lit';
 
-export const compositionPreviewStyles = css`
+const compositionPreviewStylesNew = css`
+  :host {
+    --eb-preview-color: var(--oryx-color-info-9);
+    --eb-preview-radius: 4px;
+  }
+
+  .eb-preview-focus::before,
+  .eb-preview-focus::after {
+    position: absolute;
+    outline: 4px solid var(--eb-preview-color);
+    outline-offset: -4px;
+  }
+
+  .eb-preview-focus::before {
+    content: '';
+    top: calc(var(--ebp-top) - var(--ebp-rel-top, 0px));
+    left: calc(var(--ebp-left) - var(--ebp-rel-left, 0px));
+    width: var(--ebp-width);
+    height: var(--ebp-height);
+    border-radius: inherit;
+    z-index: var(--oryx-overlay-z-index, 3);
+  }
+
+  .eb-preview-focus.ebp-absolute::before,
+  .eb-preview-focus.ebp-sticky::before {
+    inset-block-start: 0;
+    inset-inline-start: 0;
+  }
+
+  .eb-preview-focus.ebp-absolute::after,
+  .eb-preview-focus.ebp-sticky::after {
+    inset-block-start: -26px;
+    inset-inline-start: 0;
+  }
+
+  .eb-preview-focus::after {
+    content: attr(name);
+    top: calc(
+      var(--ebp-top) - var(--ebp-rel-top, 0px) -
+        (26px - var(--eb-preview-radius))
+    );
+    left: calc(var(--ebp-left) - var(--ebp-rel-left, 0px));
+    height: 26px;
+    padding: 2px 15px;
+    border-radius: var(--eb-preview-radius) var(--eb-preview-radius) 0 0;
+    z-index: var(--oryx-overlay-z-index, 3);
+    background-color: var(--eb-preview-color);
+    font: 500 16px/22px Montserrat, sans-serif;
+    color: black;
+    box-sizing: border-box;
+  }
+`;
+
+export const compositionPreviewStylesOld = css`
   .eb-preview-focus {
     position: relative;
   }
@@ -27,3 +81,8 @@ export const compositionPreviewStyles = css`
     z-index: var(--oryx-overlay-z-index, 3);
   }
 `;
+
+export const compositionPreviewStyles =
+  featureVersion >= '1.4'
+    ? compositionPreviewStylesNew
+    : compositionPreviewStylesOld;
