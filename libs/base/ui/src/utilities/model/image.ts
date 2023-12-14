49aeede51d0245847d7b3081cfa6c772de5dbada
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-14 09:16:07 +0100

    improve var name

--- libs/base/ui/src/utilities/model/image.ts
+++ libs/base/ui/src/utilities/model/image.ts
@@ -1,2 +1,6 @@
-export const OBJECT_FIT = '--image-fit';
-export const OBJECT_POSITION = '--image-position';
+import { featureVersion } from '@spryker-oryx/utilities';
+
+export const OBJECT_FIT =
+  featureVersion > '1.4' ? '--object-fit' : '--image-fit';
+export const OBJECT_POSITION =
+  featureVersion > '1.4' ? '--object-position' : '--image-position';
