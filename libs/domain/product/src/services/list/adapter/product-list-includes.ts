697991e6b61920c014699f2122610eef429a5eab
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-14 10:32:44 +0100

    fix includes

--- libs/domain/product/src/services/list/adapter/product-list-includes.ts
+++ libs/domain/product/src/services/list/adapter/product-list-includes.ts
@@ -1,6 +1,6 @@
 import { provideIncludes } from '@spryker-oryx/core';
-import { ProductResource } from '@spryker-oryx/product';
 import { ApiProductModel } from '../../../models';
+import { ProductResource } from '../../adapter/product.adapter';
 import { ProductListResource } from './product-list.adapter';
 
 export const productListIncludes = provideIncludes(
