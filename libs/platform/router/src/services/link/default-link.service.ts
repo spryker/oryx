4fba629fc51c6495f94c5fa0fdd7badf27a8954b
dunqan <dunqan@w9.pl>
2023-12-14 15:02:49 +0100

    after merge fixes

--- libs/platform/router/src/services/link/default-link.service.ts
+++ libs/platform/router/src/services/link/default-link.service.ts
@@ -1,8 +1,16 @@
 import { inject } from '@spryker-oryx/di';
-import { BASE_ROUTE, RouteType, RouterService } from '@spryker-oryx/router';
-import { isRouterPath } from '@spryker-oryx/router/lit';
+import {
+  PathRouteConfig,
+  RouteConfig,
+  isRouterPath,
+} from '@spryker-oryx/router/lit';
+import { featureVersion } from '@spryker-oryx/utilities';
 import { Observable, of, switchMap, throwError } from 'rxjs';
+import { BASE_ROUTE } from '../base-route';
+import { RouteType, RouterService } from '../router.service';
 import { LinkOptions, LinkService } from './link.service';
+
+type GenericQualifier = Record<string, string | number | boolean>;
 
 export class DefaultLinkService implements LinkService {
   constructor(
@@ -14,19 +22,36 @@ export class DefaultLinkService implements LinkService {
     return this.routerService.getRoutes().pipe(
       switchMap((routes) => {
         const route =
-          routes?.find((route) => route.type === link.type) ??
-          routes?.find((route) => route.type === RouteType.Page);
+          this.findRoute(routes, link) ??
+          this.findRoute(routes, { type: RouteType.Page });
 
         if (!route || !isRouterPath(route)) {
           return throwError(() => new Error('Link type is not supported'));
         }
 
-        const dynamicId =
-          link.type && link.id
-            ? encodeURIComponent(link.id)
-            : route.type === RouteType.Page
-            ? encodeURIComponent(link.type)
-            : encodeURIComponent(link.id ?? '');
+        let dynamicId = '';
+
+        if (link.qualifier || !link.id) {
+          const path = link.qualifier
+            ? this.generatePath(
+                (route as PathRouteConfig).path,
+                link.qualifier as GenericQualifier
+              )
+            : (route as PathRouteConfig).path;
+          const dynamicParams = link.params
+            ? `?${this.getUrlParams(link.params)}`
+            : '';
+
+          return of(`${this.baseRoute}${path}${dynamicParams}`);
+        } else {
+          dynamicId =
+            link.type && link.id
+              ? encodeURIComponent(link.id)
+              : route.type === RouteType.Page
+              ? encodeURIComponent(link.type)
+              : encodeURIComponent(link.id ?? '');
+        }
+
         const dynamicParams = link.params
           ? `?${this.getUrlParams(link.params)}`
           : '';
@@ -57,6 +82,39 @@ export class DefaultLinkService implements LinkService {
         );
       })
     );
+  }
+
+  protected findRoute(
+    routes: RouteConfig[] = [],
+    link: LinkOptions
+  ): RouteConfig | undefined {
+    if (featureVersion >= '1.4') {
+      const filteredRoutes = routes.filter(
+        (route) => route.type === link.type && isRouterPath(route)
+      ) as PathRouteConfig[];
+      return filteredRoutes.find((route) =>
+        this.canSatisfyPath(route.path!, link.qualifier as GenericQualifier)
+      );
+    } else {
+      return routes.find((route) => route.type === link.type);
+    }
+  }
+
+  protected canSatisfyPath(path: string, qualifier: GenericQualifier): boolean {
+    if (!qualifier) return true;
+    const requiredSegments = path.match(/:\w+/g) || [];
+    return requiredSegments.every((segment) =>
+      qualifier.hasOwnProperty(segment.substring(1))
+    );
+  }
+
+  protected generatePath(path: string, qualifier: GenericQualifier) {
+    return path.replace(/:\w+/g, (match) => {
+      const key = match.substring(1);
+      return qualifier.hasOwnProperty(key)
+        ? encodeURIComponent(qualifier[key] as string | number | boolean)
+        : '';
+    });
   }
 
   private getUrlParams(params: Record<string, string>): string {
