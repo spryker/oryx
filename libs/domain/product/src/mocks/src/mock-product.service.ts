a121172fec0f1df6f99a326ba8605117ff909ff7
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-12-21 14:23:18 +0100

    move product offers out of standard product mock service

--- libs/domain/product/src/mocks/src/mock-product.service.ts
+++ libs/domain/product/src/mocks/src/mock-product.service.ts
@@ -317,82 +317,6 @@ export class MockProductService implements Partial<ProductService> {
       },
       mediaSet: [],
     },
-
-    {
-      sku: 'mp-1',
-      price: {
-        defaultPrice: { value: 1879, isNet: true, currency: 'EUR' },
-        originalPrice: { value: 1779, isNet: true, currency: 'EUR' },
-      },
-      offers: [
-        {
-          id: 'offer-1',
-          merchant: {
-            id: 'MER000005',
-            name: 'Budget Cameras',
-            deliveryTime: '1-3 days',
-          },
-          price: {
-            defaultPrice: { value: 1579, isNet: true, currency: 'EUR' },
-          },
-        },
-        {
-          id: 'offer-2',
-          merchant: {
-            id: 'MER000005',
-            name: 'Video King',
-          },
-          price: {
-            defaultPrice: { value: 1879, isNet: true, currency: 'EUR' },
-          },
-          availability: {
-            quantity: 5,
-          },
-        },
-        {
-          id: 'offer-3',
-          merchant: {
-            id: 'MER000005',
-            name: 'Sony',
-            deliveryTime: '2-4 days',
-          },
-          price: {
-            defaultPrice: { value: 1279, isNet: true, currency: 'EUR' },
-          },
-          availability: {
-            isNeverOutOfStock: true,
-          },
-        },
-      ],
-    },
-    {
-      sku: 'mp-2',
-      price: {
-        defaultPrice: { value: 1879, isNet: true, currency: 'EUR' },
-      },
-      availability: {
-        isNeverOutOfStock: true,
-        quantity: 0,
-        availability: false,
-      },
-      offers: [
-        {
-          id: 'offer-2',
-          merchant: {
-            id: 'MER000005',
-            name: 'Video King',
-            deliveryTime: '2-4 days',
-          },
-          price: {
-            defaultPrice: {
-              value: 1879,
-              isNet: true,
-              currency: 'EUR',
-            },
-          },
-        },
-      ],
-    },
   ];
 
   get(qualifier: ProductQualifier): Observable<Product> {
@@ -400,24 +324,6 @@ export class MockProductService implements Partial<ProductService> {
       (p) => p.sku === qualifier.sku
     ) as Product;
 
-    return of(productForOfferTransform(product, qualifier) as Product);
-  }
-}
-
-// copied from merchant query to avoid cycle deps
-function productForOfferTransform(
-  data: Product,
-  qualifier: ProductQualifier
-): Product | void {
-  if (data && data.offers?.length) {
-    const selectedOffer = qualifier.offer
-      ? data.offers?.find((o) => o.id === qualifier.offer)
-      : data.offers?.find((o) => o.isDefault);
-    return {
-      ...data,
-      merchantId: selectedOffer?.merchant?.id ?? undefined,
-      price: selectedOffer?.price ?? data.price,
-      availability: selectedOffer?.availability ?? data.availability,
-    };
+    return of(product);
   }
 }
