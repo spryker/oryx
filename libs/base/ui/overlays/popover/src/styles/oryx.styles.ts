32370335756a2050629d7b659a90aba185889173
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2024-01-05 11:11:56 +0100

    feat(experience): Added dropdown layout plugin (#1047)
    The dropdown layout plugin is used to render components inside a
    dropdown navigation.
    
    closes: [HRZ-90385](https://spryker.atlassian.net/browse/HRZ-90385)
    
    
    [HRZ-90385]:
    https://spryker.atlassian.net/browse/HRZ-90385?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- libs/base/ui/overlays/popover/src/styles/oryx.styles.ts
+++ libs/base/ui/overlays/popover/src/styles/oryx.styles.ts
@@ -4,7 +4,10 @@ export const popoverStyles = css`
   :host {
     background-color: var(--oryx-color-neutral-1);
     box-shadow: var(--oryx-elevation-2) var(--oryx-color-elevation);
-    border-radius: var(--oryx-border-radius-small);
+    border-radius: var(
+      --oryx-popover-border-radius,
+      var(--oryx-border-radius-small)
+    );
     transition: transform var(--oryx-transition-time) ease-in-out;
   }
 `;
