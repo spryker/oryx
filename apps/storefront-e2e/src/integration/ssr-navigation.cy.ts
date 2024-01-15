1e2da7be649666ee1af2df1585356499c53031a6
pavel.gulvanskiy <pavlo.hulvanskiy@spryker.com>
2024-01-15 12:13:27 +0300

    restore tests

--- apps/storefront-e2e/src/integration/ssr-navigation.cy.ts
+++ apps/storefront-e2e/src/integration/ssr-navigation.cy.ts
@@ -1,8 +1,16 @@
 import { GlueAPI } from '../support/apis/glue.api';
+import { isSSREnabled } from '../support/index';
 import { FooterFragment } from '../support/page-fragments/footer.fragment';
 import { HeaderFragment } from '../support/page-fragments/header.fragment';
 import { SearchBoxFragment } from '../support/page-fragments/search-box.fragment';
+import { CartPage } from '../support/page-objects/cart.page';
+import { CategoryPage } from '../support/page-objects/category.page';
 import { CheckoutPage } from '../support/page-objects/checkout.page';
+import { ContactPage } from '../support/page-objects/contact.page';
+import { LandingPage } from '../support/page-objects/landing.page';
+import { LoginPage } from '../support/page-objects/login.page';
+import { ProductDetailsPage } from '../support/page-objects/product-details.page';
+import { SearchPage } from '../support/page-objects/search.page';
 import { ProductStorage } from '../support/test-data/storages/product.storage';
 import { visibilityCheck } from '../support/utils';
 
@@ -19,122 +27,122 @@ const visitOptions = {
 };
 
 describe('SSR suite', { tags: ['smoke', 'visual-regression'] }, () => {
-  // if (isSSREnabled()) {
-  // it('should render Landing page', () => {
-  //   const landingPage = new LandingPage();
+  if (isSSREnabled()) {
+    it('should render Landing page', () => {
+      const landingPage = new LandingPage();
 
-  //   landingPage.visit(visitOptions);
+      landingPage.visit(visitOptions);
 
-  //   verifyHeader();
-  //   landingPage.getHeroBanner().should('be.visible');
-  //   verifyFooter();
+      verifyHeader();
+      landingPage.getHeroBanner().should('be.visible');
+      verifyFooter();
 
-  //   cy.takeScreenshot('Landing Page');
-  // });
+      cy.takeScreenshot('Landing Page');
+    });
 
-  // it('should render Product details page', () => {
-  //   const productData = ProductStorage.getByEq(1);
-  //   const pdp = new ProductDetailsPage(productData);
+    it('should render Product details page', () => {
+      const productData = ProductStorage.getByEq(1);
+      const pdp = new ProductDetailsPage(productData);
 
-  //   pdp.visit(visitOptions);
+      pdp.visit(visitOptions);
 
-  //   verifyHeader();
-  //   pdp.checkDefaultProduct();
-  //   verifyFooter();
+      verifyHeader();
+      pdp.checkDefaultProduct();
+      verifyFooter();
 
-  //   cy.takeScreenshot('Product details page');
-  // });
+      cy.takeScreenshot('Product details page');
+    });
 
-  // it('should render Contact us page', () => {
-  //   const contactPage = new ContactPage();
+    it('should render Contact us page', () => {
+      const contactPage = new ContactPage();
 
-  //   contactPage.visit(visitOptions);
+      contactPage.visit(visitOptions);
 
-  //   verifyHeader();
-  //   contactPage.getHeading().should('be.visible');
-  //   verifyFooter(false);
+      verifyHeader();
+      contactPage.getHeading().should('be.visible');
+      verifyFooter(false);
 
-  //   cy.takeScreenshot('Contact Us page');
-  // });
+      cy.takeScreenshot('Contact Us page');
+    });
 
-  // it('should render Login page', () => {
-  //   const loginPage = new LoginPage();
+    it('should render Login page', () => {
+      const loginPage = new LoginPage();
 
-  //   loginPage.visit(visitOptions);
+      loginPage.visit(visitOptions);
 
-  //   verifyHeader();
-  //   loginPage.loginForm.getWrapper().should('be.visible');
-  //   verifyFooter();
+      verifyHeader();
+      loginPage.loginForm.getWrapper().should('be.visible');
+      verifyFooter();
 
-  //   cy.takeScreenshot('Login page');
-  // });
+      cy.takeScreenshot('Login page');
+    });
 
-  // it('should render Cart page', () => {
-  //   const cartPage = new CartPage();
+    it('should render Cart page', () => {
+      const cartPage = new CartPage();
 
-  //   api = new GlueAPI();
+      api = new GlueAPI();
 
-  //   cy.createGuestCart(api);
-  //   cy.addProductToGuestCart(api, 1, ProductStorage.getByEq(4));
-  //   cy.goToGuestCart(visitOptions);
+      cy.createGuestCart(api);
+      cy.addProductToGuestCart(api, 1, ProductStorage.getByEq(4));
+      cy.goToGuestCart(visitOptions);
 
-  //   verifyHeader();
-  //   cartPage.checkNotEmptyCart();
-  //   verifyFooter();
+      verifyHeader();
+      cartPage.checkNotEmptyCart();
+      verifyFooter();
 
-  //   cartPage.templateIsReady();
+      cartPage.templateIsReady();
 
-  //   cy.takeScreenshot('Cart page');
-  // });
+      cy.takeScreenshot('Cart page');
+    });
 
-  // it('should render Search page', () => {
-  //   const searchPage = new SearchPage({ q: 'TomTom' });
+    it('should render Search page', () => {
+      const searchPage = new SearchPage({ q: 'TomTom' });
 
-  //   searchPage.visit(visitOptions);
+      searchPage.visit(visitOptions);
 
-  //   verifyHeader();
-  //   searchPage.getFacets().getWrapper().should('be.visible');
-  //   searchPage.getProductSorting().getWrapper().should('be.visible');
-  //   searchPage.getProductCards().should('have.length.greaterThan', 1);
-  //   searchPage.getProductHeadings().should('contain.text', 'TomTom');
-  //   verifyFooter();
+      verifyHeader();
+      searchPage.getFacets().getWrapper().should('be.visible');
+      searchPage.getProductSorting().getWrapper().should('be.visible');
+      searchPage.getProductCards().should('have.length.greaterThan', 1);
+      searchPage.getProductHeadings().should('contain.text', 'TomTom');
+      verifyFooter();
 
-  //   cy.takeScreenshot('Search page');
-  // });
+      cy.takeScreenshot('Search page');
+    });
 
-  // it('should render Category page', () => {
-  //   const categoryPage = new CategoryPage({ id: '6' });
+    it('should render Category page', () => {
+      const categoryPage = new CategoryPage({ id: '6' });
 
-  //   categoryPage.visit(visitOptions);
+      categoryPage.visit(visitOptions);
 
-  //   verifyHeader();
-  //   categoryPage.getFacets().getWrapper().should('be.visible');
-  //   categoryPage.getProductSorting().getWrapper().should('be.visible');
-  //   categoryPage.getProductCards().should('have.length.greaterThan', 1);
-  //   verifyFooter();
+      verifyHeader();
+      categoryPage.getFacets().getWrapper().should('be.visible');
+      categoryPage.getProductSorting().getWrapper().should('be.visible');
+      categoryPage.getProductCards().should('have.length.greaterThan', 1);
+      verifyFooter();
 
-  //   cy.takeScreenshot('Category page');
-  // });
+      cy.takeScreenshot('Category page');
+    });
 
-  it('should render Checkout page', () => {
-    const checkoutPage = new CheckoutPage();
+    it('should render Checkout page', () => {
+      const checkoutPage = new CheckoutPage();
 
-    api = new GlueAPI();
+      api = new GlueAPI();
 
-    cy.createGuestCart(api);
-    cy.addProductToGuestCart(api, 1, ProductStorage.getByEq(4));
-    cy.goToGuestCheckout();
+      cy.createGuestCart(api);
+      cy.addProductToGuestCart(api, 1, ProductStorage.getByEq(4));
+      cy.goToGuestCheckout();
 
-    checkoutPage.visit(visitOptions);
-    verifyHeader();
-    checkoutPage.getPlaceOrderBtn().should('be.visible');
-    verifyFooter();
+      checkoutPage.visit(visitOptions);
+      verifyHeader();
+      checkoutPage.getPlaceOrderBtn().should('be.visible');
+      verifyFooter();
 
-    checkoutPage.templateIsReady();
+      checkoutPage.templateIsReady();
 
-    cy.takeScreenshot('Checkout page');
-  });
-  // }
+      cy.takeScreenshot('Checkout page');
+    });
+  }
 });
 
 function verifyFooter(isPageScrollable = true) {
