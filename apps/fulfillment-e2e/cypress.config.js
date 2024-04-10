90f802b19005bb1e0f5e62c93a4850c8c750d920
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2024-04-10 12:10:06 +0300

    Formatting

--- apps/fulfillment-e2e/cypress.config.js
+++ apps/fulfillment-e2e/cypress.config.js
@@ -24,7 +24,6 @@ module.exports = defineConfig({
   env: {
     glueApiUrl: 'https://glue.de.spryker-b2c.cloud.spryker.toys',
     backofficeUrl: 'http://backoffice.de.spryker-b2c.cloud.spryker.toys',
-    backofficeApiUrl:
-      'https://glue-backend.de.spryker-b2c.cloud.spryker.toys',
+    backofficeApiUrl: 'https://glue-backend.de.spryker-b2c.cloud.spryker.toys',
   },
 });
