e6fab564508c428b695a6e76fb4ac8286c767edc
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2024-04-10 17:55:26 +0300

    Fix linter

--- apps/fulfillment-e2e/src/support/commands.ts
+++ apps/fulfillment-e2e/src/support/commands.ts
@@ -113,6 +113,7 @@ Cypress.Commands.add('glueApiCreateOrder', (numberOfItems = 1) => {
 Cypress.Commands.add('waitForPickingToAppear', (orderId: string) => {
   const list = new ListsFragment();
 
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
   cy.wait(10000);
 
   list.getPickingListsItems().should('have.length', 1);
